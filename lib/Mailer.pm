bac4c73c26d7a778a42447f99c6727798f674dfe
Robert Tansley <support@eprints.org>
2000-04-27 14:00:51 +0000

    - Added a couple of convenience methods for filling out the mail templates and mailing them.
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@58 9491667e-5006-0410-a446-efbe8990b998

--- lib/Mailer.pm
+++ lib/Mailer.pm
@@ -36,15 +36,97 @@ sub send_mail
 		or return( 0 );
 
 	print SENDMAIL <<"EOF";
+X-Loop: $EPrintSite::SiteInfo::automail
 From: $EPrintSite::SiteInfo::sitename<$EPrintSite::SiteInfo::admin>
 To: $name <$address>
 Subject: $EPrintSite::SiteInfo::sitename: $subject
 
 $body
+
+$EPrintSite::SiteInfo::signature
 EOF
 
 	close(SENDMAIL) or return( 0 );
 	return( 1 );
 }
+
+
+######################################################################
+#
+# $body = prepare_mail( $template_filename, $user )
+#
+#  Reads the mail template from the given file, and replaces the
+#  relevant placeholders with values. Placeholders are:
+#
+#   __username__    username of $user
+#   __password__    password of $user
+#   __sitename__    name of the site
+#   __admin__       admin email address
+#   __automail__    email address of automatic mail processing account
+#   __perlroot__    URL of perl server
+#   __staticroot__  URL of static HTTP server
+#   __frontpage__   URL of site front page
+#   __usermail__    user's email address
+#
+#  The updated body is returned, or undef if an error occurred.
+#
+######################################################################
+
+sub prepare_mail
+{
+	my( $class, $template_filename, $user ) = @_;
+	
+	my $body = "";
+
+	open( INTROFILE, $template_filename ) or return( undef );
+	
+	while( <INTROFILE> )
+	{
+		s/__username__/$user->{username}/g;
+		s/__password__/$user->{passwd}/g;
+		s/__usermail__/$user->{email}/g;
+		s/__sitename__/$EPrintSite::SiteInfo::sitename/g;
+		s/__admin__/$EPrintSite::SiteInfo::admin/g;
+		s/__automail__/$EPrintSite::SiteInfo::automail/g;
+		s/__perlroot__/$EPrintSite::SiteInfo::server_perl/g;
+		s/__staticroot__/$EPrintSite::SiteInfo::server_static/g;
+		s/__frontpage__/$EPrintSite::SiteInfo::frontpage/g;
+
+		$body .= $_;
+	}
+	
+	close( INTROFILE );
+
+	return( $body );
+}
+
+
+######################################################################
+#
+# $success = prepare_send_mail( $name, $address, $subject,
+#                               $templatefile, $user )
+#
+#  Prepare and send a mail to the given $name and $address, filling
+#  out $templatefile with appropriate values.
+#
+######################################################################
+
+sub prepare_send_mail
+{
+	my( $class, $name, $address, $subject, $templatefile, $user ) = @_;
+	
+	my $body = EPrints::Mailer->prepare_mail(
+		$templatefile,
+		$user );
+
+	return( 0 ) unless( defined $body );
+
+	return( EPrints::Mailer->send_mail(
+		$name,
+		$address,
+		$subject,
+		$body ) );
+}
+
 
 1;
