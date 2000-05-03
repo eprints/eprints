f4f51c5436d96ed882c594044a471a7b6b293a64
Robert Tansley <support@eprints.org>
2000-05-03 14:07:38 +0000

    - Updated name code to take into account the "John van der Smith" type case. Unfortunately it still can't cope with the names specified in the old CogPrints files as "Smith, John van der".
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@73 9491667e-5006-0410-a446-efbe8990b998

--- lib/Name.pm
+++ lib/Name.pm
@@ -125,4 +125,136 @@ sub add_name
 }
 
 
+######################################################################
+#
+# $namespec = import_names( $list, $join_char )
+#
+#  Formats a list of names into the internal format used by EPrints.
+#  $join_char is the character that separates names in the old list.
+#
+######################################################################
+
+sub import_names
+{
+	my( $calss, $list, $join_char ) = @_;
+	
+	my @oldnames = split /\s*$join_char\s*/, $list;
+	my $n;	
+
+	my $namelist = undef;
+
+	foreach $n (@oldnames)
+	{
+		if( $n =~ /,/ )
+		{
+			# It's in the form surname, first names (we hope)
+			my( $surname, $firstnames ) = split /\,\s*/, $n;
+			$namelist = EPrints::Name->add_name( $namelist,
+			                                     $surname,
+			                                     $firstnames );
+		}
+		else
+		{
+			# Form firstname surname
+			my( $surname, $firstnames ) = EPrints::Name->split_name( $n );
+			$namelist = EPrints::Name->add_name( $namelist, $surname, $firstnames )
+				if( defined $surname );
+		}
+	}
+
+	return( $namelist );
+}
+
+
+######################################################################
+#
+# ( $surname, $firstnames ) = split_name( $name )
+#
+#  This utility method splits a name into surname and firstname parts.
+#  It recognises when a comma is used to put the surname first. It
+#  also recognises words like "van" and "van der" as part of the surname.
+#  (Technically termed in the code "middle bits".)
+#
+######################################################################
+
+sub split_name
+{
+	my( $class, $name ) = @_;
+	
+	# If the name is empty, return undef
+	return( undef ) if( !defined $name || $name eq "" );
+
+	my @names = split /\s+/, $name;
+	
+	my $middle_bits = 0;
+
+	# If there's more than one part to the name, count how many "middle bits"
+	# there are (i.e. words like "van" and "de" that are part of the surname,
+	# but aren't the last word in the name)
+	if( $#names >= 1 )
+	{
+		my @known_middle_bits = ( "van", "de", "von", "der" );
+		
+		my $i;
+		for( $i = $#names - 1; $i > 0; $i-- )
+		{
+			my $possible_middle_bit = $names[$#names-1];
+			my $is_middle_bit = 0;
+		
+			foreach (@known_middle_bits)
+			{
+				$is_middle_bit = 1 if( (lc $_) eq (lc $possible_middle_bit) );
+			}
+
+			if( $is_middle_bit )
+			{
+				$middle_bits++;
+			}
+			else
+			{
+				$i = 0;
+			}
+		}
+	}
+		
+	# Now split the name up
+	my $firstnames = "";
+	my $surname = "";
+	my $i;
+
+	if( $middle_bits )
+	{	
+		my $i;
+
+		# Collect the surname
+		for( $i = $#names; $i >= $#names-$middle_bits; $i-- )
+		{
+			$surname = $names[$i]." ".$surname;
+		}
+		
+		# Remove trailing spaces
+		$surname =~ s/\s*$//;
+
+		for( $i=0; $i<$#names-$middle_bits; $i++ )
+		{
+			$firstnames .= " " if ($i>0);
+			$firstnames .= $names[$i];
+		}
+	}
+	else
+	{
+		$surname = $names[$#names];
+
+		for( $i=0; $i<$#names; $i++ )
+		{
+			$firstnames .= " " if ($i>0);
+			$firstnames .= $names[$i];
+		}
+	}
+
+	my @result = ( $surname, $firstnames );
+	
+	return( @result );
+}
+
 1;
