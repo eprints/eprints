a264ed378d6d591cc918cc551cc72d3d7e1afd02
Robert Tansley <support@eprints.org>
2000-09-01 10:54:06 +0000

    - Editors now rendered in book chapter references.
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@199 9491667e-5006-0410-a446-efbe8990b998

--- site_lib/SiteRoutines.pm
+++ site_lib/SiteRoutines.pm
@@ -33,9 +33,9 @@ use strict;
 
 %EPrints::SiteRoutines::citation_specs =
 (
-	"bookchapter" => "{authors} [({year}) ]<i>{title}</i>[, in <i>{publication}</i>][, chapter {chapter}][, pages {pages}]. [{publisher}.]",
-	"confpaper"   => "{authors} [({year}) ]{title}. In [{editors}, Eds. ] [<i>Proceedings {conference}.</i>][ <B>{volume}</B>][({number})][, pages {pages}][, {confloc}].",
-	"confposter"  => "{authors} [({year}) ]{title}. In [{editors}, Eds. ] [<i>Proceedings {conference}.</i>][ <B>{volume}</B>][({number})][, pages {pages}][, {confloc}].",
+	"bookchapter" => "{authors} [({year}) ]<i>{title}</i>, in [{editors}, Eds. ][<i>{publication}</i>][, chapter {chapter}][, pages {pages}]. [{publisher}.]",
+	"confpaper"   => "{authors} [({year}) ]{title}. In [{editors}, Eds. ][<i>Proceedings {conference}.</i>][ <B>{volume}</B>][({number})][, pages {pages}][, {confloc}].",
+	"confposter"  => "{authors} [({year}) ]{title}. In [{editors}, Eds. ][<i>Proceedings {conference}.</i>][ <B>{volume}</B>][({number})][, pages {pages}][, {confloc}].",
 	"techreport"  => "{authors} [({year}) ]{title}. Technical Report[ {reportno}][, {department}][, {institution}].",
 	"journale"    => "{authors} [({year}) ]{title}. <i>{publication}</i>[ {volume}][({number})].",
 	"journalp"    => "{authors} [({year}) ]{title}. <i>{publication}</i>[ {volume}][({number})][:{pages}].",
