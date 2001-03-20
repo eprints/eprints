5f6f985002789e2a2c03f0a4639d33bf9b0edf0e
Christopher Gutteridge <cjg@ecs.soton.ac.uk>
2001-03-20 19:30:52 +0000

    Version file.
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@381 9491667e-5006-0410-a446-efbe8990b998

---
+++ lib/Version.pm
@@ -0,0 +1,25 @@
+######################################################################
+#
+# EPrint Version
+#
+#  Holds information about the current version of EPrints.
+#
+######################################################################
+#
+#  __COPYRIGHT__
+#
+# Copyright 2000-2008 University of Southampton. All Rights Reserved.
+# 
+#  __LICENSE__
+#
+######################################################################
+
+package EPrints::Version;
+
+use strict;
+
+
+# GLOBAL SITE REVISION NUMBER
+$EPrints::Version::eprints_software_version = "Version 2.0 (pre-alpha)";
+
+1;
