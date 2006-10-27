f330f0148d232ca0c5d9e94844bb00ced80f6ecf
Timothy Miles-Board <tmb@ecs.soton.ac.uk>
2006-10-27 16:52:19 +0000

    * Fixed and tidied 'request a copy' feature
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@2034 9491667e-5006-0410-a446-efbe8990b998

--- lib/defaultcfg/cfg.d/email.pl
+++ lib/defaultcfg/cfg.d/email.pl
@@ -26,18 +26,32 @@ $c->{send_email} = \&EPrints::Utils::send_mail_via_smtp;
 #};
 
 
+# The 'Request a copy' feature allows any user to request a copy of a 
+# non-OA document by email. This function determines who to send the 
+# request to. If, for a given eprint, the function returns undef, the 
+# 'Request a copy' button will not be shown for that eprint.
 $c->{email_for_doc_request} = sub 
 {
 	my ( $session, $eprint ) = @_;
-	# To turn off this feature, uncomment the line below
+
+	# Uncomment the line below to turn off this feature
 	#return undef;
+
 	if ($eprint->is_set("contact_email")) {
 		return $eprint->get_value("contact_email");
 	}
-	my $user = $eprint->get_user;
-	if( defined $user && $user->is_set("email")) {
-		return $user->get_value("email");
-	}
-	return $session->get_repository->get_conf("adminemail");
+
+	# Uncomment the line below to fall back to the email
+	# address of the person who deposited this eprint - beware
+	# that this may not always be the author!
+	#my $user = $eprint->get_user;
+	#if( defined $user && $user->is_set("email")) {
+	#	return $user->get_value("email");
+	#}
+
+	# Uncomment the line below to fall back to the email
+	# address of the archive administrator - think carefully!
+	#return $session->get_repository->get_conf("adminemail");
+
+	return undef;
 }
-;
