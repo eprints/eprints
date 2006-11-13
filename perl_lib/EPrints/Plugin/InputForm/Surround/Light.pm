001387a0ca2970990a62b29090d67d7c921fb2c7
Mike Jewell <moj@ecs.soton.ac.uk>
2006-11-13 18:11:49 +0000

    2006-11-13 moj  * Added Light surround for file upload help  * Added help phrase for upload  * Added CC link to license help
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@2117 9491667e-5006-0410-a446-efbe8990b998

---
+++ perl_lib/EPrints/Plugin/InputForm/Surround/Light.pm
@@ -0,0 +1,37 @@
+package EPrints::Plugin::InputForm::Surround::Light;
+
+use strict;
+
+our @ISA = qw/ EPrints::Plugin /;
+
+
+sub render
+{
+	my( $self, $component ) = @_;
+	
+	my $content_class="";
+
+	my $surround = $self->{session}->make_element( "div", class => "ep_sr_component" );
+	foreach my $field_id ( $component->get_fields_handled )
+	{
+		$surround->appendChild( $self->{session}->make_element( "a", name=>$field_id ) );
+	}
+	if( $component->get_internal_button && (scalar @{$component->{stage}->{components}} > 1) )
+	{
+		$surround->appendChild( $self->{session}->make_element( "a", name=>"t" ) );
+	}
+	
+	my $content = $self->{session}->make_element( "div", id => $component->{prefix}."_content", class=>"$content_class ep_sr_content" );
+	my $content_inner = $self->{session}->make_element( "div", id => $component->{prefix}."_content_inner" );
+
+	$content->appendChild( $content_inner );
+	$content_inner->appendChild( $component->render_content( $self ) );
+	
+	
+	$surround->appendChild( $content );
+
+	return $surround;
+}
+
+
+1;
