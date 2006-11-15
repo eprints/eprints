fb7221360f4f0f2d0717c11b136060f108de2331
Tim Brody <tdb2@ecs.soton.ac.uk>
2006-11-15 18:08:56 +0000

    * MODS/METS fairly complete: Mime-type TODO, check on global ID usage, testing
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@2146 9491667e-5006-0410-a446-efbe8990b998

--- perl_lib/EPrints/Plugin/Export/METS.pm
+++ perl_lib/EPrints/Plugin/Export/METS.pm
@@ -19,6 +19,7 @@ use EPrints::Plugin::Export;
 our @ISA = qw( EPrints::Plugin::Export );
 
 our $PREFIX = "mets:";
+our $MODS_PREFIX = "mods:";
 
 sub new
 {
@@ -74,26 +75,34 @@ sub xml_dataobj
 	$mets->appendChild( _make_header( $session, $dataobj ));
 
 	# dmdSec
-	my $mods_id = "DMD_".$id."_mods";
+	my $mods_id = "DMD_".$id."_mods"; # also used in structMap
 	$mets->appendChild(my $mods_dmd = $session->make_element(
-		"dmdSec",
+		"${PREFIX}dmdSec",
 		"ID" => $mods_id
 	));
 	$mods_dmd->appendChild(my $mods_mdWrap = $session->make_element(
-		"mdWrap",
-		"MDTYPE" => "MODS"
+		"${PREFIX}mdWrap",
+		"MDTYPE" => "mods"
 	));
-	$mods_mdWrap->appendChild(
-		$mods_plugin->xml_dataobj( $dataobj )
-	);
+	my $mods = $mods_plugin->xml_dataobj( $dataobj );
+	$mods_mdWrap->appendChild( my $xmlData = $session->make_element(
+		"${PREFIX}xmlData"
+	));
+	foreach my $node ($mods->childNodes)
+	{
+		$xmlData->appendChild( $node );
+	}
 	
 	# amdSec
 	my $amd_id = "TMD_".$id;
 	my $rights_id = "rights_".$id."_mods";
+	$mets->appendChild( _make_amdSec( $session, $dataobj, $amd_id, $rights_id ));
 	
 	# fileSec
+	$mets->appendChild( _make_fileSec( $session, $dataobj, $id ));
 	
 	# structMap
+	$mets->appendChild( _make_structMap( $session, $dataobj, $id, $mods_id, $amd_id ));
 
 	return $mets;
 }
@@ -121,6 +130,129 @@ sub _make_header
 	$name->appendChild( $session->make_text( $aname ));
 	
 	return $header;
+}
+
+sub _make_amdSec
+{
+	my( $session, $dataobj, $amd_id, $rights_id ) = @_;
+	
+	my $amdSec = $session->make_element(
+		"${PREFIX}amdSec",
+		"ID" => $amd_id
+	);
+	
+	$amdSec->appendChild(my $rightsMD = $session->make_element(
+		"${PREFIX}rightsMD",
+		"ID" => $rights_id
+	));
+	
+	$rightsMD->appendChild( my $mdWrap = $session->make_element(
+		"${PREFIX}mdWrap",
+		"MDTYPE" => "mods"
+	));
+	
+	$mdWrap->appendChild( my $xmlData = $session->make_element(
+		"${PREFIX}xmlData",
+	));
+	
+	$xmlData->appendChild( my $mods_use = $session->make_element(
+		"${MODS_PREFIX}useAndReproduction"
+	));
+	$mods_use->appendChild( $session->html_phrase( "deposit_agreement_text" ));
+	
+	return $amdSec;
+}
+
+sub _make_fileSec
+{
+	my( $session, $dataobj, $id ) = @_;
+	
+	my $fileSec = $session->make_element(
+		"${PREFIX}fileSec"
+	);
+	
+	foreach my $doc ($dataobj->get_all_documents)
+	{
+		my $baseurl = $doc->get_baseurl;
+		my $id_base = $id."_".$doc->get_id;
+		my %files = $doc->files;
+
+		$fileSec->appendChild(my $fileGrp = $session->make_element(
+			"${PREFIX}fileGrp",
+			"USE" => "reference"
+		));
+
+		my $file_idx = 0;
+		while( my( $name, $size) = each %files )
+		{
+			$file_idx++;
+			my $url = $baseurl . $name;
+			my $mimetype = "TODO: FIXME!!!";
+
+			$fileGrp->appendChild( my $file = $session->make_element(
+				"${PREFIX}file",
+				"ID" => $id_base."_".$file_idx,
+				"SIZE" => $size,
+				"OWNERID" => $url,
+				"MIMETYPE" => $mimetype
+			));
+
+			$file->appendChild( $session->make_element(
+				"${PREFIX}FLocat",
+				"LOCTYPE" => "URL",
+				"xlink:type" => "simple",
+				"xlink:href" => $url
+			));
+		}
+	}
+	
+	return $fileSec;
+}
+
+sub _make_structMap
+{
+	my( $session, $dataobj, $id, $dmd_id, $amd_id ) = @_;
+	
+	my $structMap = $session->make_element( "${PREFIX}structMap" );
+	
+	$structMap->appendChild( my $top_div = $session->make_element(
+		"${PREFIX}div",
+		"DMDID" => $dmd_id,
+		"AMDID" => $amd_id
+	));
+	
+	foreach my $doc ($dataobj->get_all_documents)
+	{
+		my $id_base = $id."_".$doc->get_id;
+		my $file_idx = 0;
+		my %files = $doc->files;
+		if( scalar keys %files > 1 )
+		{
+			while( my( $name, $size ) = each %files )
+			{
+				$file_idx++;
+				my $file_id = $id_base."_".$file_idx;
+				$top_div->appendChild( my $div = $session->make_element(
+					"${PREFIX}div"
+				));
+				$div->appendChild( $session->make_element(
+					"${PREFIX}fptr",
+					"FILEID" => $file_id
+				));
+			}
+		}
+		else
+		{
+			$file_idx++;
+			my $file_id = $id_base."_".$file_idx;
+			$top_div->appendChild( $session->make_element(
+				"${PREFIX}fptr",
+				"FILEID" => $file_id
+			));
+		}
+	}
+	
+	return $structMap;
 }
 
 1;
