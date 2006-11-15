09c668c0b4d6b63a968a0e3d9136a652ada2685c
Tim Brody <tdb2@ecs.soton.ac.uk>
2006-11-15 13:21:20 +0000

    * Documented EPrints::Platform
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@2136 9491667e-5006-0410-a446-efbe8990b998

--- perl_lib/EPrints/Platform.pm
+++ perl_lib/EPrints/Platform.pm
@@ -28,6 +28,9 @@ EPrints::Platform::Unix
 Which module is used is configured by the {platform} setting in
 SystemSettings.pm
 
+All file and directory names are absolute and joined by the Unix
+file separator character '/'.
+
 =over 4
 
 =cut
@@ -42,14 +45,70 @@ my $platform = $EPrints::SystemSettings::conf->{platform};
 my $real_module = "EPrints::Platform::\u$platform";
 eval "use $real_module;";
 
+#####################################################################
+
+=item chmod( MODE, @filelist )
+
+Change the access control on files listed in @filelist to MODE.
+
+=cut
+
+#####################################################################
+
 sub chmod { return &{$real_module."::chmod"}( @_ ); }
+
+#####################################################################
+
+=item chown( $uid, $gid, @filelist )
+
+Change the user and group on files listed in @filelist to $uid and
+$gid. $uid and $gid are as returned by L<getpwnam> (usually numeric).
+
+=cut
+
+#####################################################################
 
 sub chown { return &{$real_module."::chown"}( @_ ); }
 
+#####################################################################
+
+=item getpwnam( $user )
+
+Return the login-name, password crypt, uid and gid for user $user.
+
+=cut
+
+#####################################################################
+
 sub getpwnam { return &{$real_module."::getpwnam"}( $_[0] ); }
 
+#####################################################################
+
+=item test_uid()
+
+Test whether the current user is the same that is configured in
+SystemSettings.
+
+=cut
+
+#####################################################################
+ 
 sub test_uid { return &{$real_module."::test_uid"}( @_ ); }
+
+#####################################################################
+
+=item mkdir( $path, MODE )
+
+Create a directory $path (including parent directories as necessary)
+set to mode MODE. If MODE is undefined defaults to dir_perms in
+SystemSettings.
+
+=cut
+
+#####################################################################
 
 sub mkdir { return &{$real_module."::mkdir"}( @_ ); }
 
 1;
+
+=back
