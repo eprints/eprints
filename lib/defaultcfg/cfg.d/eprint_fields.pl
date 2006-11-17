a5fa81799e14d499ca1cd6e33fd54a15e16965e0
Christopher Gutteridge <cjg@ecs.soton.ac.uk>
2006-11-17 20:50:45 +0000

    * Tidied up field definitions for users & eprints
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@2158 9491667e-5006-0410-a446-efbe8990b998

--- lib/defaultcfg/cfg.d/eprint_fields.pl
+++ lib/defaultcfg/cfg.d/eprint_fields.pl
@@ -1,183 +1,318 @@
 
 $c->{fields}->{eprint} = [
 
-	{ 
-		name        => "creators", 
-		type        => "compound",  
-		input_boxes => 4,
-		multiple    => 1,
-		fields=>[
-			{ 
-				sub_name         => "name", 
-				type            => "name", 
-				family_first    => 1, 
-				hide_honourific => 1, 
-				hide_lineage    => 1, 
-			}, 
-			{ 
-				sub_name    => "id", 
-				type       => "text", 
-				input_cols => 20, 
-				allow_null => 1, 
-			},
-		],
-	},
+          {
+            'name' => 'creators',
+            'type' => 'compound',
+            'multiple' => 1,
+            'fields' => [
+                          {
+                            'sub_name' => 'name',
+                            'type' => 'name',
+                            'hide_honourific' => 1,
+                            'hide_lineage' => 1,
+                            'family_first' => 1,
+                          },
+                          {
+                            'sub_name' => 'id',
+                            'type' => 'text',
+                            'input_cols' => 20,
+                            'allow_null' => 1,
+                          }
+                        ],
+            'input_boxes' => 4,
+          },
 
-	{
-		name     => "corp_creators",
-		type     => "text",
-		multiple => 1,
-	},
-		
+          {
+            'name' => 'corp_creators',
+            'type' => 'text',
+            'multiple' => 1,
+          },
 
-	{ name => "title", type => "longtext", multilang=>0, input_rows => 3 },
+          {
+            'name' => 'title',
+            'type' => 'longtext',
+            'input_rows' => 3,
+          },
 
-	{ name => "ispublished", type => "set", input_style=>"medium",
-			options => [ "pub","inpress","submitted" , "unpub" ] },
+          {
+            'name' => 'ispublished',
+            'type' => 'set',
+            'options' => [
+                           'pub',
+                           'inpress',
+                           'submitted',
+                           'unpub',
+                         ],
+            'input_style' => 'medium',
+          },
 
-	{ name => "subjects", type=>"subject", top=>"subjects", multiple => 1, 
-		browse_link => "subjects",
-		render_input=>"EPrints::Extras::subject_browser_input" },
+          {
+            'name' => 'subjects',
+            'type' => 'subject',
+            'multiple' => 1,
+            'top' => 'subjects',
+            'render_input' => 'EPrints::Extras::subject_browser_input',
+            'browse_link' => 'subjects',
+          },
 
-	{ name => "full_text_status", type=>"set", input_style=>"medium",
-			options => [ "public", "restricted", "none" ] },
+          {
+            'name' => 'full_text_status',
+            'type' => 'set',
+            'options' => [
+                           'public',
+                           'restricted',
+                           'none',
+                         ],
+            'input_style' => 'medium',
+          },
 
-	{ name => "monograph_type", type=>"set", input_style=>"medium",
-			options => [ 
-				"technical_report", 
-				"project_report",
-				"documentation",
-				"manual",
-				"working_paper",
-				"discussion_paper",
-				"other" ] },
+          {
+            'name' => 'monograph_type',
+            'type' => 'set',
+            'options' => [
+                           'technical_report',
+                           'project_report',
+                           'documentation',
+                           'manual',
+                           'working_paper',
+                           'discussion_paper',
+                           'other',
+                         ],
+            'input_style' => 'medium',
+          },
 
+          {
+            'name' => 'pres_type',
+            'type' => 'set',
+            'options' => [
+                           'paper',
+                           'lecture',
+                           'speech',
+                           'poster',
+                           'other',
+                         ],
+            'input_style' => 'medium',
+          },
 
+          {
+            'name' => 'keywords',
+            'type' => 'longtext',
+            'input_rows' => 2,
+          },
 
-	{ name => "pres_type", type=>"set", input_style=>"medium",
-			options => [ 
-				"paper", 
-				"lecture", 
-				"speech", 
-				"poster", 
-				"other" ] },
+          {
+            'name' => 'note',
+            'type' => 'longtext',
+            'input_rows' => 3,
+          },
 
-	{ name => "keywords", type => "longtext", input_rows => 2 },
+          {
+            'name' => 'suggestions',
+            'type' => 'longtext',
+            'render_value' => 'EPrints::Extras::render_highlighted_field',
+          },
 
-	{ 
-		name => "note", 
-		type => "longtext", 
-		input_rows => 3,
-	},
+          {
+            'name' => 'abstract',
+            'type' => 'longtext',
+            'input_rows' => 10,
+          },
 
-	{ 
-		name => "suggestions", 
-		type => "longtext",
-		render_value => "EPrints::Extras::render_highlighted_field",
-	},
+          {
+            'name' => 'date',
+            'type' => 'date',
+            'min_resolution' => 'year',
+          },
 
-	{ 
-		name => "abstract", 
-		input_rows => 10,
-		type => "longtext",
-	},
+          {
+            'name' => 'date_type',
+            'type' => 'set',
+            'options' => [
+                           'published',
+                           'submitted',
+                           'completed',
+                         ],
+            'input_style' => 'medium',
+          },
 
-	{ name => "date", type=>"date", min_resolution=>"year" },
+          {
+            'name' => 'series',
+            'type' => 'text',
+          },
 
-	{ name => "date_type", type=>"set", options=>[qw/ published submitted completed /],
-		 input_style=>"medium", },
+          {
+            'name' => 'publication',
+            'type' => 'text',
+          },
 
-	{ name => "series", type => "text" },
+          {
+            'name' => 'volume',
+            'type' => 'text',
+            'maxlength' => 6,
+          },
 
-	{ name => "publication", type => "text" },
+          {
+            'name' => 'number',
+            'type' => 'text',
+            'maxlength' => 6,
+          },
 
-	{ name => "volume", type => "text", maxlength => 6 },
+          {
+            'name' => 'publisher',
+            'type' => 'text',
+          },
 
-	{ name => "number", type => "text", maxlength => 6 },
+          {
+            'name' => 'place_of_pub',
+            'type' => 'text',
+          },
 
-	{ name => "publisher", type => "text" },
+          {
+            'name' => 'pagerange',
+            'type' => 'pagerange',
+          },
 
-	{ name => "place_of_pub", type => "text" },
+          {
+            'name' => 'pages',
+            'type' => 'int',
+            'maxlength' => 6,
+            'sql_index' => 0,
+          },
 
-	{ name => "pagerange", type => "pagerange" },
+          {
+            'name' => 'event_title',
+            'type' => 'text',
+          },
 
-	{ name => "pages", type => "int", maxlength => 6, sql_index => 0 },
+          {
+            'name' => 'event_location',
+            'type' => 'text',
+          },
 
-	{ name => "event_title", type => "text" },
+          {
+            'name' => 'event_dates',
+            'type' => 'text',
+          },
 
-	{ name => "event_location", type => "text" },
-	
-	{ name => "event_dates", type => "text" },
+          {
+            'name' => 'event_type',
+            'type' => 'set',
+            'options' => [
+                           'conference',
+                           'workshop',
+                           'other',
+                         ],
+            'input_style' => 'medium',
+          },
 
-	{ name => "event_type", type => "set", options=>[ "conference","workshop","other" ], input_style=>"medium", },
+          {
+            'name' => 'id_number',
+            'type' => 'text',
+          },
 
-	{ name => "id_number", type => "text" },
+          {
+            'name' => 'patent_applicant',
+            'type' => 'text',
+          },
 
-	{ name => "patent_applicant", type => "text" },
+          {
+            'name' => 'institution',
+            'type' => 'text',
+          },
 
-	{ name => "institution", type => "text" },
+          {
+            'name' => 'department',
+            'type' => 'text',
+          },
 
-	{ name => "department", type => "text" },
+          {
+            'name' => 'thesis_type',
+            'type' => 'set',
+            'options' => [
+                           'masters',
+                           'phd',
+                           'other',
+                         ],
+            'input_style' => 'medium',
+          },
 
-	{ name => "thesis_type", type => "set", options=>[ "masters", "phd", "other"], input_style=>"medium", },
+          {
+            'name' => 'refereed',
+            'type' => 'boolean',
+            'input_style' => 'radio',
+          },
 
-	{ name => "refereed", type => "boolean", input_style=>"radio" },
+          {
+            'name' => 'isbn',
+            'type' => 'text',
+          },
 
-	{ name => "isbn", type => "text" },
+          {
+            'name' => 'issn',
+            'type' => 'text',
+          },
 
-	{ name => "issn", type => "text" },
+          {
+            'name' => 'book_title',
+            'type' => 'text',
+          },
 
-	{ name => "book_title", type => "text" },
-	
-	{
-		name => "editors", 
-		type=>"compound",  
-		input_boxes => 4,
-		multiple=>1,
-		fields=>[
-			{ 
-				sub_name => "name", 
-				type => "name", 
-				family_first => 1, 
-				hide_honourific => 1, 
-				hide_lineage => 1, 
-			}, 
-			{ 
-				sub_name => "id", 
-				type => "text", 
-				input_cols => 20, 
-				allow_null => 1, 
-			},
-		],
-	},
+          {
+            'name' => 'editors',
+            'type' => 'compound',
+            'multiple' => 1,
+            'fields' => [
+                          {
+                            'hide_honourific' => 1,
+                            'type' => 'name',
+                            'hide_lineage' => 1,
+                            'family_first' => 1,
+                            'sub_name' => 'name',
+                          },
+                          {
+                            'input_cols' => 20,
+                            'allow_null' => 1,
+                            'type' => 'text',
+                            'sub_name' => 'id',
+                          }
+                        ],
+            'input_boxes' => 4,
+          },
 
-	{ 
-		name => "official_url", 
-		type=>"url",  
-	},
+          {
+            'name' => 'official_url',
+            'type' => 'url',
+          },
 
-	{ 
-		name => "related_url", 
-		type=>"compound",  
-		input_boxes => 1,
-		input_ordered => 0,
-		multiple=>1,
-		fields=>[
-			{ 
-				sub_name => "url", 
-				type => "url", 
-				input_cols => 40,
-			},
-			{ 
-				sub_name => "type", 
-				type => "set", 
-				options => [qw/ pub author org /],
-			},
-		],
-	},
+          {
+            'name' => 'related_url',
+            'type' => 'compound',
+            'multiple' => 1,
+            'fields' => [
+                          {
+                            'sub_name' => 'url',
+                            'type' => 'url',
+                            'input_cols' => 40,
+                          },
+                          {
+                            'sub_name' => 'type',
+                            'type' => 'set',
+                            'options' => [
+                                           'pub',
+                                           'author',
+                                           'org',
+                                         ],
+                          }
+                        ],
+            'input_boxes' => 1,
+            'input_ordered' => 0,
+          },
 
-# nb. Can't call this field "references" because that's a MySQL keyword.
-	{ name => "referencetext", type => "longtext", input_rows => 15 },
+          {
+            'name' => 'referencetext',
+            'type' => 'longtext',
+            'input_rows' => 15,
+          }
 
 ];
-
--- lib/defaultcfg/cfg.d/user_fields.pl
+++ lib/defaultcfg/cfg.d/user_fields.pl
@@ -1,22 +1,54 @@
 
 $c->{fields}->{user} = [
 
-	{ name => "name", type => "name", render_order=>"gf" },
+          {
+            'name' => 'name',
+            'type' => 'name',
+            'render_order' => 'gf',
+          },
 
-	{ name => "dept", type => "text" },
+          {
+            'name' => 'dept',
+            'type' => 'text',
+          },
 
-	{ name => "org", type => "text" },
+          {
+            'name' => 'org',
+            'type' => 'text',
+          },
 
-	{ name => "address", type => "longtext", input_rows => 5 },
+          {
+            'name' => 'address',
+            'type' => 'longtext',
+            'input_rows' => 5,
+          },
 
-	{ name => "country", type => "text" },
+          {
+            'name' => 'country',
+            'type' => 'text',
+          },
 
-	{ name => "hideemail", type => "boolean", input_style=>"radio" },
+          {
+            'name' => 'hideemail',
+            'input_style' => 'radio',
+            'type' => 'boolean',
+          },
 
-	{ name => "os", type => "set", input_rows => 1,
-		options => [ "win", "unix", "vms", "mac", "other" ] },
+          {
+            'name' => 'os',
+            'type' => 'set',
+            'options' => [
+                           'win',
+                           'unix',
+                           'vms',
+                           'mac',
+                           'other',
+                         ],
+            'input_rows' => 1,
+          },
 
-	{ name => "url", type => "url" }
-
-];
-
+          {
+            'name' => 'url',
+            'type' => 'url',
+          }
+]; 
