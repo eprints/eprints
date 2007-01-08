c6f7ecd5ec797f7c5bf6c8fd249a4305b9a26a40
Mike Jewell <moj@ecs.soton.ac.uk>
2007-01-08 17:14:33 +0000

    2007-01-08 moj  * Set userid to be optional  * Previous commit: Changed userid to use user dataset rather than     document.
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@2371 9491667e-5006-0410-a446-efbe8990b998

--- perl_lib/EPrints/DataObj/Request.pm
+++ perl_lib/EPrints/DataObj/Request.pm
@@ -53,7 +53,7 @@ sub get_system_field_info
 		{ name=>"datestamp", type=>"time", required=>1, },
 
 		{ name=>"userid", type=>"itemref", 
-			datasetid=>"user", required=>1 },
+			datasetid=>"user", required=>0 },
 
 		{ name=>"email", type=>"text", required=>1 },
 
