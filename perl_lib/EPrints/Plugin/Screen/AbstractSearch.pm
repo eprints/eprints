7c302740bd187ae4577d6fc6909149d80cfb6fc2
Christopher Gutteridge <cjg@ecs.soton.ac.uk>
2007-08-13 12:56:46 +0000

    * Tidied Abstract search to be easier to subclass export plugin list.
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@2861 9491667e-5006-0410-a446-efbe8990b998

--- perl_lib/EPrints/Plugin/Screen/AbstractSearch.pm
+++ perl_lib/EPrints/Plugin/Screen/AbstractSearch.pm
@@ -256,13 +256,16 @@ sub render
 
 sub _get_export_plugins
 {
-	my( $self ) = @_;
+	my( $self, $include_not_advertised ) = @_;
 
-	return $self->{session}->plugin_list( 
+	my $is_advertised = 1;
+	my %opts =  (
 			type=>"Export",
 			can_accept=>"list/".$self->{processor}->{search}->{dataset}->confid, 
-			is_advertised=>1,
-			is_visible=>$self->_vis_level );
+			is_visible=>$self->_vis_level,
+	);
+	unless( $include_not_advertised ) { $is_advertised = 1; }
+	return $self->{session}->plugin_list( %opts );
 }
 
 sub _vis_level
@@ -698,10 +701,7 @@ sub wishes_to_export
 
 	my $format = $self->{session}->param( "output" );
 
-	my @plugins = $self->{session}->plugin_list(
-		type=>"Export",
-		can_accept=>"list/eprint",
-		is_visible=>$self->_vis_level );
+	my @plugins = $self->_get_export_plugins( 1 );
 		
 	my $ok = 0;
 	foreach( @plugins ) { if( $_ eq "Export::$format" ) { $ok = 1; last; } }
