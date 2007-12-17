eprints/eprints

commit 2c8b28b118f4057f5eca7341402ff90fe290ee45

* Changed Items and Review screens to use an icon to view the eprint rather     than hyperlinking the text.
git-svn-id: https://svn.eprints.org/eprints/trunk/system@3149 9491667e-5006-0410-a446-efbe8990b998

perl_lib/EPrints/Plugin/Screen/Items.pm
@@ -151,12 +151,10 @@ sub render
 			my $first = 1;
 			for( @$columns )
 			{
-				my $td = $session->make_element( "td", class=>"ep_columns_cell_$status".($first?" ep_columns_cell_first":"") );
+				my $td = $session->make_element( "td", class=>"ep_columns_cell".($first?" ep_columns_cell_first":"")." ep_columns_cell_$_"  );
 				$first = 0;
 				$tr->appendChild( $td );
-				my $a = $session->render_link( "?eprintid=".$e->get_id."&screen=EPrint::View::Owner" );
-				$td->appendChild( $a );
-				$a->appendChild( $e->render_value( $_ ) );
+				$td->appendChild( $e->render_value( $_ ) );
 			}
 
 			$self->{processor}->{eprint} = $e;

perl_lib/EPrints/Plugin/Screen/EPrint/View/Editor.pm
@@ -15,6 +15,15 @@ sub new
 
 	$self->{staff} = 1;
 
+	$self->{icon} = "/style/images/action_view.png";
+
+	$self->{appears} = [
+		{
+			place => "eprint_review_actions",
+			position => 10,
+		},
+	];
+
 	return $self;
 }
 

perl_lib/EPrints/Plugin/Screen/EPrint/View/Owner.pm
@@ -15,6 +15,15 @@ sub new
 
 	$self->{staff} = 0;
 
+	$self->{icon} = "/style/images/action_view.png";
+
+	$self->{appears} = [
+		{
+			place => "eprint_item_actions",
+			position => 10,
+		},
+	];
+
 	return $self;
 }
 

perl_lib/EPrints/Plugin/Screen/Review.pm
@@ -92,12 +92,10 @@ sub render
 			my $first = 1;
 			for( @$cols )
 			{
-				my $td = $session->make_element( "td", class=>"ep_columns_cell".($first?" ep_columns_cell_first":"") );
+				my $td = $session->make_element( "td", class=>"ep_columns_cell".($first?" ep_columns_cell_first":"")." ep_columns_cell_$_"  );
 				$first = 0;
 				$tr->appendChild( $td );
-				my $a = $session->render_link( "?eprintid=".$e->get_id."&screen=EPrint::View::Editor" );
-				$td->appendChild( $a );
-				$a->appendChild( $e->render_value( $_ ) );
+				$td->appendChild( $e->render_value( $_ ) );
 			}
 
 			$self->{processor}->{eprint} = $e;

commit 3484591d410df4cd8e1c41d71c8ced0dd7581a60

* Fixed bug introduced in r3149 which lost the colour on rows in Items.
git-svn-id: https://svn.eprints.org/eprints/trunk/system@3151 9491667e-5006-0410-a446-efbe8990b998

perl_lib/EPrints/Plugin/Screen/Items.pm
@@ -151,7 +151,7 @@ sub render
 			my $first = 1;
 			for( @$columns )
 			{
-				my $td = $session->make_element( "td", class=>"ep_columns_cell".($first?" ep_columns_cell_first":"")." ep_columns_cell_$_"  );
+				my $td = $session->make_element( "td", class=>"ep_columns_cell_$status".($first?" ep_columns_cell_first":"")." ep_columns_cell_$_"  );
 				$first = 0;
 				$tr->appendChild( $td );
 				$td->appendChild( $e->render_value( $_ ) );