3f4c5d02e13f2cf3565a61d661cf2e22c74957d1
Christopher Gutteridge <cjg@ecs.soton.ac.uk>
2007-12-18 11:08:34 +0000

    * Added Export/IDs which is a very simple (and fast) export that just     outputs a \n separated list of IDs of the items.
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@3158 9491667e-5006-0410-a446-efbe8990b998

---
+++ perl_lib/EPrints/Plugin/Export/Ids.pm
@@ -0,0 +1,40 @@
+package EPrints::Plugin::Export::Ids;
+
+use Unicode::String qw( utf8 );
+
+use EPrints::Plugin::Export;
+
+@ISA = ( "EPrints::Plugin::Export" );
+
+use strict;
+
+sub new
+{
+	my( $class, %opts ) = @_;
+
+	my $self = $class->SUPER::new( %opts );
+
+	$self->{name} = "Object IDs";
+	$self->{accept} = [ 'list/*' ];
+	$self->{visible} = "all";
+	$self->{suffix} = ".txt";
+	$self->{mimetype} = "text/plain; charset=utf-8";
+	
+	return $self;
+}
+
+
+sub output_list
+{
+	my( $plugin, %opts ) = @_;
+
+	if( defined $opts{fh} )
+	{
+		print {$opts{fh}} join( "\n", @{$opts{list}->get_ids} )."\n";
+		return;
+	}
+
+	return join( "\n", @{$opts{list}->get_ids} )."\n";
+}
+
+1;
