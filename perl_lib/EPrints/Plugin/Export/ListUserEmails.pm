13965661cec523e8d6a7e316bcf485ca0c7918ac
Christopher Gutteridge <cjg@ecs.soton.ac.uk>
2007-12-24 11:26:22 +0000

    * Removed bin/list_user_emails, replacing it with an export plugin to     perform the same action.
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@3174 9491667e-5006-0410-a446-efbe8990b998

---
+++ perl_lib/EPrints/Plugin/Export/ListUserEmails.pm
@@ -0,0 +1,41 @@
+package EPrints::Plugin::Export::ListUserEmails;
+
+use Unicode::String qw( utf8 );
+
+use EPrints::Plugin::Export;
+
+@ISA = ( "EPrints::Plugin::Export" );
+
+use strict;
+
+sub new
+{
+	my( $class, %params ) = @_;
+
+	my $self = $class->SUPER::new( %params );
+
+	$self->{name} = "List of Email Addresses";
+	$self->{accept} = [ 'dataobj/user', 'list/user' ];
+	$self->{visible} = "staff";
+	$self->{suffix} = "text";
+	$self->{mimetype} = "text/plain";
+
+	return $self;
+}
+
+
+sub output_dataobj
+{
+	my( $plugin, $dataobj ) = @_;
+
+	my $email = $dataobj->get_value( "email" );
+	if( !defined $email ) 
+	{
+		return "";
+	}
+
+	return "$email\n";
+}
+
+
+1;
