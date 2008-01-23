5a3d6b56a20f8f3821782c5a9eea4d51f06a42c6
Tim Brody <tdb2@ecs.soton.ac.uk>
2008-01-23 13:53:39 +0000

    * Added EPrints::CLIProcessor module for handling CLI messages, reverted previous changes to import scripts
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@3263 9491667e-5006-0410-a446-efbe8990b998

---
+++ perl_lib/EPrints/CLIProcessor.pm
@@ -0,0 +1,131 @@
+######################################################################
+#
+# EPrints::CLIProcessor
+#
+######################################################################
+#
+#  __COPYRIGHT__
+#
+# Copyright 2000-2008 University of Southampton. All Rights Reserved.
+# 
+#  __LICENSE__
+#
+######################################################################
+
+=head1 NAME
+
+EPrints::CLIProcessor - utility module supporting command-line scripts
+
+=head1 DESCRIPTION
+
+Currently this module is just a 'handler' for import scripts. In future it may do more things to support the CLI (Command-Line Interface) to EPrints.
+
+=head1 METHODS
+
+=over 4
+
+=cut
+
+package EPrints::CLIProcessor;
+
+use strict;
+
+use vars qw( %COLORS );
+
+eval "use Term::ANSIColor qw()";
+unless( $@ )
+{
+	%COLORS = (
+		reset => "reset",
+		error => "bold red",
+		warning => "bold yellow",
+	);
+}
+
+sub color
+{
+	my( $type ) = @_;
+
+	return exists $COLORS{$type} ? Term::ANSIColor::color( $COLORS{$type} ) : "";
+}
+
+=item $processor = EPrints::CLIProcessor->new( session => $session, %opts )
+
+Create a new processor object. Supported options:
+
+  scripted - backwards compatibility for import scripted interface
+
+=cut
+
+sub new
+{
+    my( $class, %self ) = @_;
+
+    $self{wrote} = 0;
+    $self{parsed} = 0;
+	$self{ids} = [];
+
+    bless \%self, $class;
+}
+
+=item $processor->add_message( TYPE, MESSAGE )
+
+Add a message for the user. TYPE is 'error', 'warning' or 'message'. MESSAGE is an XHTML fragment.
+
+=cut
+
+*add_message = \&message;
+sub message
+{
+    my( $self, $type, $msg ) = @_;
+
+	print STDERR color($type);
+	$msg = EPrints::Utils::tree_to_utf8( $msg );
+	print STDERR "\u$type! $msg\n";
+	print STDERR color('reset');
+}
+
+=item $processor->parsed( [ $epdata ] )
+
+Register a parsed event, optionally with $epdata.
+
+=cut
+
+sub parsed
+{
+    my( $self, $epdata ) = @_;
+
+    $self->{parsed}++;
+
+	if( $self->{scripted} )
+	{
+		print "EPRINTS_IMPORT: ITEM_PARSED\n";
+	}
+}
+
+=item $processor->object( DATASET, DATAOBJ )
+
+Register a new object event in DATASET with new object DATAOBJ.
+
+=cut
+
+sub object
+{
+    my( $self, $dataset, $dataobj ) = @_;
+
+    $self->{wrote}++;
+
+	push @{$self->{ids}}, $dataobj->get_id;
+
+	if( $self->{session}->get_noise > 1 )
+	{
+		print STDERR "Imported ".$dataset->id." ".$dataobj->get_id."\n";
+	}
+
+	if( $self->{scripted} )
+	{
+		print "EPRINTS_IMPORT: ITEM_IMPORTED ".$dataobj->get_id."\n";
+	}
+}
+
+1;
