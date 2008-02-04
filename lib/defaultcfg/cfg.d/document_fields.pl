45f105eb387ce747b18e78e3c1b1ed7df1a59cf4
Tim Brody <tdb2@ecs.soton.ac.uk>
2008-02-04 14:09:52 +0000

    * Example for adding document fields
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@3287 9491667e-5006-0410-a446-efbe8990b998

---
+++ lib/defaultcfg/cfg.d/document_fields.pl
@@ -0,0 +1,14 @@
+# By default there are no custom fields in document objects, but this file
+# provides you with an example should you wish to (c.f. eprint_fields.pl)
+
+#$c->{fields}->{document} = [
+#	{
+#		name => "application",
+#		type => "set",
+#		options => [
+#			'msword95',
+#			'msword2000',
+#			'msword2007',
+#		],
+#	},
+#];
