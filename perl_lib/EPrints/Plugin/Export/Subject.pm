4bfd191e2c4f54a73633bb2d725185ed1e3968eb
Tim Brody <tdb2@ecs.soton.ac.uk>
2008-05-07 11:58:14 +0000

    * Export Subjects in the same format as plain text
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@3565 9491667e-5006-0410-a446-efbe8990b998

---
+++ perl_lib/EPrints/Plugin/Export/Subject.pm
@@ -0,0 +1,49 @@
+package EPrints::Plugin::Export::Subject;
+
+use Unicode::String qw( utf8 );
+
+use EPrints::Plugin::Export;
+
+@ISA = ( "EPrints::Plugin::Export" );
+
+use strict;
+
+sub new
+{
+	my( $class, %opts ) = @_;
+
+	my $self = $class->SUPER::new( %opts );
+
+	$self->{name} = "Subjects file";
+	$self->{accept} = [ 'dataobj/subject', 'list/subject' ];
+	$self->{visible} = "all";
+	$self->{suffix} = ".txt";
+	$self->{mimetype} = "text/plain; charset=utf-8";
+	
+	return $self;
+}
+
+sub output_dataobj
+{
+	my( $self, $dataobj ) = @_;
+
+	my @parts;
+
+	push @parts, $dataobj->get_id;
+	my $names = $dataobj->get_value( "name_name" );
+	if( EPrints::Utils::is_set( $names ) )
+	{
+		push @parts, $names->[0];
+	}
+	else
+	{
+		push @parts, "";
+	}
+	my $parents = $dataobj->get_value( "parents" );
+	push @parts, join ",", @$parents;
+	push @parts, $dataobj->get_value( "depositable" ) ? "1" : "0";
+
+	return join(":", @parts)."\n";
+}
+
+1;
