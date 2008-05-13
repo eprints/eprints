4ce94e88d0ab278984ca478b0c95397a14365dc2
David Tarrant <davetaz@ecs.soton.ac.uk>
2008-05-13 16:38:56 +0000

    OAI-ORE Resource Map Export Plugin now updated to export an aggregation consisting of all export plugins and representations available. With added specification for each to contain a dc:conformsTo tag where there is data.
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@3590 9491667e-5006-0410-a446-efbe8990b998

--- perl_lib/EPrints/Plugin/Export/ResMap.pm
+++ perl_lib/EPrints/Plugin/Export/ResMap.pm
@@ -1,3 +1,43 @@
+######################################################################
+#
+# EPrints::Plugin::Export::ResMap
+#
+######################################################################
+#
+#  __COPYRIGHT__
+#
+# Copyright 2000-2008 University of Southampton. All Rights Reserved.
+# 
+#  __LICENSE__
+#
+######################################################################
+
+
+=pod
+
+=head1 NAME
+
+B<EPrints::Plugin::Export::ResMap> - OAI_ORE Resource Map Export/Aggregation Plugin.
+
+=head1 DESCRIPTION
+
+This export plugin is written to the early alpha-4 specification of OAI-ORE 
+(Open Access Initiative Object Reuse and Exchange). It exports a Resource Map 
+containing aggregations each of which represent a single EPrint and it's related 
+matadata. 
+
+Related to an EPrint are all the documents this EPrint contains as well as a list 
+of the possible representations and additional metadata available via ALL other 
+export plugins. 
+
+For ORE importers we have included the compulsary dc:conformsTo field in each objects
+description. This provides the namespace which any metadata related to an EPrint is 
+defined by.
+
+=over 4
+
+=cut
+
 package EPrints::Plugin::Export::ResMap;
 
 use EPrints::Plugin::Export;
@@ -25,9 +65,10 @@ sub output_list
 
 	my $type = $opts{list}->get_dataset->confid;
 	
+	my $part = "";
+	
 	my $r = [];
 
-	my $part = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n<rdf:RDF xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"\n\txmlns:ore=\"http://www.openarchives.org/ore/terms/\"\n\txmlns:dc=\"http://purl.org/dc/terms/\">\n";
 	if( defined $opts{fh} )
 	{
 		print {$opts{fh}} $part;
@@ -50,7 +91,6 @@ sub output_list
 		}
 	});
 
-	$part= "</rdf:RDF>\n";
 	if( defined $opts{fh} )
 	{
 		print {$opts{fh}} $part;
@@ -85,32 +125,34 @@ sub output_dataobj
 	my $base_url = $session->get_repository->get_conf("base_url");
 	my $archive_id = $session->get_repository->get_id;
 	
-	my $additional = "";	
-	my $head = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n<rdf:RDF xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"\n\txmlns:ore=\"http://www.openarchives.org/ore/terms/\"\n\txmlns:dc=\"http://purl.org/dc/terms/\">\n";
-	my $content = "\t<rdf:Description rdf:about=\"$resmap_url\">\n";
-	$content .= "\t\t<rdf:type rdf:resource=\"http://www.openarchives.org/ore/terms/ResourceMap\" />\n";
-	$content .= "\t\t<dc:modified rdf:datatype=\"http://www.w3.org/2001/XMLSchema#dateTime\">" . $lastmod . "</dc:modified>\n";
-	$content .= "\t\t<ore:describes rdf:resource=\"$resmap_url#aggregation\" />\n";
-	$content .= "\t</rdf:Description>\n";
+	my $response = $session->make_doc_fragment;
 
-	$content .= "\t<rdf:Description rdf:about=\"$resmap_url#aggregation\">\n";
-
-	##Metadata 
-	my $file_url = "$base_url/cgi/export/$eprint_id/XML/$archive_id-eprint-$eprint_id.xml";
-	$content .= "\t\t<ore:aggregates rdf:resource=\"$file_url\"/>\n";
-	$additional .= "\t<rdf:Description rdf:about=\"$file_url\">\n";
-	$additional .= "\t\t<dc:format>EP3_XML</dc:format>\n";
-	$additional .= "\t\t<dc:format>application/xml+EP3</dc:format>\n";
-	$additional .= "\t\t<dc:hasVersion>$eprint_rev</dc:hasVersion>\n";
-	$additional .= "\t</rdf:Description>\n";
+	my $topcontent = $session->make_element( "rdf:Description",
+		"rdf:about"=>"$resmap_url" );
 	
-	$file_url = "$base_url/cgi/export/$eprint_id/OAI_DC/$archive_id-eprint-$eprint_id.xml";
-	$content .= "\t\t<ore:aggregates rdf:resource=\"$file_url\"/>\n";
-	$additional .= "\t<rdf:Description rdf:about=\"$file_url\">\n";
-	$additional .= "\t\t<dc:format>OAI_DC</dc:format>\n";
-	$additional .= "\t\t<dc:format>text/xml</dc:format>\n";
-	$additional .= "\t\t<dc:conformsTo>http://www.openarchives.org/OAI/2.0/oai_dc/</dc:conformsTo>\n";
-	$additional .= "\t</rdf:Description>\n";
+	my $sub_content = $session->make_element ("rdf:type",
+		"rdf:resource"=>"http://www.openarchives.org/ore/terms/ResourceMap" );
+	
+	$topcontent->appendChild( $sub_content );
+	
+	$sub_content = $session->render_data_element (
+		4,
+		"dc:modified",
+		$lastmod,
+		"rdf:datatype"=>"http://www.w3.org/2001/XMLSchema#dateTime" );
+
+	$topcontent->appendChild( $sub_content);
+
+	$sub_content = $session->make_element ("ore:describes",
+		"rdf:resource"=>"$resmap_url#aggregation" );
+
+	$topcontent->appendChild( $sub_content );
+	
+	my $aggregation = $session->make_element( "rdf:Description",
+		"rdf:about"=>"$resmap_url#aggregation" );
+	
+	$response->appendChild( $topcontent );	
+	$response->appendChild( $aggregation );
 
 	my @docs = $dataobj->get_all_documents;
 	foreach my $doc (@docs)
@@ -121,28 +163,127 @@ sub output_dataobj
 		foreach my $key (keys %files)
 		{
 			my $fileurl = $doc->get_url($key);
-			$content .= "\t\t<ore:aggregates rdf:resource=\"$fileurl\" />\n";
-			$additional .= "\t<rdf:Description rdf:about=\"$fileurl\">\n";
-			$additional .= "\t\t<dc:format>$format</dc:format>\n";
-			$additional .= "\t\t<dc:hasVersion>$rev_number</dc:hasVersion>\n";	
-			$additional .= "\t</rdf:Description>\n";
+			$sub_content = $session->make_element("ore:aggregates",
+				"rdf:resource"=>"$fileurl" );
+			$aggregation->appendChild ( $sub_content );
+			my $additional = $session->make_element( "rdf:Description",
+				"rdf:about"=>"$fileurl" );
+			$sub_content = $session->render_data_element(
+				4,
+				"dc:format",
+				$format
+ 			);
+			$additional->appendChild( $sub_content );
+		
+			$sub_content = $session->render_data_element(
+				4,
+				"dc:hasVersion",
+				$rev_number
+			);
+			$additional->appendChild( $sub_content );
+			
+			$response->appendChild( $additional );	
 		}
 
 	}
-		
-	$content .= "\t</rdf:Description>\n";
+	
+	my @plugins = $session->plugin_list();
+	foreach my $plugin_name (@plugins) 
+	{
+		my $url = "$base_url/cgi/export/$eprint_id/";
+		my $string = substr($plugin_name,0,6);
+		if ($string eq "Export") 
+		{
+			my $plugin_id = $plugin_name;
+			$plugin_name = substr($plugin_id,8,length($plugin_id));
+			my $plugin_temp = $session->plugin($plugin_id);
+			my $plugin_suffix = $plugin_temp->param("suffix");
+			my $uri =  $plugin_temp->local_uri();
+			$uri =~ /Export/g;
+			my $plugin_location = substr($uri,pos($uri)+1,length($uri));
+			$url = $url."$plugin_location/$archive_id-eprint-$eprint_id$plugin_suffix";
 
-	my $tail = '</rdf:RDF>';
+			$sub_content = $session->make_element("ore:aggregates",
+				"rdf:resource"=>"$url" );
+			$aggregation->appendChild ( $sub_content );
+
+			my $dc_title = $plugin_temp->param("name") || "";
+			my $dc_format = $plugin_temp->param("mimetype") || "";
+			my $dc_conformsTo = $plugin_temp->param("xmlns") || "";
+			my $schema_location = $plugin_temp->param("schemaLocation") || "";
+			
+			my $additional = $session->make_element( "rdf:Description",
+				"rdf:about"=>"$url" );
+
+			if ( $dc_title ne "" )
+			{	
+				$sub_content = $session->render_data_element(
+					4,
+					"dc:title",
+					$dc_title
+	 			);
+				$additional->appendChild( $sub_content );
+			}
+			
+			if ( $dc_format ne "" )		
+			{
+				$sub_content = $session->render_data_element(
+					4,
+					"dc:format",
+					$dc_format
+ 				);
+				$additional->appendChild( $sub_content );
+			}
+			
+			if ( $dc_conformsTo ne "" ) 
+			{
+				$sub_content = $session->render_data_element(
+					4,
+					"dc:conformsTo",
+					$dc_conformsTo
+ 				);
+				$additional->appendChild( $sub_content );
+			}
+		
+			if ( $schema_location ne "") 
+			{
+				$sub_content = $session->render_data_element(
+					4,
+					"rdfs:comment",
+					$schema_location
+	 			);
+				$additional->appendChild( $sub_content );
+			}
+			$response->appendChild( $additional );
+		
+		}
+		
+	}	
+
+	EPrints::XML::tidy( $response );
+
+	my $resourceMap= EPrints::XML::to_string( $response );
+	EPrints::XML::dispose( $response );
 	
 	if( $single )
 	{
-		return $content . $additional;
+		return $resourceMap;
 	}
-	else
+	else 
 	{
-		return $head . $content . $additional . $tail;
+		return $plugin->rdf_header.$resourceMap.$plugin->rdf_footer;
 	}
 
+}
+
+sub rdf_header
+{
+	return "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n<rdf:RDF xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"\n\txmlns:rdfs=\"http://www.w3.org/2000/01/rdf-schema#\"\n\txmlns:ore=\"http://www.openarchives.org/ore/terms/\"\n\txmlns:dc=\"http://purl.org/dc/terms/\">\n";
+}
+
+sub rdf_footer
+{
+	return "</rdf:RDF>";
 }
 
 1;
