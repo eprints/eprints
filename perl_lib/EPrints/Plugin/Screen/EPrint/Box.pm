7640bc031126ea6d94416196e9b05d8846191187
Christopher Gutteridge <cjg@ecs.soton.ac.uk>
2008-05-16 10:34:20 +0000

    Example box plugin
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@3598 9491667e-5006-0410-a446-efbe8990b998

---
+++ perl_lib/EPrints/Plugin/Screen/EPrint/Box.pm
@@ -0,0 +1,39 @@
+package EPrints::Plugin::Screen::EPrint::Box;
+
+our @ISA = ( 'EPrints::Plugin::Screen' );
+
+use strict;
+
+sub new
+{
+	my( $class, %params ) = @_;
+
+	my $self = $class->SUPER::new(%params);
+
+	# Register sub-classes but not this actual class.
+	if( $class ne "EPrints::Plugin::Screen::EPrint::Box" )
+	{
+		$self->{appears} = [
+			{
+				place => "summary_right",
+				position => 1000,
+			},
+		];
+	}
+
+	return $self;
+}
+
+sub render_collapsed { return 0; }
+
+sub can_be_viewed { return 1; }
+
+sub render
+{
+	my( $self ) = @_;
+
+	return $self->{session}->make_text( "Please add a 'render' method to this box!" );
+}
+
+1;
+
