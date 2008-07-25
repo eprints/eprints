2992bb779b13911f2b506e4d0159d4b5c1f8a737
Tim Brody <tdb2@ecs.soton.ac.uk>
2008-07-25 11:39:52 +0000

    * Added 'cron' script to generate flash video previews offline  * Video thumbnail module for generating flash-video previews
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@3748 9491667e-5006-0410-a446-efbe8990b998

--- perl_lib/EPrints/Plugin/Convert/ThumbnailVideos.pm
+++ perl_lib/EPrints/Plugin/Convert/ThumbnailVideos.pm
@@ -43,6 +43,12 @@ for(my $i = 0; $i < @ORDERED; $i+=2)
 }
 our $EXTENSIONS_RE = join '|', keys %FORMATS;
 
+our $GEOMETRY = {
+	small=>"66x50",
+	medium=>"200x150",
+	preview=>"400x300"
+};
+
 sub new
 {
 	my( $class, %opts ) = @_;
@@ -51,6 +57,9 @@ sub new
 
 	$self->{name} = "Thumbnail Videos";
 	$self->{visible} = "all";
+	$self->{ffmpeg} = defined($self->{session}) ?
+		$self->get_repository->get_conf( "executables", "ffmpeg" ) :
+		undef;
 
 	return $self;
 }
@@ -59,7 +68,7 @@ sub can_convert
 {
 	my ($plugin, $doc) = @_;
 
-	my $ffmpeg = $plugin->get_repository->get_conf( 'executables', 'ffmpeg' ) or return ();
+	return unless $plugin->{ffmpeg};
 
 	my %types;
 
@@ -71,6 +80,7 @@ sub can_convert
 		$types{"thumbnail_small"} = { plugin => $plugin, };
 		$types{"thumbnail_medium"} = { plugin => $plugin, };
 		$types{"thumbnail_preview"} = { plugin => $plugin, };
+		$types{"thumbnail_video"} = { plugin => $plugin, };
 	}
 
 	return %types;
@@ -82,28 +92,86 @@ sub export
 {
 	my ( $plugin, $dir, $doc, $type ) = @_;
 
-	my $ffmpeg = $plugin->get_repository->get_conf( 'executables', 'ffmpeg' ) or return ();
+	return unless $plugin->{ffmpeg};
 
-	my $src = $doc->get_stored_files( "data", $doc->get_main );
-	$src = $src->get_local_copy();
+	my( $size ) = $type =~ m/^thumbnail_(.*)$/;
+	if( !defined( $size ) )
+	{
+		return ();
+	}
+	elsif( $size eq "video" )
+	{
+		return $plugin->export_flv( $dir, $doc, $type, $size );
+	}
+	elsif( exists( $GEOMETRY->{$size} ) )
+	{
+		# Export the cell at 5 seconds
+		return $plugin->export_cell( $dir, $doc, $type, "00:00:05" );
+	}
+	else
+	{
+		return ();
+	}
+}
 
-	$type =~ m/^thumbnail_(.*)$/;
-	my $size = $1;
-	return () unless defined $size;
-	my $geom = { small=>"66x50", medium=>"200x150",preview=>"400x300" }->{$1};
-	return () unless defined $geom;
+sub export_cell
+{
+	my( $self, $dir, $doc, $type, $offset ) = @_;
 	
+	my $ffmpeg = $self->{ffmpeg};
+
+	my( $size ) = $type =~ /^thumbnail_(.+)$/;
+
+	# As we may be attempting this twice, don't attempt to copy the file
+	# twice!
+	my $src = $doc->get_stored_files( "data", $doc->get_main );
+	$src = $self->{_local_copy} ||= $src->get_local_copy();
+
 	my $fn = "$size.png";
 
-	system($ffmpeg." -y -i ".quotemeta($src)." -vcodec png -vframes 1 -an -f rawvideo -ss 00:00:02 -s ".$geom." ".quotemeta($dir.'/'.$fn)." 2>/dev/null");
+	my $geom = $GEOMETRY->{$size};
 
-	return () unless( -e "$dir/$fn" && -s "$dir/$fn" );
+	my $cmd = "$ffmpeg -y -i ".quotemeta($src)." -vcodec png -vframes 1 -an -f rawvideo -ss $offset -s $geom ".quotemeta($dir.'/'.$fn)." 2>/dev/null";
+	system($cmd);
+
+	if( not -s "$dir/$fn" )
+	{
+		if( $offset eq "00:00:00" )
+		{
+			delete $self->{_local_copy};
+			return ();
+		}
+		else
+		{
+			return $self->export_cell( $dir, $doc, $type, "00:00:00" );
+		}
+	}
 
 	# if output file does not exist it could be that the video is VERY short (<2secs), in this case-> need to gen the thumbnails from 0sec
 	#actually in that case, the file exists, but its size = 0
 	
 	EPrints::Utils::chown_for_eprints( "$dir/$fn" );
 	
+	delete $self->{_local_copy};
+
+	return ($fn);
+}
+
+sub export_flv
+{
+	my( $self, $dir, $doc, $type ) = @_;
+
+	my $ffmpeg = $self->{ffmpeg};
+
+	my $src = $doc->get_stored_files( "data", $doc->get_main );
+	$src = $src->get_local_copy;
+
+	my $fn = "video_preview.flv";
+
+	system("$ffmpeg -y -i ".quotemeta($src)." -acodec mp3 -ac 2 -ar 22050 -ab 32 -f flv -r 10.00 -s 320x240 $dir/$fn 2>/dev/null");
+
+	EPrints::Utils::chown_for_eprints( "$dir/$fn" );
+
 	return ($fn);
 }
 
