95ee099eddda0d6c6ad989ab9ea7178cee969fe5
Tim Brody <tdb2@ecs.soton.ac.uk>
2008-08-15 15:43:10 +0000

    * Use an absolute URL for document icons (will complain under HTTPS)
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@3797 9491667e-5006-0410-a446-efbe8990b998

--- perl_lib/EPrints/DataObj/Document.pm
+++ perl_lib/EPrints/DataObj/Document.pm
@@ -1569,7 +1569,7 @@ sub icon_url
 	my $type = $self->get_value( "format" );
 	$type =~ s/\//_/g;
 
-	return $self->{session}->get_repository->get_conf( "rel_path" ).
+	return $self->{session}->get_repository->get_conf( "http_url" ).
 			"/style/images/fileicons/$type.png";
 }
 
