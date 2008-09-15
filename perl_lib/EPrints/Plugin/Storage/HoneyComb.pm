5c337de2e05d1e8e2593393c2088724f7edb8c9b
Tim Brody <tdb2@ecs.soton.ac.uk>
2008-09-15 11:59:02 +0000

    * Draft HoneyComb storage plugin
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@3841 9491667e-5006-0410-a446-efbe8990b998

---
+++ perl_lib/EPrints/Plugin/Storage/HoneyComb.pm
@@ -0,0 +1,103 @@
+=head1 NAME
+
+EPrints::Plugin::Storage::HoneyComb - storage in a Sun StorageTek 5800
+
+=head1 DESCRIPTION
+
+See L<EPrints::Plugin::Storage> for available methods.
+
+=head1 METHODS
+
+=over 4
+
+=cut
+
+package EPrints::Plugin::Storage::HoneyComb;
+
+use URI;
+use URI::Escape;
+use Net::Sun::HoneyComb;
+
+use EPrints::Plugin::Storage;
+
+@ISA = ( "EPrints::Plugin::Storage" );
+
+use strict;
+
+sub new
+{
+	my( $class, %params ) = @_;
+
+	my $self = $class->SUPER::new( %params );
+
+	$self->{name} = "HoneyComb storage";
+	if( $params{session} )
+	{
+		$self->{honey} = Net::Sun::HoneyComb->new(
+			"hc-data",
+			8080
+		);
+	}
+
+	return $self;
+}
+
+sub store
+{
+	my( $self, $fileobj, $fh ) = @_;
+
+	my $length = 0;
+
+	use bytes;
+	use integer;
+
+	my $oid = $self->{honey}->store_both( sub {
+			my( $ctx, $n ) = @_;
+			sysread($ctx, my $buffer, $n);
+			$length += length($buffer);
+			return $buffer;
+		},
+		$fh,
+		{}
+	);
+
+	$fileobj->set_plugin_copy( $self, $oid );
+
+	return $length;
+}
+
+sub retrieve
+{
+	my( $self, $fileobj, $revision ) = @_;
+
+	my $oid = $fileobj->get_plugin_copy( $self );
+
+	my $fh = File::Temp->new();
+
+	binmode($fh);
+	$self->{honey}->retrieve_fh( $fh, $oid );
+
+	seek($fh, 0, 0);
+
+# This clobbers temp files/directories due to fork()/exit()
+#	my $fh = $self->{honey}->get_fh( $oid );
+
+	return $fh;
+}
+
+sub delete
+{
+	my( $self, $fileobj, $revision ) = @_;
+
+	my $oid = $fileobj->get_plugin_copy( $self );
+
+	my $ok = $self->{honey}->delete( $oid );
+
+	return $ok;
+}
+
+=back
+
+=cut
+
+1;
