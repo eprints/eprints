8abf2f5caaf02b5255370c988556d29e396a5ea9
Christopher Gutteridge <cjg@ecs.soton.ac.uk>
2008-10-29 12:57:13 +0000

    Made changes to mspace to do cleverer stuff for date & times, support document info, and more info about eprints themselves.
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@3888 9491667e-5006-0410-a446-efbe8990b998

--- tools/mspace/EPrints::MSpace.pm
+++ tools/mspace/EPrints::MSpace.pm
@@ -13,7 +13,7 @@ sub add_eprint
 
 	my $session = $eprint->get_session;
 	my $dataset = $eprint->get_dataset;
-	my $id = $eprint->get_id;
+	my $eprintid = $eprint->get_id;
 	my $db = $session->get_database;
 
 	FIELD: foreach my $field ( $dataset->get_fields )
@@ -23,34 +23,89 @@ sub add_eprint
 		my $table_name = "mspace_".$field->{name};
  		next FIELD if !$eprint->is_set( $field->get_name );
 		my $v = $eprint->get_value( $field->get_name );
+		my $add_fn = \&EPrints::MSpace::add_row;
+		if( $field->is_type( "date", "time") )
+		{
+			$add_fn = \&EPrints::MSpace::add_date_rows;
+		}
 		if( $field->get_property( "multiple" ) )
 		{
 			foreach my $s ( @{$v} )
 			{
-				my $desc = $field->render_value_no_multiple( $session, $s, 0, 1, $eprint );
-				my $vid = $field->get_id_from_value( $session, $s );
-				EPrints::MSpace::add_row( $db, $table_name, $id, $vid, $desc );
+				&{$add_fn}( $db, $table_name, $eprintid, $s, $eprint, $field );
 			}	
 		}
 		else
 		{
-			my $desc = $field->render_value_no_multiple( $session, $v, 0, 1, $eprint );
-			my $vid = $field->get_id_from_value( $session, $v );
-			EPrints::MSpace::add_row( $db, $table_name, $id, $vid, $desc );
+			&{$add_fn}( $db, $table_name, $eprintid, $v, $eprint, $field );
 		}
 	}
 
 	my $citation = EPrints::XML::to_string( $eprint->render_citation_link, "utf-8", 1 );
-	$db->insert( 'mspace_citation', ['eprintid','citation'], [$id,$citation] );
+	my $short = EPrints::Utils::tree_to_utf8( $eprint->render_citation('brief') );
+	my $url = $eprint->get_url;
+	$db->insert( 'mspace_citation', ['eprintid','citation','short','url'], [$eprintid,$citation,$short,$url] );
+
+	my @docs = $eprint->get_all_documents;
+	foreach my $doc ( @docs )
+	{
+		my @k = ();
+		my @v = ();
+
+		push @k, "eprintid";
+		push @v, $eprintid;
+	
+		push @k, "documentid";
+		push @v, $doc->get_id;
+		
+		push @k, "formatid";
+		push @v, $doc->get_value( "format" );
+		
+		push @k, "formatdesc";
+		push @v, EPrints::Utils::tree_to_utf8( $doc->render_value( "format" ) );
+		
+		push @k, "contentid";
+		push @v, $doc->get_value( "content" );
+		
+		push @k, "contentdesc";
+		push @v, EPrints::Utils::tree_to_utf8( $doc->render_value( "content" ) );
+		
+		push @k, "url";
+		push @v, $doc->get_url;
+		
+		push @k, "iconurl";
+		push @v, $doc->icon_url;
+	
+		$db->insert( 'mspace_documents', \@k, \@v );
+	}
+
+
 }
+
+sub add_date_rows
+{
+	my( $db, $table_name, $eprintid, $value, $eprint, $field ) = @_;
+
+	add_row( $db, $table_name, $eprintid, $value, $eprint, $field );
+	add_row( $db, $table_name."_day", $eprintid, substr( $value, 0, 10 ), $eprint, $field );
+	add_row( $db, $table_name."_month", $eprintid, substr( $value, 0, 6 ), $eprint, $field );
+	add_row( $db, $table_name."_year", $eprintid, substr( $value, 0, 4 ), $eprint, $field );
+	my $decade = substr( $value, 0, 3 )."0s";
+	$db->insert( $table_name."_decade", ['eprintid','value','desc'], [$eprintid,$decade,$decade] );
+}
+
 
 sub add_row
 {
-	my( $db, $table_name, $id, $value, $rendered_xml ) = @_;
+	my( $db, $table_name, $eprintid, $value, $eprint, $field ) = @_;
 
-	my $desc = EPrints::XML::to_string( $rendered_xml, "utf-8", 1 );
+	my $session = $eprint->get_session;
+	my $desc_xml = $field->render_value_no_multiple( $session, $value, 0, 1, $eprint );
+	my $vid = $field->get_id_from_value( $session, $value );
 
-	$db->insert( $table_name, ['eprintid','value','desc'], [$id,$value,$desc] );
+	my $desc = EPrints::Utils::tree_to_utf8( $desc_xml );
+
+	$db->insert( $table_name, ['eprintid','value','desc'], [$eprintid,$vid,$desc] );
 }
 
 sub remove_eprint
@@ -59,7 +114,7 @@ sub remove_eprint
 
 	my $session = $eprint->get_session;
 	my $dataset = $eprint->get_dataset;
-	my $id = $eprint->get_id;
+	my $eprintid = $eprint->get_id;
 	my $db = $session->get_database;
 
 	FIELD: foreach my $field ( $dataset->get_fields )
@@ -67,9 +122,18 @@ sub remove_eprint
 		next if $field->is_virtual;
 
 		my $table_name = "mspace_".$field->{name};
-		$db->delete_from( $table_name, ['eprintid'], [$id] )
+		$db->delete_from( $table_name, ['eprintid'], [$eprintid] );
+		if( $field->is_type( "date", "time" ) )
+		{
+			$db->delete_from( $table_name."_day", ['eprintid'], [$eprintid] );
+			$db->delete_from( $table_name."_month", ['eprintid'], [$eprintid] );
+			$db->delete_from( $table_name."_year", ['eprintid'], [$eprintid] );
+			$db->delete_from( $table_name."_decade", ['eprintid'], [$eprintid] );
+		}
+		
 	}
-	$db->delete_from( 'mspace_citation', ['eprintid'], [$id] )
+	$db->delete_from( 'mspace_citation', ['eprintid'], [$eprintid] );
+	$db->delete_from( 'mspace_documents', ['eprintid'], [$eprintid] );
 }
 
 1;
