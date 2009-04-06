e82409ba76473cec6351c3b045e8883760ac9789
Tim Brody <tdb2@ecs.soton.ac.uk>
2009-04-06 11:43:33 +0000

    * Reverted [4159]  * Index complete emails in the indexer to allow correct search-by-email
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@4162 9491667e-5006-0410-a446-efbe8990b998

--- perl_lib/EPrints/MetaField/Email.pm
+++ perl_lib/EPrints/MetaField/Email.pm
@@ -31,51 +31,14 @@ package EPrints::MetaField::Email;
 use strict;
 use warnings;
 
+BEGIN
+{
+	our( @ISA );
+
+	@ISA = qw( EPrints::MetaField::Text );
+}
+
 use EPrints::MetaField::Text;
-our @ISA = qw( EPrints::MetaField::Text );
-
-sub get_property_defaults
-{
-	my( $self ) = @_;
-	my %defaults = $self->SUPER::get_property_defaults;
-	$defaults{text_index} = 0;
-	$defaults{sql_index} = 1;
-	return %defaults;
-}
-
-sub render_search_value
-{
-	my( $self, $session, $value ) = @_;
-
-	my $valuedesc = $session->make_doc_fragment;
-	$valuedesc->appendChild( $session->make_text( '"' ) );
-	$valuedesc->appendChild( $session->make_text( $value ) );
-	$valuedesc->appendChild( $session->make_text( '"' ) );
-
-	return $valuedesc;
-}
-
-sub from_search_form
-{
-	my( $self, $session, $basename ) = @_;
-
-	# complex text types
-
-	my $val = $session->param( $basename );
-	return unless defined $val;
-
-	my $search_type = $session->param( $basename."_merge" );
-	my $search_match = $session->param( $basename."_match" );
-		
-	# Default search type if none supplied (to allow searches 
-	# using simple HTTP GETs)
-	$search_type = "ALL" unless defined( $search_type );
-	$search_match = "EX" unless defined( $search_match );
-		
-	return unless( defined $val );
-
-	return( $val, $search_type, $search_match );	
-}		
 
 sub render_single_value
 {
@@ -89,6 +52,50 @@ sub render_single_value
 	my $a = $session->render_link( "mailto:".$value );
 	$a->appendChild( $text );
 	return $a;
+}
+
+sub get_index_codes
+{
+       my( $self, $session, $value ) = @_;
+
+       if( !$self->get_property( "multiple" ) )
+       {
+               return( [ $value ], [], [] );
+       }
+       return( $value, [], [] );
+}
+
+sub get_search_conditions_not_ex
+{
+       my( $self, $session, $dataset, $search_value, $match, $merge,
+               $search_mode ) = @_;
+       
+       if( $match eq "EQ" )
+       {
+               return EPrints::Search::Condition->new( 
+                       '=', 
+                       $dataset,
+                       $self, 
+                       $search_value );
+       }
+
+       return EPrints::Search::Condition->new( 
+                       'index',
+                       $dataset,
+                       $self, 
+                       $search_value );
+}
+
+sub render_search_value
+{
+       my( $self, $session, $value ) = @_;
+
+       my $valuedesc = $session->make_doc_fragment;
+       $valuedesc->appendChild( $session->make_text( '"' ) );
+       $valuedesc->appendChild( $session->make_text( $value ) );
+       $valuedesc->appendChild( $session->make_text( '"' ) );
+
+       return $valuedesc;
 }
 
 ######################################################################
