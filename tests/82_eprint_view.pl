9dc43095500296c1259cefe412017d81fea6fcd4
Tim Brody <tdb2@ecs.soton.ac.uk>
2009-04-17 13:22:39 +0000

    * Unit test for profiling EPrint::View::Editor screen
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@4221 9491667e-5006-0410-a446-efbe8990b998

---
+++ tests/82_eprint_view.pl
@@ -0,0 +1,35 @@
+use strict;
+use Test::More tests => 4;
+
+BEGIN { use_ok( "EPrints" ); }
+BEGIN { use_ok( "EPrints::Test" ); }
+BEGIN { use_ok( "EPrints::ScreenProcessor" ); }
+
+my $session = EPrints::Test::get_test_session();
+
+# find an example eprint
+my $dataset = $session->get_repository->get_dataset( "eprint" );
+my( $eprintid ) = @{ $dataset->get_item_ids( $session ) };
+
+$session = EPrints::Test::OnlineSession->new( $session, {
+	method => "GET",
+	path => "/cgi/users/home",
+	username => "admin",
+	query => {
+		screen => "EPrint::View::Editor",
+		eprintid => $eprintid,
+	},
+});
+
+EPrints::ScreenProcessor->process(
+	session => $session,
+	url => $session->get_repository->get_conf( "base_url" ) . "/cgi/users/home"
+	);
+
+$session->terminate;
+
+my $content = $session->test_get_stdout();
+
+#diag($content);
+
+ok(1);
