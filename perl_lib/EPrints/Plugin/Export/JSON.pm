7de5711e3190d5896df231ff18ee9892b1e9848b
Tim Brody <tdb2@ecs.soton.ac.uk>
2009-05-13 15:27:59 +0000

    * Removed Unicode::String include  * Things exporting plain text utf-8 should inherit from Export::TextFile
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@4299 9491667e-5006-0410-a446-efbe8990b998

--- perl_lib/EPrints/Plugin/Export/JSON.pm
+++ perl_lib/EPrints/Plugin/Export/JSON.pm
@@ -1,17 +1,10 @@
 package EPrints::Plugin::Export::JSON;
 
-use Unicode::String qw( utf8 );
+use EPrints::Plugin::Export::TextFile;
 
-use EPrints::Plugin::Export;
-
-@ISA = ( "EPrints::Plugin::Export" );
+@ISA = ( "EPrints::Plugin::Export::TextFile" );
 
 use strict;
-
-# The utf8() method is called to ensure that
-# any broken characters are removed. There should
-# not be any broken characters, but better to be
-# sure.
 
 sub new
 {
