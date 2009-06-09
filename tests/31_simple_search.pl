d512ca43fa8ceaedb68a2176df1e6c102da4f379
Tim Brody <tdb2@ecs.soton.ac.uk>
2009-06-09 08:58:24 +0000

    * Simple search unit test example (doesn't actually check anything)
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@4333 9491667e-5006-0410-a446-efbe8990b998

---
+++ tests/31_simple_search.pl
@@ -0,0 +1,36 @@
+use strict;
+use Test::More tests => 4;
+
+BEGIN { use_ok( "EPrints" ); }
+BEGIN { use_ok( "EPrints::Test" ); }
+BEGIN { use_ok( "EPrints::ScreenProcessor" ); }
+
+my $session = EPrints::Test::get_test_session();
+
+# find an example eprint
+my $dataset = $session->get_repository->get_dataset( "eprint" );
+my( $eprintid ) = @{ $dataset->get_item_ids( $session ) };
+
+$session = EPrints::Test::OnlineSession->new( $session, {
+	method => "GET",
+	path => "/cgi/search/simple",
+	query => {
+		q => "demonstration",
+		_action_search => "Search",
+		_order => "bytitle",
+	},
+});
+
+my $sconf = $session->get_repository->get_conf( "search", "simple" );
+
+EPrints::ScreenProcessor->process( 
+	session => $session, 
+	url => $session->get_repository->get_conf( "perl_url" )."/search/simple",
+	sconf => $sconf,
+	template => $sconf->{template},
+	screenid => "Public::EPrintSearch",
+);
+
+#print STDERR $session->test_get_stdout;
+
+ok(1);
