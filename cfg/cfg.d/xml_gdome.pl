e730d99f104416b3c9dd856352ccd81006efca3c
Tim Brody <tdb2@ecs.soton.ac.uk>
2009-06-18 14:41:59 +0000

    * Reduced XML::LibXML version to 1.63  * If these settings are already defined, don't override them
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@4368 9491667e-5006-0410-a446-efbe8990b998

--- cfg/cfg.d/xml_gdome.pl
+++ cfg/cfg.d/xml_gdome.pl
@@ -1,4 +1,7 @@
-eval "use XML::GDOME";
+if( !defined $c->{enable_gdome} )
+{
+	eval "use XML::GDOME";
 
-# set this to 0 to completely disable XML::GDOME
-$c->{enable_gdome} = $@ ? 0 : 1;
+	# set this to 0 to completely disable XML::GDOME
+	$c->{enable_gdome} = $@ ? 0 : 1;
+}
--- cfg/cfg.d/xml_libxml.pl
+++ cfg/cfg.d/xml_libxml.pl
@@ -1,4 +1,7 @@
-eval "use XML::LibXML 1.66";
+if( !defined $c->{enable_libxml} )
+{
+	eval "use XML::LibXML 1.63";
 
-# set this to 0 to completely disable XML::LibXML
-$c->{enable_libxml} = $@ ? 0 : 1;
+	# set this to 0 to completely disable XML::LibXML
+	$c->{enable_libxml} = $@ ? 0 : 1;
+}
