848c807efaaae7d0dbda3723f61aa5accf7ca825
Tim Brody <tdb2@ecs.soton.ac.uk>
2009-08-11 15:22:49 +0000

    * Work-in-progress: replace Yapp parser with a smaller memory footprint parser
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@4434 9491667e-5006-0410-a446-efbe8990b998

---
+++ perl_lib/BibTeX/Parser.pm
@@ -0,0 +1,271 @@
+package BibTeX::Parser;
+
+use warnings;
+use strict;
+
+our $VERSION = '0.3';
+
+use BibTeX::Parser::Entry;
+
+=for stopwords jr von
+
+=head1 NAME
+
+BibTeX::Parser - A pure perl BibTeX parser
+
+=head1 VERSION
+
+version 0.3.1
+
+=cut
+
+my $re_namechar = qr/[a-zA-Z0-9\!\$\&\*\+\-\.\/\:\;\<\>\?\[\]\^\_\`\|]/o;
+my $re_name     = qr/$re_namechar+/o;
+
+=head1 SYNOPSIS
+
+Parses BibTeX files.
+
+    use BibTeX::Parser;
+	use IO::File;
+
+    my $fh     = IO::File->new("filename");
+
+    # Create parser object ...
+    my $parser = BibTeX::Parser->new($fh);
+    
+    # ... and iterate over entries
+    while (my $entry = $parser->next ) {
+	    if ($entry->parse_ok) {
+		    my $type    = $entry->type;
+		    my $title   = $entry->field("title");
+
+		    my @authors = $entry->author;
+		    # or:
+		    my @editors = $entry->editor;
+		    
+		    foreach my $author (@authors) {
+			    print $author->first . " " 
+			    	. $author->von . " " 
+				. $author->last . ", "
+				. $author->jr;
+		    }
+	    } else {
+		    warn "Error parsing file: " . $entry->error;
+	    }
+    }
+
+
+=head1 FUNCTIONS
+
+=head2 new
+
+Creates new parser object. 
+
+Parameters:
+
+	* fh: A filehandle
+
+=cut
+
+sub new {
+    my ( $class, $fh ) = @_;
+
+    return bless {
+        fh      => $fh,
+        strings => {
+            jan => "January",
+            feb => "February",
+            mar => "March",
+            apr => "April",
+            may => "May",
+            jun => "June",
+            jul => "July",
+            aug => "August",
+            sep => "September",
+            oct => "October",
+            nov => "November",
+            dec => "December",
+
+        },
+        line   => -1,
+        buffer => "",
+    }, $class;
+}
+
+sub _slurp_close_bracket;
+
+sub _parse_next {
+    my $self = shift;
+
+    while (1) {    # loop until regular entry is finished
+        return 0 if $self->{fh}->eof;
+        local $_ = $self->{buffer};
+
+        until (/@/m) {
+            my $line = $self->{fh}->getline;
+            return 0 unless defined $line;
+            $_ .= $line;
+        }
+
+        my $current_entry = new BibTeX::Parser::Entry;
+        if (/@($re_name)/cgo) {
+			my $type = uc $1;
+            $current_entry->type( $type );
+            my $start_pos = pos($_) - length($type) - 1;
+
+            # read rest of entry (matches braces)
+            my $bracelevel = 0;
+            $bracelevel += tr/\{/\{/;    #count braces
+            $bracelevel -= tr/\}/\}/;
+            while ( $bracelevel != 0 ) {
+                my $position = pos($_);
+                my $line     = $self->{fh}->getline;
+				last unless defined $line;
+                $bracelevel =
+                  $bracelevel + ( $line =~ tr/\{/\{/ ) - ( $line =~ tr/\}/\}/ );
+                $_ .= $line;
+                pos($_) = $position;
+            }
+
+            # Remember raw bibtex code
+            my $raw = substr($_, $start_pos);
+            $raw =~ s/^\s+//;
+            $raw =~ s/\s+$//;
+            $current_entry->raw_bibtex($raw);
+
+            my $pos = pos $_;
+            tr/\n/ /;
+            pos($_) = $pos;
+
+            if ( $type eq "STRING" ) {
+                if (/\G{\s*($re_name)\s*=\s*/cgo) {
+                    my $key   = $1;
+                    my $value = _parse_string( $self->{strings} );
+                    if ( defined $self->{strings}->{$key} ) {
+                        warn("Redefining string $key!");
+                    }
+                    $self->{strings}->{$key} = $value;
+                    /\G[\s\n]*\}/cg;
+                } else {
+                    $current_entry->error("Malformed string!");
+					return $current_entry;
+                }
+            } elsif ( $type eq "COMMENT" or $type eq "PREAMBLE" ) {
+                /\G\{./cgo;
+                _slurp_close_bracket;
+            } else {    # normal entry
+                $current_entry->parse_ok(1);
+
+				# parse key
+                if (/\G\{\s*($re_name),[\s\n]*/cgo) {
+                    $current_entry->key($1);
+
+					# fields
+                    while (/\G[\s\n]*($re_name)[\s\n]*=[\s\n]*/cgo) {
+                        $current_entry->field(
+                                      $1 => _parse_string( $self->{strings} ) );
+                        my $idx = index( $_, ',', pos($_) );
+                        pos($_) = $idx + 1 if $idx > 0;
+                    }
+
+                    return $current_entry;
+
+                } else {
+
+                    $current_entry->error("Malformed entry (key contains illegal characters) at " . substr($_, pos($_) || 0, 20)  . ", ignoring");
+                    _slurp_close_bracket;
+					return $current_entry;
+                }
+            }
+
+            $self->{buffer} = substr $_, pos($_);
+
+        } else {
+            $current_entry->error("Did not find type at " . substr($_, pos($_) || 0, 20)); 
+			return $current_entry;
+        }
+
+    }
+}
+
+=head2 next
+
+Returns the next parsed entry or undef.
+
+=cut
+
+sub next {
+    my $self = shift;
+
+    return $self->_parse_next;
+}
+
+# slurp everything till the next closing brace. Handels
+# nested brackets
+sub _slurp_close_bracket {
+    my $bracelevel = 0;
+  BRACE: {
+        /\G[^\}]*\{/cg && do { $bracelevel++; redo BRACE };
+        /\G[^\{]*\}/cg
+          && do {
+            if ( $bracelevel > 0 ) {
+                $bracelevel--;
+                redo BRACE;
+            } else {
+                return;
+            }
+          }
+    }
+}
+
+# parse bibtex string in $_ and return. A BibTeX string is either enclosed
+# in double quotes '"' or matching braces '{}'. The braced form may contain
+# nested braces.
+sub _parse_string {
+    my $strings_ref = shift;
+
+    my $value = "";
+
+  PART: {
+        if (/\G(\d+)/cg) {
+            $value .= $1;
+        } elsif (/\G($re_name)/cgo) {
+            warn("Using undefined string $1") unless defined $strings_ref->{$1};
+            $value .= $strings_ref->{$1} || "";
+        } elsif (/\G"(([^"\\]*(\\.)*[^\\"]*)*)"/cgs)
+        {    # quoted string with embeded escapes
+            $value .= $1;
+        } else {
+            my $part = _extract_bracketed( $_ );
+            $value .= substr $part, 1, length($part) - 2;    # strip quotes
+        }
+
+        if (/\G\s*#\s*/cg) {    # string concatenation by #
+            redo PART;
+        }
+    }
+    $value =~ s/[\s\n]+/ /g;
+    return $value;
+}
+
+sub _extract_bracketed
+{
+	for($_[0]) # alias to $_
+	{
+		/\G\s+/cg;
+		my $start = pos($_);
+		my $depth = 0;
+		while(1)
+		{
+			/\G\\./cg && next;
+			/\G\{/cg && (++$depth, next);
+			/\G\}/cg && (--$depth > 0 ? next : last);
+			/\G([^\\\{\}]+)/cg && next; 
+			last; # end of string
+		}
+		return substr($_, $start, pos($_)-$start);
+	}
+}
+
+1;    # End of BibTeX::Parser
---
+++ perl_lib/BibTeX/Parser/Author.pm
@@ -0,0 +1,188 @@
+package BibTeX::Parser::Author;
+
+use warnings;
+use strict;
+
+our $VERSION = '0.3';
+
+use overload
+	'""' => \&to_string;
+
+=head1 NAME
+
+BibTeX::Author - Contains a single author for a BibTeX document.
+
+=head1 VERSION
+
+version 0.3.1
+
+=cut
+
+=head1 SYNOPSIS
+
+This class ist a wrapper for a single BibTeX author. It is usually created
+by a BibTeX::Parser.
+
+
+    use BibTeX::Parser::Author;
+
+    my $entry = BibTeX::Parser::Author->new($full_name);
+    
+    my $firstname = $author->first;
+    my $von	  = $author->von;
+    my $last      = $author->last;
+    my $jr	  = $author->jr;
+
+    # or ...
+    
+    my ($first, $von, $last, $jr) = BibTeX::Author->split($fullname);
+
+
+=head1 FUNCTIONS
+
+=head2 new
+
+Create new author object. Expects full name as parameter.
+
+=cut
+
+sub new {
+	my $class = shift;
+
+	if (@_) {
+		my $self = [ $class->split(@_) ];
+		return bless $self, $class;
+	} else {
+		return bless [], $class;
+	}
+}
+
+sub _get_or_set_field {
+	my ($self, $field, $value) = @_;
+	if (defined $value) {
+		$self->[$field] = $value;
+	} else {
+		return $self->[$field];
+	}
+}
+
+=head2 first
+
+Set or get first name(s).
+
+=cut
+
+sub first {
+	shift->_get_or_set_field(0, @_);
+}
+
+=head2 von
+
+Set or get 'von' part of name.
+
+=cut
+
+sub von {
+	shift->_get_or_set_field(1, @_);
+}
+
+=head2 last
+
+Set or get last name(s).
+
+=cut
+
+sub last {
+	shift->_get_or_set_field(2, @_);
+}
+
+=head2 jr
+
+Set or get 'jr' part of name.
+
+=cut
+
+sub jr {
+	shift->_get_or_set_field(3, @_);
+}
+
+=head2 split
+
+Split name into (firstname, von part, last name, jr part). Returns array
+with four strings, some of them possibly empty.
+
+=cut
+
+sub split {
+	my ($self_or_class, $name) = @_;
+
+	# remove whitespace at start and end of string
+	$name =~ s/^\s*(.*)\s*$/$1/s;
+
+	my @parts = split /\s*,\s*/, $name;
+
+	if (@parts == 0) {
+		return (undef, undef, undef, undef);
+	} elsif (@parts == 1) {	# name without comma
+		if ( $name =~ /(^|\s)[[:lower:]]/) { # name has von part or has only lowercase names
+			my @name_parts = split /\s+/, $parts[0];
+
+			my $first;
+			while (@name_parts && ucfirst($name_parts[0]) eq $name_parts[0] ) {
+				$first .= $first ? ' ' . shift @name_parts : shift @name_parts;
+			}
+
+			my $von;
+			# von part are lowercase words
+			while ( @name_parts && lc($name_parts[0]) eq $name_parts[0] ) {
+				$von .= $von ? ' ' . shift @name_parts : shift @name_parts;
+			}
+
+			if (@name_parts) {
+				return ($first, $von, join(" ", @name_parts), undef);
+			} else {
+				return (undef, undef, $name, undef);
+			}
+		} else {
+			if ($name =~ /^((.*)\s+)?\b(\S+)$/) {
+				return ($2, undef, $3, undef);
+			}
+		}
+
+	} elsif (@parts == 2) {
+		my @von_last_parts = split /\s+/, $parts[0];
+		my $von;
+		# von part are lowercase words
+		while ( lc($von_last_parts[0]) eq $von_last_parts[0] ) {
+			$von .= $von ? ' ' . shift @von_last_parts : shift @von_last_parts;
+		}
+		return ($parts[1], $von, join(" ", @von_last_parts), undef);
+	} else {
+		my @von_last_parts = split /\s+/, $parts[0];
+		my $von;
+		# von part are lowercase words
+		while ( lc($von_last_parts[0]) eq $von_last_parts[0] ) {
+			$von .= $von ? ' ' . shift @von_last_parts : shift @von_last_parts;
+		}
+		return ($parts[2], $von, join(" ", @von_last_parts), $parts[1]);
+	}
+
+}
+
+=head2 to_string
+
+Return string representation of the name.
+
+=cut
+
+sub to_string {
+	my $self = shift;
+
+	if ($self->jr) {
+		return $self->von . " " . $self->last . ", " . $self->jr . ", " . $self->first;
+	} else {
+		return ($self->von ? $self->von . " " : '') . $self->last . ($self->first ? ", " . $self->first : '');
+	}
+}
+
+1; # End of BibTeX::Entry
