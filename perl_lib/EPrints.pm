40ecc2a189bd716c30ed426e120b33022c6ec1ed
Christopher Gutteridge <cjg@ecs.soton.ac.uk>
2009-08-18 12:02:52 +0000

    Documented core API modules list & synopsys.
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@4460 9491667e-5006-0410-a446-efbe8990b998

--- perl_lib/EPrints.pm
+++ perl_lib/EPrints.pm
@@ -21,17 +21,189 @@ B<EPrints> - Institutional Repository software
 
 =head1 SYNOPSIS
 
-	use EPrints qw();
+	#!/usr/bin/perl -w -I/opt/eprints3/perl_lib
+	
+	use EPrints;
+	use strict;
+	
+	my $handle = new EPrints::Handle( 1 , 'my_repository_id' );
+	exit( 1 ) unless( defined $handle );
 
-	my $handle = EPrints::Handle->new( 1, "demoprints" );
+	$eprint = new EPrints::DataObj::EPrint( $handle, 23 );
+	my $title = $eprint->get_value( 'title' );
+	
+	$eprint->set_value( 'creators', 
+		[
+			{ 
+				name => { given=>'John', family=>'Smith' },
+				id => 'js@example.com',
+			},
+			{ 
+				name => { given=>'Marvin', family=>'Fenderson' },
+				id => 'marvin@totl.net',
+			},
+		]
+	);
+	$eprint->commit;
+	
+	my $archive_ds = $handle->get_dataset( "archive" );
+	my $search = new EPrints::Search( 
+		handle => $handle, 
+		dataset => $archive_ds );
+	my $date_mf = $archive_ds->get_field( "date" );
+	$search->add_field( $date_mf, "2000-2003" );
 
-	...
+	my $list = $search->perform_search;
+	$list->map(
+		sub {
+			my( $handle, $dataset, $eprint, $info ) = @_;
+	
+			printf( "%s: %s\n", 
+				 $eprint->get_value( "date" ),
+				 $eprint->get_value( "title" ) );
+		}
+	);
+	$list->dispose();
 
+	$handle->log( "We did some stuff." );
+
+	if( I<sometest> ) { EPrints::abort( "Something bad happened" ); }
+	
 	$handle->terminate;
 
 =head1 DESCRIPTION
 
-See http://www.eprints.org/.
+Using this module will cause all the other EPrints modules to be used also.
+
+See http://www.eprints.org/ for more information about EPrints. Much more documentation can be found at http://wiki.eprints.org/w/Documentation
+
+=head2 Main EPrints Modules
+
+=over 4
+
+=item EPrints
+
+This module! Used to load the other modules.
+
+=item EPrints::Box
+
+A utitility module to render HTML boxes with style and javascript roll-up animations.
+
+=item EPrints::Database
+
+An object representing a connection to the database for a repository. This is an abstraction over sub-objects which connect to MySQL or Oracle.
+
+=item EPrints::DataObj
+
+Abstract object representing a single record in a DataSet. Has one subclass for each type of DataSet. The most important subclasses are listed below. This module documents generic functions which work on all (or most) data objects. Every DataObj has a unique ID within the dataset (an integer, with the exception of Subject). Every DataObj is given a URI of the form I<repository_url>/id/I<datasetid>/I<dataobj_id>
+
+=item EPrints::DataObj::Document
+
+Represents a single document. A document is a set of metadata plus files. It *may* have some repository configuraed metadata in addition to the default. The metadata describes the document and is mostly concerned with formats, and rights. Documents belong to exactly one EPrints::DataObj::EPrint are are destroyed if it is destroyed. A document has one or more file. If there's more than one file then they are related, like a .css file for a .html
+
+=item EPrints::DataObj::EPrint
+
+Represents a single submission to the repository. May have 0+ documents as sub-objects. Has both system defined metafields plus many defined in the repository configuration. 
+
+=item EPrints::DataObj::File
+
+Represents a single file in a document with some basic metadata such as checksums.
+
+=item EPrints::DataObj::User
+
+Represents a single registered user of the repository. Used for keeping track of preferences, profile information and rights management.
+
+=item EPrints::DataObj::Subject
+
+This dataset is used to store the structure of heierachichal(sp?) sets, used by the "Subject" metafield type.
+
+=item EPrints::DataSet
+
+This object represents a set of objects of the same time, and has associated MetaFields and database tables. 
+
+=item EPrints::Email
+
+Tool for sending email.
+
+=item EPrints::Handle
+
+the core of the EPrints API. This object represents a connection between the configuration for a repository, the database connection and either the CGI (web) or CLI (command line) interface.
+
+Handle has a large number of methods, which are documented in more than one file:
+
+=item EPrints::Handle::Language
+
+Handle methods for i18n.
+
+=item EPrints::Handle::Render
+
+Handle methods for generating XHTML as XML::DOM objects.
+
+=item EPrints::Handle::CGI
+
+Handle methods for working with the mod_perl connection.
+
+=item EPrints::Handle::Page
+
+Handle methods for generating and serving XHTML web pages.
+
+=item EPrints::Handle::XML
+
+Handle methods for creating XML::DOM objects.
+
+=item EPrints::List
+
+A list of zero or more data-objects in a single dataset. It can be constructed from a list of ID's or returned as the result of a search.
+
+=item EPrints::MetaField
+
+A single field in a dataset. It has many subclasses, one for each type of field.
+
+=item EPrints::Paginate
+
+Tools for rendering an EPrint::List as paginated HTML.
+
+=item EPrints::Paginate::Columns
+
+An extension to EPrints::Paginate which shows the results in sortable columns, as seen in Items and Review screens.
+
+=item EPrints::Platform
+
+Methods to abstract the operating system. Specifically between UNIX and Windows.
+
+=item EPrints::Repository
+
+Represents the configuration, datasets and dataobjects of a single repository. It is loaded from the configuration files and is essentially read-only.
+
+=item EPrints::Search
+
+The search object takes parameters and returns a List object of matching dataobjs from a given dataset. It can also be used it reverse to test if a dataobj matches it's parameters.
+
+=item EPrints::Storage
+
+Methods to abstract the process of reading and writing files. EPrints 3.2 introduced the possibility of storing files in the cloud, or in other storage devices, and this module is the interface to that.
+
+=item EPrints::TempDir
+
+Tools for creating and destorying temporary directories.
+
+=item EPrints::Time
+
+A set of methods for handling time and converting between time formats.
+
+=item EPrints::URL
+
+Utility methods for generating and getting URLs, relative paths etc.
+
+=item EPrints::Utils
+
+Misc. utility methods.
+
+=item EPrints::XML
+
+Utility methods for working with XML and DOM. This papers over the cracks between the 3 different XML DOM libraries EPrints supports.
+
+=back
 
 =head2 Available Symbols
 
@@ -58,8 +230,6 @@ Where PID is the id number of the stalled process.
 A shell script will print the stack trace to the console.
 
 =head1 METHODS
-
-=over 4
 
 =cut
 
@@ -98,6 +268,8 @@ use EPrints::Apache::Storage;
 	# abort($err) Defined here so modules can abort even at startup
 ######################################################################
 =pod
+
+=over 4
 
 =item EPrints::abort( $msg )
 
