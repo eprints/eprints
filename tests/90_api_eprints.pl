158a4e787b21aab8c1b376e0332ef5b595fa47de
Tim Brody <tdb2@ecs.soton.ac.uk>
2009-10-01 09:32:23 +0000

    * Added test that will show up any missing API methods  * Completed dataset unit test
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@4512 9491667e-5006-0410-a446-efbe8990b998

--- tests/90_api_eprints.pl
+++ tests/90_api_eprints.pl
@@ -1,12 +1,20 @@
 #!/usr/bin/perl
 
-use Test::More tests => 5;
+use Test::More tests => 6;
 
 use strict;
 use warnings;
 
 BEGIN { use_ok( "EPrints" ); }
 BEGIN { use_ok( "EPrints::Test" ); }
+
+# These methods are defined in the API and should exist
+my %API_METHODS = (
+	"EPrints" => [qw( new abort current_repository repository repository_ids )],
+	"EPrints::Repository" => [qw( config current_url current_user dataset eprint log redirect query user user_by_email user_by_username xml )],
+	"EPrints::Dataset" => [qw( base_id create_dataobj dataobj field fields key_field id list prepare_search search )],
+	"EPrints::List" => [qw( count ids item map slice )],
+);
 
 my $repoid = EPrints::Test::get_test_id();
 
@@ -26,3 +34,21 @@ if( !defined $repo ) { BAIL_OUT( "Could not obtain the Repository object" ); }
 #
 # EPrints->abort( $message );
 
+# Check all of the defined API methods are available
+my @missing;
+foreach my $class (sort keys %API_METHODS)
+{
+	foreach my $method (sort @{$API_METHODS{$class}})
+	{
+		my $name = "${class}::${method}";
+		if( !defined &$name )
+		{
+			push @missing, $name;
+		}
+	}
+}
+if( @missing )
+{
+	diag("Missing following API methods: ".join(', ', @missing));
+}
+ok(@missing==0, "all API methods defined");
--- tests/92_api_dataset.pl
+++ tests/92_api_dataset.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-use Test::More tests => 0; # FIXME
+use Test::More tests => 14;
 
 use strict;
 use warnings;
@@ -21,9 +21,11 @@ if( !defined $repo ) { BAIL_OUT( "Could not obtain the Repository object" ); }
 my $dataset = $repo->dataset( "archive" );
 if( !defined $dataset ) { BAIL_OUT( "Could not obtain the archive dataset" ); }
 
+
 is($dataset->id,"archive","id");
 is($dataset->base_id,"eprint","id");
 
+### Fields ###
 my $field = $dataset->field( "eprint_status" );
 if( !defined $dataset ) { BAIL_OUT( "Could not obtain the eprint_status field" ); }
 isa_ok( $field, "EPrints::MetaField::Set", "eprint_status field type" );
@@ -33,6 +35,9 @@ ok(!defined($dataset->field( "INVALID_FIELDNAME" )), "invalid field name is unde
 my $key_field = $dataset->key_field;
 ok(defined($key_field), "key field exists");
 
+my @fields = $dataset->fields;
+is($key_field->name,$fields[0]->name,"key field is first element");
+
 ### Search ###
 my $searchexp = $dataset->prepare_search(filters => [
 	{ meta_fields => [qw( eprint_status )], value => "archive" }
@@ -40,11 +45,15 @@ my $searchexp = $dataset->prepare_search(filters => [
 isa_ok( $searchexp, "EPrints::Search", "prepare_search()" );
 
 ### Objects ###
-my $dataobj = $dataset->create_dataobj( { eprint_status => "inbox", title => "TEST EPRINT", userid => 1 } );
+my $TITLE = "TEST EPRINT";
+
+my $dataobj = $dataset->create_dataobj( { eprint_status => "inbox", title => $title, userid => 1 } );
 if( !defined $dataobj ) { BAIL_OUT( "Could not create a new data object" ); }
 my $dataobj_id = $dataobj->get_id;
 my $dataobj_copy = $dataset->dataobj( $dataobj_id );
 if( !defined $dataobj_copy ) { BAIL_OUT( "Could not retrieve new data object copy" ); }
 ok( $dataobj_copy->get_id, $dataobj_id, "ids match of retrieved objects" );
 ok(!defined($dataset->dataobj( "INVALID_ID" )), "invalid dataobj identifier is undef" )
+is( $dataobj_copy->value( "title" ), $TITLE, "title value" );
 
+$dataobj->delete; # clean up
