0d97bd29b557f27d13938cc32bedbde9e7371704
Tim Brody <tdb2@ecs.soton.ac.uk>
2009-11-12 10:51:19 +0000

    * Use default_order from search config in Staff eprint search, fixing #3517
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@4614 9491667e-5006-0410-a446-efbe8990b998

--- perl_lib/EPrints/Plugin/Screen/Staff/EPrintSearch.pm
+++ perl_lib/EPrints/Plugin/Screen/Staff/EPrintSearch.pm
@@ -55,6 +55,7 @@ sub from
 		dataset_id => "eprint",
 		citation => $self->{session}->get_repository->get_conf( "search","advanced","citation" ),
 		order_methods => $self->{session}->get_repository->get_conf( "search","advanced","order_methods" ),
+		default_order => $self->{session}->get_repository->get_conf( "search","advanced","default_order"),
 	};
 		
 	my $adv_fields = $self->{session}->get_repository->get_conf( "search","advanced","search_fields" );
