9154cdb1c2b5a9d4bb8fc28d0cf3a5bd42fd0d43
Christopher Gutteridge <cjg@ecs.soton.ac.uk>
2009-11-17 17:40:18 +0000

    * Removed unused dependency on Unicode::String from RDFN3
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@4643 9491667e-5006-0410-a446-efbe8990b998

--- perl_lib/EPrints/Plugin/Export/RDFN3.pm
+++ perl_lib/EPrints/Plugin/Export/RDFN3.pm
@@ -4,8 +4,6 @@ package EPrints::Plugin::Export::RDFN3;
 
 # documents needs magic files field
 
-use Unicode::String qw( utf8 );
-
 use EPrints::Plugin::Export;
 
 @ISA = ( "EPrints::Plugin::Export" );
