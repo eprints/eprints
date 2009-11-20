ad95394dde0272b5e02f5bad537147a0dccae7c3
Tim Brody <tdb2@ecs.soton.ac.uk>
2009-11-20 13:55:51 +0000

    * Removed Unicode::String
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@4662 9491667e-5006-0410-a446-efbe8990b998

--- perl_lib/EPrints/Plugin/Export/RDFXML.pm
+++ perl_lib/EPrints/Plugin/Export/RDFXML.pm
@@ -4,8 +4,6 @@ package EPrints::Plugin::Export::RDFXML;
 
 # documents needs magic files field
 
-use Unicode::String qw( utf8 );
-
 use EPrints::Plugin::Export;
 
 @ISA = ( "EPrints::Plugin::Export" );
