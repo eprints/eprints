0667d47a0df2b59d8dc011ac843f871a54e869b6
Tim Brody <tdb2@ecs.soton.ac.uk>
2009-12-07 16:54:04 +0000

    * Avoid a unit test failure on Apache pool missing
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@4739 9491667e-5006-0410-a446-efbe8990b998

--- perl_lib/EPrints/Test/RequestRec.pm
+++ perl_lib/EPrints/Test/RequestRec.pm
@@ -16,4 +16,29 @@ sub uri
 	return $self->{uri};
 }
 
+sub pool
+{
+	my( $self ) = @_;
+
+	return $self->{pool} ||= EPrints::Test::Pool->new();
+}
+
+package EPrints::Test::Pool;
+
+sub new
+{
+	my( $class, %opts ) = @_;
+
+	$opts{cleanup} = [];
+
+	return bless \%opts, $class;
+}
+
+sub cleanup_register
+{
+	my( $self, $f, $ctx ) = @_;
+
+	unshift @{$self->{cleanup}}, [$f, $ctx];
+}
+
 1;
