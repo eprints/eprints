dcd9c58f6db9daded07b41a79d70a63ec41d64e6
Christopher Gutteridge <cjg@ecs.soton.ac.uk>
2009-12-10 16:35:54 +0000

    Ensure that STDOUT is set to be utf-8 (fixes some encoding issues seen)
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@4774 9491667e-5006-0410-a446-efbe8990b998

--- perl_lib/EPrints/Repository.pm
+++ perl_lib/EPrints/Repository.pm
@@ -5628,6 +5628,9 @@ sub init_from_request
 {
 	my( $self, $request ) = @_;
 
+	# ensure our output stream is UTF8
+	binmode( *STDOUT, ":utf8" );
+
 	if( defined $self->{request} )
 	{
 		# we're in a sub-request
