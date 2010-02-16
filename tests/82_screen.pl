189b4a3db68af28d04dcd9f70a45a32b4fd96438
Christopher Gutteridge <cjg@ecs.soton.ac.uk>
2010-02-16 16:58:51 +0000

    * Fixed issue in test which got confused by cached action lists. Resolves #3683
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@4973 9491667e-5006-0410-a446-efbe8990b998

--- tests/82_screen.pl
+++ tests/82_screen.pl
@@ -56,6 +56,8 @@ is( defined($items_screen->{screen_id}) && $items_screen->{screen_id}, $screen_i
 # disable
 {
 	local $session->get_repository->{config}->{plugins}->{$screen_id}->{appears}->{"key_tools"} = undef;
+	delete $session->{screen_lists};
+	$processor->cache_list_items();
 
 	my( $t ) = grep { $_->{screen_id} eq $screen_id } $screen->list_items( "key_tools" );
 
@@ -73,6 +75,8 @@ ok( defined($ne_screen), "item_tools contains $screen_id" );
 # disable action
 {
 	local $session->get_repository->{config}->{plugins}->{$screen_id}->{appears}->{"item_tools"} = undef;
+	delete $session->{screen_lists};
+	$processor->cache_list_items();
 
 	my( $t ) = grep { $_->{screen_id} eq $screen_id } $screen->action_list( "item_tools" );
 
@@ -83,6 +87,8 @@ ok( defined($ne_screen), "item_tools contains $screen_id" );
 {
 	# note we also check based on definedness, not trueness
 	local $session->get_repository->{config}->{plugins}->{$screen_id}->{appears}->{"__TEST__"} = 0;
+	delete $session->{screen_lists};
+	$processor->cache_list_items();
 
 	my( $t ) = grep { $_->{screen_id} eq $screen_id } $screen->action_list( "__TEST__" );
 
