5f4f6c4d853130d2d4f01e04afbc5faa637f2e09
Tim Brody <tdb2@ecs.soton.ac.uk>
2010-02-23 16:49:04 +0000

    * Modified Feed export plugins to inherit from XMLFile, so setting the output correctly
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@5043 9491667e-5006-0410-a446-efbe8990b998

--- perl_lib/EPrints/Plugin/Export/Feed.pm
+++ perl_lib/EPrints/Plugin/Export/Feed.pm
@@ -1,8 +1,8 @@
 package EPrints::Plugin::Export::Feed;
 
-use EPrints::Plugin::Export;
+use EPrints::Plugin::Export::XMLFile;
 
-@ISA = ( "EPrints::Plugin::Export" );
+@ISA = ( "EPrints::Plugin::Export::XMLFile" );
 
 use strict;
 
