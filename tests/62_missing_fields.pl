75f853adc1e101954b746523fee6351ae819693e
Tim Brody <tdb2@ecs.soton.ac.uk>
2010-03-01 11:43:23 +0000

    * Don't test unadvertised plugins (e.g. GScholar!) for missing fields  * Refactoured 62_missing_fields to make better use of the test harness
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@5058 9491667e-5006-0410-a446-efbe8990b998

--- tests/62_missing_fields.pl
+++ tests/62_missing_fields.pl
@@ -1,30 +1,33 @@
 #!/usr/bin/perl
 
-use Test::More tests => 10;
+use Test::More;
 
 use strict;
 use warnings;
 
-BEGIN { use_ok( "EPrints" ); }
-BEGIN { use_ok( "EPrints::Test" ); }
-BEGIN { use_ok( "EPrints::Test::RepositoryLog" ); }
+use EPrints;
+use EPrints::Test;
+use EPrints::Test::RepositoryLog;
 
 my $repoid = EPrints::Test::get_test_id();
 
 my $ep = EPrints->new();
-isa_ok( $ep, "EPrints", "EPrints->new()" );
 if( !defined $ep ) { BAIL_OUT( "Could not obtain the EPrints System object" ); }
 
 my $repo = $ep->repository( $repoid );
-isa_ok( $repo, "EPrints::Repository", "Get a repository object ($repoid)" );
 if( !defined $repo ) { BAIL_OUT( "Could not obtain the Repository object" ); }
 
 my @fields = @{ $repo->config( "fields","eprint" ) };
 
-
 my $dataset = $repo->dataset( "archive" );
 if( !defined $dataset ) { BAIL_OUT( "Could not obtain the archive dataset" ); }
 
+my @plugins = $repo->get_plugins( 
+	type => "Export",
+	can_accept => "dataobj/eprint",
+	is_advertised => 1 ); # only check public plugins - others might to bad things!
+
+plan tests => @fields * 4 + @fields * @plugins;
 
 my $export_ok = 1;
 my $create_ok = 1;
@@ -65,70 +68,27 @@ foreach my $field_data ( @fields )
 		isbn => "1234567890",
 		issn => "12345690",
 	} ); };
-	if( $@ )
-	{
-		$create_ok = 0;
-		print STDERR "*************************************************************\n";
-		print STDERR "* Create eprint assumed field '$field_name' existed\n";
-		print STDERR "*************************************************************\n";
-		next;
-	}
+	ok( !$@, "create eprint without '$field_name' field" );
 
 	if( !defined $eprint ) { BAIL_OUT( "Could not create a new eprint object (in sans $field_name mode)" ); }
 
 	eval { $eprint->move_to_buffer(); };
-	if( $@ )
-	{
-		$move_to_buffer_ok = 0;
-		print STDERR "*************************************************************\n";
-		print STDERR "* Move to buffer assumed field '$field_name' existed\n";
-		print STDERR "*************************************************************\n";
-	}
+	ok( !$@, "move_to_buffer without '$field_name' field" );
 
 	eval { $eprint->move_to_archive(); };
-	if( $@ )
-	{
-		$move_to_archive_ok = 0;
-		print STDERR "*************************************************************\n";
-		print STDERR "* Move to archive assumed field '$field_name' existed\n";
-		print STDERR "*************************************************************\n";
-	}
+	ok( !$@, "move_to_archive without '$field_name' field" );
 
-	my @plugins = $repo->plugin_list( 
-				type=>"Export",
-				can_accept=>"dataobj/eprint" );
-	foreach my $plugin_id ( @plugins ) 
+	foreach my $plugin ( @plugins ) 
 	{
-		$plugin_id =~ m/^[^:]+::(.*)$/;
-		my $id = $1;
-		my $plugin = $repo->plugin( $plugin_id );
+		my $plugin_id = $plugin->get_id;
 		eval { $plugin->output_dataobj( $eprint ); };
-		if( $@ )
-		{
-			$export_ok = 0;
-			print STDERR "*************************************************************\n";
-			print STDERR "* Plugin '$plugin_id' assumed field '$field_name' existed\n";
-			print STDERR "*************************************************************\n";
-		}
+		ok( !$@, "plugin '$plugin_id' without '$field_name' field" );
 	}
 
 	eval { $eprint->move_to_deletion(); };
-	if( $@ )
-	{
-		$move_to_deletion_ok = 0;
-		print STDERR "*************************************************************\n";
-		print STDERR "* Move to deletion assumed field '$field_name' existed\n";
-		print STDERR "*************************************************************\n";
-	}
+	ok( !$@, "move_to_deletion without '$field_name' field" );
 
 	$eprint->delete; # clean up
 }
-
-ok( $export_ok, "Plugins with missing default field" );
-ok( $create_ok, "Creating eprint with missing default field" );
-ok( $move_to_buffer_ok, "move_to_buffer with missing default field" );
-ok( $move_to_archive_ok, "move_to_archive with missing default field" );
-ok( $move_to_deletion_ok, "move_to_deletion with missing default field" );
-
 
 # done
