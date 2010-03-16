0e4ecd5fa8752810a272cbaad47361bb24f8e296
Tim Brody <tdb2@ecs.soton.ac.uk>
2010-03-16 16:05:41 +0000

    * Experimental export of and summary-page generation for arbitrary datasets
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@5156 9491667e-5006-0410-a446-efbe8990b998

---
+++ perl_lib/EPrints/Plugin/Export/SummaryPage.pm
@@ -0,0 +1,40 @@
+package EPrints::Plugin::Export::SummaryPage;
+
+use EPrints::Plugin::Export::HTMLFile;
+
+@ISA = ( "EPrints::Plugin::Export::HTMLFile" );
+
+use strict;
+
+sub new
+{
+	my( $class, %opts ) = @_;
+
+	my( $self ) = $class->SUPER::new( %opts );
+
+	$self->{name} = "Summary Page";
+	$self->{accept} = [ 'dataobj/*' ];
+	$self->{visible} = "all";
+	$self->{advertise} = 0;
+	$self->{qs} = 0.9;
+
+	return $self;
+}
+
+sub output_dataobj
+{
+	my( $self, $dataobj, %opts ) = @_;
+
+	my $repo = $self->{session};
+
+	return "" if !$repo->get_online;
+
+	my $title = $dataobj->render_citation( "summary_title" );
+	my $page = $dataobj->render_citation( "summary" );
+	$repo->build_page( $title, $page, "export" );
+	$repo->send_page;
+
+	return "";
+}
+
+1;
