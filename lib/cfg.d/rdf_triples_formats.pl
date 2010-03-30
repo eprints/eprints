262f7ce3d110ec5bd0384b8aa8047c5f4592f243
Christopher Gutteridge <cjg@ecs.soton.ac.uk>
2010-03-30 14:50:32 +0000

    * Fixes to RDF seeAlso triples (missed "<" and ">") * Added seeAlso back to summary page
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@5233 9491667e-5006-0410-a446-efbe8990b998

--- lib/cfg.d/rdf_triples_formats.pl
+++ lib/cfg.d/rdf_triples_formats.pl
@@ -3,33 +3,60 @@ $c->add_trigger( "rdf_triples_eprint", sub {
 	my( %o ) = @_;
 	my $eprint = $o{"dataobj"};
 	my $eprint_uri = "<".$eprint->uri.">";
+	my $eprint_url = "<".$eprint->url.">";
 
 	my $repo = $o{repository};
+	my $xml = $repo->xml;
+
+	my $title = $xml->to_string( $eprint->render_citation( 'brief' ));
+
+	$o{graph}->add( 
+		  subject => $eprint_uri,
+		predicate => "rdfs:seeAlso",
+  		   object => $eprint_url );
+	$o{graph}->add( 
+		  subject => $eprint_url,
+		predicate => "dc:title",
+  		   object => "HTML Summary of of #".$eprint->id." $title",
+		     type => "literal" );
+	$o{graph}->add( 
+		  subject => $eprint_url,
+		predicate => "dc:format",
+		   object => "text/html",
+		     type => "literal" );
+	$o{graph}->add( 
+		  subject => $eprint_url,
+		predicate => "foaf:primaryTopic",
+  		   object => $eprint_uri );
+
 	my @plugins = $repo->plugin_list( 
 					type=>"Export",
 					can_accept=>"dataobj/eprint",
 					is_advertised=>1,
 					is_visible=>"all" );
-	my $xml = $repo->xml;
 	foreach my $plugin_id ( @plugins ) 
 	{
 		my $plugin = $repo->plugin( $plugin_id );
-		my $url = $plugin->dataobj_export_url( $eprint );
+		my $url = "<".$plugin->dataobj_export_url( $eprint ).">";
 
 		$o{graph}->add( 
 			  subject => $eprint_uri,
 			predicate => "rdfs:seeAlso",
-  			   object => "<$url>" );
+  			   object => $url );
 		$o{graph}->add( 
 			  subject => $url,
 			predicate => "dc:title",
-  			   object => $xml->to_string( $plugin->render_name ),
+  			   object => $xml->to_string( $plugin->render_name )." of #".$eprint->id." $title",
 			     type => "literal" );
 		$o{graph}->add( 
 			  subject => $url,
 			predicate => "dc:format",
 			   object => $plugin->param("mimetype"),
 			     type => "literal" );
+		$o{graph}->add( 
+			  subject => $url,
+			predicate => "foaf:primaryTopic",
+  			   object => $eprint_uri );
 	}
 
 } );
