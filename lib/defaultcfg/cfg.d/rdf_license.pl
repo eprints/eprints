598b6e9369bd0cdb2aee7180987cbd0d31dd5e3c
Christopher Gutteridge <cjg@ecs.soton.ac.uk>
2010-04-21 19:42:37 +0000

    * Added wiki link to rdf config so we can add more help there later.
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@5314 9491667e-5006-0410-a446-efbe8990b998

--- lib/defaultcfg/cfg.d/rdf_license.pl
+++ lib/defaultcfg/cfg.d/rdf_license.pl
@@ -1,6 +1,10 @@
 
 # Licence to be inserted into all RDF data.
 
+# See: 
+#   http://wiki.eprints.org/w/EPrints_RDF 
+# for more information.
+
 # We suggest using Open Data Commons licenses as these are more sutiable for 
 # data than normal Creative Commons. Follow the license URLs for more 
 # information.
@@ -13,3 +17,4 @@
 # $c->{rdf}->{attributionURL} = ".....";
 
 # More detailed repository policy information is lifted from oai.pl
+
