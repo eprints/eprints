0717f4e7ad084ccb17a945d7d08f9124f5c3e276
Tim Brody <tdb2@ecs.soton.ac.uk>
2010-07-12 13:23:25 +0000

    * Added backwards compatibility for Import::DefaultXML subclasses
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@5639 9491667e-5006-0410-a446-efbe8990b998

--- perl_lib/EPrints/Plugin/Import/DefaultXML.pm
+++ perl_lib/EPrints/Plugin/Import/DefaultXML.pm
@@ -52,7 +52,19 @@ sub input_fh
 {
 	my( $plugin, %opts ) = @_;
 
-	my $handler = EPrints::Plugin::Import::DefaultXML::Handler->new(
+	# to support sub-classes that expect xml_to_epdata( DOM ) we'll spot
+	# that and give them a DOM to convert
+	my $class;
+	if( $plugin->can( "xml_to_epdata" ) )
+	{
+		$class = "EPrints::Plugin::Import::DefaultXML::DOMHandler";
+	}
+	else
+	{
+		$class = "EPrints::Plugin::Import::DefaultXML::Handler";
+	}
+
+	my $handler = $class->new(
 		dataset => $opts{dataset},
 		plugin => $plugin,
 		depth => 0,
@@ -66,6 +78,36 @@ sub input_fh
 			dataset => $opts{dataset},
 			session => $plugin->{session},
 			ids => $handler->{imported} );
+}
+
+sub xml_to_text
+{
+	my( $plugin, $xml ) = @_;
+
+	my @list = $xml->childNodes;
+	my $ok = 1;
+	my @v = ();
+	foreach my $node ( @list ) 
+	{  
+		if( EPrints::XML::is_dom( $node,
+			"Text",
+			"CDATASection",
+			"EntityReference" ) ) 
+		{
+			push @v, $node->nodeValue;
+		}
+		else
+		{
+			$ok = 0;
+		}
+	}
+
+	unless( $ok )
+	{
+		$plugin->warning( $plugin->{session}->phrase( "Plugin/Import/DefaultXML:unexpected_xml", xml => $xml->toString ) );
+	}
+
+	return join( "", @v );
 }
 
 package EPrints::Plugin::Import::DefaultXML::Handler;
@@ -119,20 +161,85 @@ sub end_element
 	if( defined(my $handler = $self->{handler}) )
 	{
 		$handler->end_element( $info, $self->{epdata}, $self->{state} );
+
+		if( $self->{depth} == 2 )
+		{
+			delete $self->{state};
+			delete $self->{handler};
+
+			my $epdata = delete $self->{epdata};
+			my $dataobj = $self->{plugin}->epdata_to_dataobj( $self->{dataset}, $epdata );
+			push @{$self->{imported}}, $dataobj->id if defined $dataobj;
+		}
 	}
 
-	if( $self->{depth} == 1 )
+	--$self->{depth};
+}
+
+sub characters
+{
+	my( $self, $info ) = @_;
+
+	if( defined(my $handler = $self->{handler}) )
 	{
+		$handler->characters( $info, $self->{epdata}, $self->{state} );
+	}
+}
+
+package EPrints::Plugin::Import::DefaultXML::DOMHandler;
+
+use strict;
+
+sub new
+{
+	my( $class, %self ) = @_;
+
+	return bless \%self, $class;
+}
+
+sub AUTOLOAD {}
+
+sub start_element
+{
+	my( $self, $info ) = @_;
+
+	++$self->{depth};
+
+	if( defined(my $handler = $self->{handler}) )
+	{
+		$handler->start_element( $info, $self->{epdata}, $self->{state} );
+	}
+	elsif( $self->{depth} == 1 && $info->{Name} ne $self->{plugin}->top_level_tag )
+	{
+		$self->{plugin}->unknown_start_element( $info->{Name}, $self->{plugin}->top_level_tag ); #dies
 	}
 	elsif( $self->{depth} == 2 )
 	{
-		delete $self->{state};
-		delete $self->{handler};
+		$self->{handler} = EPrints::XML::SAX::Builder->new(
+			repository => $self->{plugin}->{session}
+		);
+		$self->{handler}->start_document;
+		$self->{handler}->start_element( $info );
+	}
+}
 
-		my $epdata = delete $self->{epdata};
-#print STDERR Data::Dumper::Dumper( $epdata );
-		my $dataobj = $self->{plugin}->epdata_to_dataobj( $self->{dataset}, $epdata );
-		push @{$self->{imported}}, $dataobj->id if defined $dataobj;
+sub end_element
+{
+	my( $self, $info ) = @_;
+
+	if( defined(my $handler = $self->{handler}) )
+	{
+		$handler->end_element( $info, $self->{epdata}, $self->{state} );
+		if( $self->{depth} == 2 )
+		{
+			delete $self->{handler};
+
+			$handler->end_document;
+			my $xml = $handler->result;
+			my $epdata = $self->{plugin}->xml_to_epdata( $self->{dataset}, $xml );
+			my $dataobj = $self->{plugin}->epdata_to_dataobj( $self->{dataset}, $epdata );
+			push @{$self->{imported}}, $dataobj->id if defined $dataobj;
+		}
 	}
 
 	--$self->{depth};
