ee2d69879bf1c881f9f6d0cf7b4bd3528848092a
David Tarrant <davetaz@ecs.soton.ac.uk>
2010-11-24 13:52:02 +0000

    * Atom plug-in updated to allow exporting of list/document and list/file
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@5969 9491667e-5006-0410-a446-efbe8990b998

--- perl_lib/EPrints/Plugin/Export/Atom.pm
+++ perl_lib/EPrints/Plugin/Export/Atom.pm
@@ -13,7 +13,7 @@ sub new
 	my $self = $class->SUPER::new( %opts );
 
 	$self->{name} = "Atom";
-	$self->{accept} = [ 'list/eprint' ];
+	$self->{accept} = [ 'list/eprint', 'list/document', 'list/file' ];
 	$self->{visible} = "all";
 	$self->{suffix} = ".xml";
 	$self->{mimetype} = "application/atom+xml";
@@ -26,12 +26,14 @@ sub output_list
 	my( $plugin, %opts ) = @_;
 
 	my $list = $opts{list};
+	my $list_dataset = $list->{dataset};
+	my $list_dataset_id = $list_dataset->id;
 
 	my $session = $plugin->{session};
 
 	my $response = $session->make_element( "feed",
 		"xmlns"=>"http://www.w3.org/2005/Atom" );
-
+	
 	my $title = $session->phrase( "archive_name" );
 
 	$title.= ": ".EPrints::Utils::tree_to_utf8( $list->render_description );
@@ -42,20 +44,13 @@ sub output_list
 		4,
 		"title",
 		$title ) );
-
+	
 	$response->appendChild( $session->render_data_element(
 		4,
 		"link",
 		"",
 		href => $session->get_repository->get_conf( "frontpage" ) ) );
 	
-	$response->appendChild( $session->render_data_element(
-		4,
-		"link",
-		"",
-		rel => "self",
-		href => $session->get_full_url ) );
-
 	$response->appendChild( $session->render_data_element(
 		4,
 		"updated", 
@@ -68,66 +63,115 @@ sub output_list
 		"id", 
 		"tag:".$host.",".($year+1900).":feed:feed-title" ) );
 
+	if ($list_dataset_id eq "file") {
+		$list->map(sub {
+				my( undef, undef, $file ) = @_;
+				my $doc = $file->parent();
 
-	$list->map(sub {
-		my( undef, undef, $eprint ) = @_;
-
-		my $item = $session->make_element( "entry" );
-		
-		$item->appendChild( $session->render_data_element(
-			2,
-			"title",
-			EPrints::Utils::tree_to_utf8( $eprint->render_description ) ) );
-		$item->appendChild( $session->render_data_element(
-			2,
-			"link",
-			"",
-			href => $eprint->get_url ) );
-		$item->appendChild( $session->render_data_element(
-			2,
-			"summary",
-			EPrints::Utils::tree_to_utf8( $eprint->render_citation ) ) );
-
-		my $updated;
-		my $datestamp = $eprint->get_value( "datestamp" );
-		if( $datestamp =~ /^(\d{4}-\d{2}-\d{2}) (\d{2}:\d{2}:\d{2})$/ )
-		{
-			$updated = "$1T$2Z";
-		}
-		else
-		{
-			print STDERR "Invalid date\n";
-			$updated =  EPrints::Time::get_iso_timestamp();
-		}
-		
-		$item->appendChild( $session->render_data_element(
-			2,
-			"updated",
-			$updated ) );	
-
-		$item->appendChild( $session->render_data_element(
-			4,
-			"id", 
-			"tag:".$host.",".($eprint->get_value( "date" )||"").":item:/".$eprint->get_id ) );
-
-		if( $eprint->exists_and_set( "creators" ) )
-		{
-			my $names = $eprint->get_value( "creators" );
-			foreach my $name ( @$names )
-			{
-				my $author = $session->make_element( "author" );
+				my $item = $session->make_element( "entry" );
 				
-				my $name_str = EPrints::Utils::make_name_string( $name->{name}, 1 );
-				$author->appendChild( $session->render_data_element(
-					4,
-					"name",
-					$name_str ) );
-				$item->appendChild( $author );
-			}
-		}
+				$item->appendChild( $session->render_data_element(
+						2,
+						"id",
+						$file->uri ) );
+				$item->appendChild( $session->render_data_element(
+						2,
+						"title",
+						$file->get_value("filename") ) );
+				$item->appendChild( $session->render_data_element(
+						2,
+						"link",
+						"",
+						href => $doc->get_url( $file->get_value( "filename" ) ) ) );
+				$response->appendChild( $item );		
+		});	
+	} elsif ($list_dataset_id eq "document") {
+		$list->map(sub {
+				my( undef, undef, $document ) = @_;
 
-		$response->appendChild( $item );		
-	});	
+				my $item = $session->make_element( "entry" );
+				
+				$item->appendChild( $session->render_data_element(
+						2,
+						"id",
+						$document->uri ) );
+				$item->appendChild( $session->render_data_element(
+						2,
+						"title",
+						EPrints::Utils::tree_to_utf8( $document->render_description ) ) );
+				$item->appendChild( $session->render_data_element(
+						2,
+						"link",
+						"",
+						href => $document->get_url ) );
+				$item->appendChild( $session->render_data_element(
+						2,
+						"summary",
+						EPrints::Utils::tree_to_utf8( $document->render_citation ) ) );
+
+				$response->appendChild( $item );		
+		});		
+	} elsif ($list_dataset_id eq "eprint")  {
+		$list->map(sub {
+				my( undef, undef, $eprint ) = @_;
+
+				my $item = $session->make_element( "entry" );
+
+				$item->appendChild( $session->render_data_element(
+						2,
+						"title",
+						EPrints::Utils::tree_to_utf8( $eprint->render_description ) ) );
+				$item->appendChild( $session->render_data_element(
+						2,
+						"link",
+						"",
+						href => $eprint->get_url ) );
+				$item->appendChild( $session->render_data_element(
+						2,
+						"summary",
+						EPrints::Utils::tree_to_utf8( $eprint->render_citation ) ) );
+
+				my $updated;
+				my $datestamp = $eprint->get_value( "datestamp" );
+				if( $datestamp =~ /^(\d{4}-\d{2}-\d{2}) (\d{2}:\d{2}:\d{2})$/ )
+				{
+					$updated = "$1T$2Z";
+				}
+				else
+				{
+					print STDERR "Invalid date\n";
+					$updated =  EPrints::Time::get_iso_timestamp();
+				}
+
+				$item->appendChild( $session->render_data_element(
+							2,
+							"updated",
+							$updated ) );	
+
+				$item->appendChild( $session->render_data_element(
+							4,
+							"id", 
+							"tag:".$host.",".($eprint->get_value( "date" )||"").":item:/".$eprint->get_id ) );
+
+				if( $eprint->exists_and_set( "creators" ) )
+				{
+					my $names = $eprint->get_value( "creators" );
+					foreach my $name ( @$names )
+					{
+						my $author = $session->make_element( "author" );
+
+						my $name_str = EPrints::Utils::make_name_string( $name->{name}, 1 );
+						$author->appendChild( $session->render_data_element(
+									4,
+									"name",
+									$name_str ) );
+						$item->appendChild( $author );
+					}
+				}
+
+				$response->appendChild( $item );		
+		});	
+	}
 
 	my $atomfeed = <<END;
 <?xml version="1.0" encoding="utf-8" ?>
