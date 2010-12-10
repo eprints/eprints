267076473a0a1f4e98c29c1119bfee38cec53188
Tim Brody <tdb2@ecs.soton.ac.uk>
2010-12-10 13:35:19 +0000

    * Oops, forgot to up Database version
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@6020 9491667e-5006-0410-a446-efbe8990b998

--- perl_lib/EPrints/Database.pm
+++ perl_lib/EPrints/Database.pm
@@ -117,7 +117,7 @@ my $DEBUG_SQL = 0;
 
 # this may not be the current version of eprints, it's the version
 # of eprints where the current desired db configuration became standard.
-$EPrints::Database::DBVersion = "3.3.0";
+$EPrints::Database::DBVersion = "3.3.1";
 
 
 # ID of next buffer table. This can safely reset to zero each time
