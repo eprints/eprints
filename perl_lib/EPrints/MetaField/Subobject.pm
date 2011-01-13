cb8b967f49347af864264d54c861cb40ad2369a9
Tim Brody <tdb2@ecs.soton.ac.uk>
2011-01-13 16:30:52 +0000

    * Consume \r when reading index codes
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@6117 9491667e-5006-0410-a446-efbe8990b998

--- perl_lib/EPrints/MetaField/Subobject.pm
+++ perl_lib/EPrints/MetaField/Subobject.pm
@@ -261,7 +261,7 @@ sub get_index_codes_basic
 	$indexcodes_file->get_file(sub {
 		$data .= $_[0];
 	});
-	my @codes = split /\n/, $data;
+	my @codes = split /\r?\n/, $data;
 
 	return( \@codes, [], [] );
 }
