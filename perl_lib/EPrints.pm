f74e6a2b61cd46e0a5fe898b7f69b6b477df1d1a
Tim Brody <tdb2@ecs.soton.ac.uk>
2011-01-17 13:49:15 +0000

    * Strip leading 'v' on stringy versions
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@6152 9491667e-5006-0410-a446-efbe8990b998

--- perl_lib/EPrints.pm
+++ perl_lib/EPrints.pm
@@ -116,7 +116,7 @@ Returns the EPrints version in a human-readable form.
 sub human_version
 {
 	return EPrints->VERSION =~ /^v/ ?
-		EPrints->VERSION :
+		substr(EPrints->VERSION,1) :
 		join('.', map { ord($_) } split(//, EPrints->VERSION));
 }
 
