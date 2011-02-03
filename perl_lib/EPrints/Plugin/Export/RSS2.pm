92f5fa98cd39b113813dc1b991ab181025a3d64b
Sebastien Francois <sf2@ecs.soton.ac.uk>
2011-02-03 12:02:25 +0000

    rolling back http://trac.eprints.org/eprints/ticket/3665
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@6204 9491667e-5006-0410-a446-efbe8990b998

--- perl_lib/EPrints/Plugin/Export/RSS2.pm
+++ perl_lib/EPrints/Plugin/Export/RSS2.pm
@@ -156,13 +156,12 @@ EOX
 			"guid",
 			$eprint->get_url ) );
 		
-		my $citation = $eprint->render_citation;
-		$citation = EPrints::Utils::tree_to_utf8( $citation );
-		utf8::decode( $citation );
 		$item->appendChild( $session->render_data_element(
 			2,
 			"description",
-			$citation ) );
+			EPrints::Utils::tree_to_utf8( $eprint->render_citation ) 
+		) );
+
 		$item->appendChild( $plugin->render_media_content( $eprint ) );
 		
 		&$f( "\n" );
