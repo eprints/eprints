4434a20cd9488237828b63e5fcf0395bc2c6bf4e
Tim Brody <tdb2@ecs.soton.ac.uk>
2011-03-03 10:52:56 +0000

    * Modified FirstTool to call properties_from() on the delegated screen and tidied up some of the code (fixes Items filtering not working)
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@6305 9491667e-5006-0410-a446-efbe8990b998

--- perl_lib/EPrints/Plugin/Screen/FirstTool.pm
+++ perl_lib/EPrints/Plugin/Screen/FirstTool.pm
@@ -1,3 +1,16 @@
+=head1 NAME
+
+EPrints::Plugin::Screen::FirstTool - the first screen to show
+
+=head1 DESCRIPTION
+
+This plugin is the screen shown by the ScreenProcessor if there is no 'screen' parameter.
+
+This plugin redirects to the "Items" screen by default. To change this use:
+
+	$c->{plugins}->{"Screen::FirstTool"}->{params}->{default} = "Screen::First";
+
+=cut
 
 package EPrints::Plugin::Screen::FirstTool;
 
@@ -7,18 +20,19 @@ use EPrints::Plugin::Screen::EPrint;
 
 use strict;
 
-
-sub from
+sub properties_from
 {
 	my( $self ) = @_;
 
 	my $screenid = $self->param( "default" );
 	$screenid = "Items" if !defined $screenid;
-	my $screen = $self->{session}->plugin( "Screen::$screenid" );
+	my $screen = $self->{session}->plugin( "Screen::$screenid",
+			processor => $self->{processor},
+		);
 	if( defined $screen )
 	{
 		$self->{processor}->{screenid} = $screenid;
-		$self->SUPER::from;
+		$screen->properties_from;
 	}
 }
 
@@ -26,11 +40,7 @@ sub render
 {
 	my( $self ) = @_;
 
-	my $chunk = $self->{session}->make_doc_fragment;
-
-	$chunk->appendChild( $self->html_phrase( "no_tools" ) );
-
-	return $chunk;
+	return $self->html_phrase( "no_tools" );
 }
 
 1;
