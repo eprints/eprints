dee4675b57fb8d6ac87c7f3355fa9f38fe5a87bf
David Tarrant <davetaz@ecs.soton.ac.uk>
2011-03-24 17:14:59 +0000

    * Changed default config to point at bazaar.eprints.org (which can be CNAME'd to files later if required)
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@6343 9491667e-5006-0410-a446-efbe8990b998

--- lib/defaultcfg/cfg.d/epm.pl
+++ lib/defaultcfg/cfg.d/epm.pl
@@ -5,6 +5,6 @@ $c->{epm_sources} = [] if !defined $c->{epm_sources};
 
 # Define the EPM sources
 push @{$c->{epm_sources}}, {
-                name => "EPrints Files",
-                base_url => "http://files.eprints.org",
+                name => "EPrints Bazaar",
+                base_url => "http://bazaar.eprints.org",
 };
