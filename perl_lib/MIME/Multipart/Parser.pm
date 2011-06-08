7d1b7e9104fe98bbaa47ab55acbaac67e8efd738
Tim Brody <tdb2@ecs.soton.ac.uk>
2011-06-08 11:52:19 +0000

    * Added support for Content-Transfer-Encoding to MIME::Multipart::Parser  * Fixed XSLT importers  * Fixed Apache::CRUD not returning a dataobj from epdata_to_dataobj
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@6643 9491667e-5006-0410-a446-efbe8990b998

--- perl_lib/MIME/Multipart/Parser.pm
+++ perl_lib/MIME/Multipart/Parser.pm
@@ -1,8 +1,30 @@
-# Parse multipart/ MIME messages
-
 package MIME::Multipart::Parser;
 
+=head1 NAME
+
+MIME::Multipart::Parser - parse multipart/mixed messages
+
+=head1 SYNOPSIS
+
+	use MIME::Multipart::Parser;
+	
+	my $parser = MIME::Multipart::Parser->new;
+	
+	my @parts = $parser->parse( $fh, $boundary );
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=over 4
+
+=cut
+
 use File::Temp;
+use HTTP::Headers;
+use MIME::Base64;
+use MIME::QuotedPrint;
+
 use constant MAX_LINE_SIZE => 8092;
 
 use strict;
@@ -14,6 +36,12 @@ sub new
 	return bless \%opts, $class;
 }
 
+=item @parts = $parser->parse( $fh, $boundary )
+
+Reads a MIME multipart message from $fh, divided by $boundary, and returns the parts as a list of L<MIME::Multipart::Part>s.
+
+=cut
+
 sub parse
 {
 	my( $self, $fh, $boundary ) = @_;
@@ -21,7 +49,7 @@ sub parse
 	local $self->{_buffer} = "";
 
 	my @parts;
-	while(my $part = $self->read_part( $fh, $boundary ))
+	while(my $part = $self->_read_part( $fh, $boundary ))
 	{
 		push @parts, $part;
 	}
@@ -29,7 +57,7 @@ sub parse
 	return @parts;
 }
 
-sub read_line
+sub _read_line
 {
 	my( $self, $fh ) = @_;
 
@@ -47,38 +75,83 @@ sub read_line
 	}
 }
 
-sub read_part
+sub _read_part
 {
 	my( $self, $fh, $boundary ) = @_;
 
 	local $_;
 
 	my @headers;
-	while(defined($_ = $self->read_line($fh)))
+	while(defined($_ = $self->_read_line($fh)))
 	{
 		s/\r?\n$//;
 		last if $_ eq "";
+		# rfc2822, section 3.5
+		die "Cowardly refusing to treat overlength header: $headers[$#headers]"
+			if length($headers[$#headers]) > 998;
 		$headers[$#headers] .= $_, next if s/^ //;
-		push @headers, split ':', $_, 2;
+		push @headers, split /\s*: ?/, $_, 2;
 	}
-	return if !defined $_;
+	return if !defined $_; # hit EOF in headers
+
+	# decode any Encoded-Words and normalise via split/join
+	foreach my $i (grep { $_ % 2 } 0 .. $#headers )
+	{
+		my @parts = HTTP::Headers::Util::split_header_words( $headers[$i] );
+		foreach my $part (@parts)
+		{
+			for(@$part)
+			{
+				s/=\?([^?]+)\?([^?]+)\?([^?]+)\?=/
+					Encode::decode($1,
+						lc($2) eq 'q' ?
+							MIME::QuotedPrint::decode_qp( $3 ) :
+							MIME::Base64::decode_base64( $3 )
+					);
+				/eg;
+				$_ = Encode::decode_utf8( $_ );
+			}
+		}
+		$headers[$i] = HTTP::Headers::Util::join_header_words( @parts );
+	}
+
+	my $headers = HTTP::Headers->new( @headers );
+	my( $cte ) = $headers->remove_header( "Content-Transfer-Encoding" );
 
 	my $tmpfile = File::Temp->new;
+	binmode($tmpfile);
+
+	my $decode_f;
+	if( !defined($cte) || $cte =~ /^7bit|8bit|binary$/ )
+	{
+	}
+	elsif( lc($cte) eq "base64" )
+	{
+		$decode_f = \&MIME::Base64::decode_base64;
+	}
+	elsif( lc($cte) eq "quoted-printable" )
+	{
+		$decode_f = \&MIME::QuotedPrint::decode_qp;
+	}
+	else
+	{
+		die( "Unknown or unsupported Content-Transfer-Encoding: $cte" );
+	}
 
 	my $le = "";
-	while(defined($_ = $self->read_line($fh)))
+	while(defined($_ = $self->_read_line($fh)))
 	{
 		last if /^--$boundary/;
-		syswrite($tmpfile, $le);
+		syswrite($tmpfile, $le) if !defined $decode_f;
 		s/(\r?\n)$//;
 		$le = $1;
-		syswrite($tmpfile, $_);
+		syswrite($tmpfile, defined($decode_f) ? &$decode_f($_) : $_);
 	}
 
 	seek($tmpfile,0,0);
 
 	return {
-		headers => \@headers,
+		headers => $headers,
 		tmpfile => $tmpfile,
 	};
 }
