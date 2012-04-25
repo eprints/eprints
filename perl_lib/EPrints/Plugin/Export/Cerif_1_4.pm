a58d86ed88a71ed617f1f705f5b8a829e1175b06
Tim Brody <tdb2@ecs.soton.ac.uk>
2012-04-25 16:37:24 +0000

    * Initial (rough) version of Import/Export for CERIF 1.4
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@7652 9491667e-5006-0410-a446-efbe8990b998

---
+++ perl_lib/EPrints/Plugin/Export/Cerif_1_4.pm
@@ -0,0 +1,280 @@
+package EPrints::Plugin::Export::Cerif_1_4;
+
+=head1 NAME
+
+EPrints::Plugin::Export::Cerif_1_4 - Cerif 1.4 XML Export
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=over 4
+
+=cut
+
+use EPrints::Plugin::Export::XMLFile;
+use EPrints::XML::SAX::SimpleDriver;
+
+@ISA = ( "EPrints::Plugin::Export::XMLFile" );
+
+use constant {
+	CERIF_NS => 'urn:xmlns:org:eurocris:cerif-1.4-0',
+	CERIF_DATE_START => "1900-01-01T00:00:00Z",
+	CERIF_DATE_END => "2099-12-31T00:00:00Z",
+};
+
+use strict;
+
+my %GRAMMAR;
+
+$GRAMMAR{eprint} = [
+	cfResPublId => { value => sub { $_[0]->uuid } },
+	cfResPublDate => { depends => [qw( date )], value => sub { expand_date($_[0]->value( 'date' )) } },
+	cfNum => { depends => [qw( number )], value => sub { $_[0]->value( 'number' ) } },
+	cfVol => { depends => [qw( volume )], value => sub { $_[0]->value( 'volume' ) } },
+	cfEdition => { depends => [qw( edition )], value => sub { $_[0]->value( 'edition' ) } },
+	cfSeries => { depends => [qw( series )], value => sub { $_[0]->value( 'series' ) } },
+	cfIssue => { depends => [qw( issue )], value => sub { $_[0]->value( 'issue' ) } },
+	cfStartPage => { depends => [qw( pagerange )], value => sub { (split(/\-/,$_[0]->value( 'pagerange' )))[0] } },
+	cfEndPage => { depends => [qw( pagerange )], value => sub { (split(/\-/,$_[0]->value( 'pagerange' )))[1] } },
+	cfTotalPages => { depends => [qw( pages )], value => sub { $_[0]->value( 'pages' ) } },
+	cfISBN => { depends => [qw( isbn )], value => sub { return $_[0]->value( 'type' ) eq 'book' ? $_[0]->value( 'isbn' ) : undef } },
+	cfISSN => { depends => [qw( issn )], value => sub { return $_[0]->value( 'type' ) eq 'book' ? $_[0]->value( 'issn' ) : undef } },
+	cfURI => { value => sub { $_[0]->uri } },
+	cfTitle => { depends => [qw( title )], value => sub { $_[0]->value( 'title' ), cfLangCode => "en_GB", cfTrans => "o" } },
+	cfAbstr => { depends => [qw( abstract )], value => sub { $_[0]->value( 'abstract' ), cfLangCode => "en_GB", cfTrans => "o" } },
+];
+
+our %cfPublicationTypes = (
+	article => "Journal Article",
+	book => "Book",
+);
+
+sub new
+{
+	my( $class, %params ) = @_;
+
+	my $self = $class->SUPER::new( %params );
+
+	$self->{name} = "CERIF 1.4";
+	$self->{accept} = [ 'list/eprint' ];
+	$self->{visible} = "staff";
+	$self->{advertise} = 1;
+
+	return $self;
+}
+
+sub writer
+{
+	my( $self, %opts ) = @_;
+
+	return $self->{_writer} ||= EPrints::XML::SAX::SimpleDriver->new(
+		Handler => EPrints::XML::SAX::PrettyPrint->new(
+		Handler => EPrints::XML::SAX::Writer->new(
+			Output => $opts{fh} ? $opts{fh} : $self->{_output}
+		) ) );
+}
+
+sub output_list
+{
+	my( $self, %opts ) = @_;
+
+	my $r = "";
+
+	local $self->{_writer};
+	local $self->{_output} = \$r;
+	my $writer = $self->writer( %opts );
+
+	$writer->xml_decl( '1.0', 'UTF-8' );
+
+	$writer->start_document;
+
+	$writer->start_element( CERIF_NS, 'CERIF',
+			release => '1.4',
+			date => EPrints::Time::iso_date,
+			sourceDatabase => $self->{session}->config( "base_url" ),
+			'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
+			'xsi:schemaLocation' => 'urn:xmlns:org:eurocris:cerif-1.4-0 http://www.eurocris.org/Uploads/Web%20pages/CERIF-1.4/CERIF_1.4_0.xsd',
+		);
+
+	$opts{list}->map( sub {
+			$self->output_dataobj( $_[2], %opts );
+		});
+
+	$writer->end_element( CERIF_NS, 'CERIF' );
+
+	$writer->end_document;
+
+	return $r;
+}
+
+sub output_dataobj
+{
+	my( $self, $dataobj, %opts ) = @_;
+
+	my $writer = $self->writer;
+
+	$writer->start_element( CERIF_NS, 'cfResPubl' );
+
+	ENTRY: for(my $i = 0; $i < @{$GRAMMAR{eprint}}; $i += 2)
+	{
+		my( $name, $spec ) = @{$GRAMMAR{eprint}}[$i,$i+1];
+		for(@{$spec->{depends}||[]})
+		{
+			next ENTRY if !$dataobj->exists_and_set( $_ );
+		}
+		$writer->data_element( CERIF_NS, $name, $spec->{value}( $dataobj ) );
+	}
+
+	my $type = $dataobj->value( "type" );
+
+	$self->cf_class( $writer, 'cfResPubl_Class',
+			classId => $cfPublicationTypes{$type} || "?$type?",
+			classSchemeId => 'class_scheme_cerif_publication_types',
+		);
+
+	my @publications;
+
+	if( $type eq "article" )
+	{
+		if( $dataobj->exists_and_set( "publication" ) || $dataobj->exists_and_set( "issn" ) )
+		{
+			my $id = $dataobj->uuid(
+				$dataobj->exists_and_set( "issn" ) ?
+					$dataobj->value( "issn" ) :
+					$dataobj->value( "publication" )
+				);
+			$writer->start_element( CERIF_NS, "cfResPubl_ResPubl" );
+			$writer->data_element( CERIF_NS, "cfResPublId2", $id );
+			$self->cf_class_fraction( $writer,
+					classId => "?part-of?",
+					classSchemeId => "?cfrespubl_respubl?",
+				);
+			$writer->end_element( CERIF_NS, "cfResPubl_ResPubl" );
+			push @publications, {
+					_id => $id,
+					title => $dataobj->exists_and_set( "publication" ) ? $dataobj->value( "publication" ) : undef,
+					issn => $dataobj->exists_and_set( "issn" ) ? $dataobj->value( "issn" ) : undef,
+				};
+		}
+	}
+
+	my @people;
+	if( $dataobj->exists_and_set( "creators" ) )
+	{
+		foreach my $creator (@{$dataobj->value( "creators" )})
+		{
+			my $id = $dataobj->uuid( $creator->{id} || EPrints::Utils::make_name_string( $creator->{name} ) );
+			push @people, {
+					%$creator,
+					_id => $id,
+				};
+			$writer->start_element( CERIF_NS, "cfPers_ResPubl" );
+			$writer->data_element( CERIF_NS, "cfPersId", $id );
+			$self->cf_class_fraction( $writer,
+					classId => "?author-of?",
+					classSchemeId => "?cfpers_respubl?",
+				);
+			$writer->end_element( CERIF_NS, "cfPers_ResPubl" );
+		}
+	}
+
+	if( $dataobj->exists_and_set( "id_number" ) )
+	{
+		$self->cf_class( $writer, "cfClass",
+				classId => $dataobj->value( "id_number" ),
+				classSchemeId => "class_scheme_publication_alternateids_doi",
+			);
+	}
+
+	$writer->end_element( CERIF_NS, 'cfResPubl' );
+
+	foreach my $pers (@people)
+	{
+		$self->cf_pers( $writer, $pers );
+	}
+
+	foreach my $publ (@publications)
+	{
+		$writer->start_element( CERIF_NS, "cfResPubl" );
+		$writer->data_element( CERIF_NS, "cfResPublId", $publ->{_id} );
+		$writer->data_element( CERIF_NS, "cfISSN", $publ->{issn} );
+		$writer->data_element( CERIF_NS, "cfTitle", $publ->{title},
+				cfLangCode => "en_GB",
+				cfTrans => "o",
+			);
+		$writer->end_element( CERIF_NS, "cfResPubl" );
+	}
+}
+
+sub cf_pers
+{
+	my( $self, $writer, $pers ) = @_;
+
+	$writer->start_element( CERIF_NS, "cfPers" );
+	$writer->data_element( CERIF_NS, "cfPersId", $pers->{_id} );
+		$writer->start_element( CERIF_NS, "cfPersName" );
+		$writer->data_element( CERIF_NS, "cfFamilyNames", $pers->{name}->{family} );
+		$writer->data_element( CERIF_NS, "cfFirstNames", $pers->{name}->{given} );
+		$writer->end_element( CERIF_NS, "cfPersName" );
+	$writer->end_element( CERIF_NS, "cfPers" );
+}
+
+sub cf_class
+{
+	my( $self, $writer, $name, %parts ) = @_;
+
+	$writer->start_element( CERIF_NS, $name );
+
+	$self->cf_class_fraction( $writer, %parts );
+
+	$writer->end_element( CERIF_NS, $name );
+}
+
+sub cf_class_fraction
+{
+	my( $self, $writer, %parts ) = @_;
+
+	$parts{startDate} ||= CERIF_DATE_START;
+	$parts{endDate} ||= CERIF_DATE_END;
+	$parts{fraction} = '1.00' if !defined $parts{fraction};
+
+	$writer->data_element( CERIF_NS, "cfClassId", $parts{classId} );
+	$writer->data_element( CERIF_NS, "cfClassSchemeId", $parts{classSchemeId} );
+	$writer->data_element( CERIF_NS, "cfStartDate", $parts{startDate} );
+	$writer->data_element( CERIF_NS, "cfEndDate", $parts{endDate} );
+	$writer->data_element( CERIF_NS, "cfFraction", $parts{fraction} );
+}
+
+sub expand_date
+{
+	my( $date ) = @_;
+
+	my @parts = split /\D/, $date;
+	push @parts, 1 while @parts < 3;
+
+	return sprintf("%04d-%02d-%02d", @parts[0..2] );
+}
+
+sub expand_time
+{
+	my( $date ) = @_;
+
+	my @parts = split /\D/, $date;
+	push @parts, 1 while @parts < 3;
+	push @parts, 0 while @parts < 6;
+
+	return sprintf("%04d-%02d-%02dT%02d:%02d:%02dZ", @parts[0..5] );
+}
+
+1;
+
+=back
+
+=head1 SEE ALSO
+
+CERIF 2008 http://www.eurocris.org/cerif/cerif-releases/cerif-2008/
+
+=head1 COPYRIGHT
+
+Copyright 2010 Tim Brody <tdb2@ecs.soton.ac.uk>, University of Southampton, UK.
---
+++ perl_lib/EPrints/Plugin/Import/Cerif_1_4.pm
@@ -0,0 +1,420 @@
+=head1 NAME
+
+EPrints::Plugin::Import::Cerif_1_4
+
+=cut
+
+package EPrints::Plugin::Import::Cerif_1_4;
+
+use EPrints::Plugin::Import::DefaultXML;
+use XML::SAX::Base;
+
+@ISA = qw( EPrints::Plugin::Import::DefaultXML XML::SAX::Base );
+
+use strict;
+
+our %CERIF_TYPE = (
+	respubl => "eprint",
+	pers => "user",
+);
+our %CERIF_RELATION_TYPE = (
+	respubl_respubl => ["eprint", "eprint"],
+	pers_respubl => ["user", "eprint"],
+);
+our %CERIF_CLASS_TYPE = (
+	respubl_class => "eprint",
+	pers_class => "user",
+	persname => "user",
+);
+our %CERIF_RESPUBL_TYPE = (
+	book => "book",
+	inbook => "book_section",
+	article => "article",
+	journal => "journal",
+);
+our %CERIF_RESPUBL_FIELD = (
+	respubldate => "date",
+	num => "number",
+	vol => "volume",
+	totalpages => "pages",
+	abstr => "abstract",
+);
+
+sub new
+{
+	my( $class, %params ) = @_;
+
+	my $self = $class->SUPER::new(%params);
+
+	$self->{name} = "CERIF 1.4";
+	$self->{visible} = "staff";
+	$self->{produce} = [ 'list/eprint' ];
+
+	return $self;
+}
+
+sub input_fh
+{
+	my( $self, %opts ) = @_;
+
+	my $repo = $self->{session};
+
+	local $self->{data} = {};
+	local $self->{depth} = 0;
+	local $self->{stack} = [];
+
+	eval { EPrints::XML::event_parse( $opts{fh}, $self ) };
+	die $@ if $@ and "$@" ne "\n";
+
+	$self->deconstruct();
+
+	$self->localise();
+
+	for(qw( eprint user ))
+	{
+		my $dataset = $repo->dataset( $_ );
+		foreach my $epdata (values %{$self->{data}{$_} || {}})
+		{
+			$self->epdata_to_dataobj( $epdata,
+					dataset => $dataset,
+				);
+		}
+	}
+	
+	return $repo->dataset( "eprint" )->list( [] );
+}
+
+# adjust to local eprints settings
+sub localise
+{
+	my( $self ) = @_;
+
+	my $repo = $self->{session};
+
+	# fix multilang
+	my $dataset = $repo->dataset( "eprint" );
+	for(qw( title abstract ))
+	{
+		next if !$dataset->has_field( $_ );
+		next if $dataset->field( $_ )->isa( "EPrints::MetaField::Multilang" );
+		foreach my $epdata (values %{$self->{data}{eprint} || {}})
+		{
+			$epdata->{$_} = $epdata->{$_}[0]{text};
+		}
+	}
+}
+
+# sort out the mess of classes/relations
+sub deconstruct
+{
+	my( $self ) = @_;
+
+	my $data = $self->{data};
+
+	# process user classes
+	foreach my $class (values %{$data->{"user:class"} || {}})
+	{
+		my $subject = $data->{user}->{$class->{_object}};
+		next if !defined $subject;
+	}
+	delete $data->{"user:class"};
+	
+	# general tidy-up of the user data
+	while(my( $objectid, $object ) = each %{$data->{user} || {}})
+	{
+		$object->{name} = {
+				given => delete $object->{firstnames},
+				family => delete $object->{familynames},
+			};
+		$object->{name}{given} .= " ".delete($object->{middlenames}) if $object->{middlenames};
+	}
+
+	# process user->* relations
+	foreach my $relation (values %{$data->{"user:relation"} || {}})
+	{
+		my $object = $data->{user}->{$relation->{_object}};
+		next if !defined $object;
+		my $subject = $data->{$relation->{_subjectclass}}{$relation->{_subject}};
+		next if !defined $subject;
+
+		if( $relation->{_subjectclass} eq "eprint" )
+		{
+			push @{$subject->{creators}}, {
+					name => $object->{name},
+				};
+		}
+	}
+
+	# process eprint classes
+	foreach my $class (values %{$data->{"eprint:class"} || {}})
+	{
+		my $subject = $data->{eprint}->{$class->{_object}};
+		next if !defined $subject;
+
+		if( "class_scheme_cerif_publication_types" eq $class->{classschemeid} )
+		{
+			$subject->{type} = $CERIF_RESPUBL_TYPE{lc($class->{classid})} || $class->{classid};
+		}
+	}
+	delete $data->{"eprint:class"};
+	
+	# general tidy-up of the eprint data
+	while(my( $objectid, $object ) = each %{$data->{eprint} || {}})
+	{
+		$object->{source} = $objectid;
+		$object->{pagerange} = join('--', grep {
+					EPrints::Utils::is_set( $_ )
+				} @{$object}{qw( startpage endpage )}
+			);
+		$object->{keywords} = join(', ', grep {
+					EPrints::Utils::is_set( $_ )
+				} map {
+					$_->{text}
+				} @{$object->{keyw}}
+			);
+		for(keys %$object)
+		{
+			delete $object->{$_} if !EPrints::Utils::is_set( $object->{$_} );
+		}
+		for(qw( startpage endpage keyw ))
+		{
+			delete $object->{$_};
+		}
+	}
+
+	# process eprint->* relations
+	foreach my $relation (values %{$data->{"eprint:relation"} || {}})
+	{
+		my $object = $data->{eprint}->{$relation->{_object}};
+		next if !defined $object;
+		my $subject = $data->{$relation->{_subjectclass}}{$relation->{_subject}};
+		next if !defined $subject;
+
+		if( "class_scheme_cerif_publication_types" eq $relation->{classschemeid} )
+		{
+			if( "child" eq $relation->{classid} )
+			{
+				if( "journal" eq $subject->{type} )
+				{
+					$object->{publication} = $subject->{title}[0]{text};
+				}
+				elsif( "book" eq $subject->{type} )
+				{
+					$object->{book_title} = $subject->{title}[0]{text};
+					$object->{isbn} ||= $subject->{isbn};
+					$object->{issn} ||= $subject->{issn};
+				}
+				else
+				{
+					warn "Unsupported: $object->{type} - $relation->{classid} - $subject->{type}";
+				}
+			}
+			else
+			{
+				warn "Unsupported classid: $relation->{classid}";
+			}
+		}
+		else
+		{
+			warn "Unsupported classschemeid: $relation->{classschemeid}";
+		}
+	}
+	delete $data->{"eprint:relation"};
+
+
+}
+
+sub start_element
+{
+	my( $self, $data ) = @_;
+
+	++$self->{depth};
+
+	return if $self->{depth} == 1;
+
+	my $name = lc($data->{LocalName}); # forgiving of our own typos as much as anything
+
+	my( $current, $current_type );
+	if( ref($self->{stack}[-1]) eq "ARRAY" )
+	{
+		$current = $self->{stack}[-1];
+		(undef, $current_type) = split /:/, $current->[0], 2;
+	}
+
+	# entities
+	if( $name =~ /^cf(respubl|pers)$/ )
+	{
+		push @{$self->{stack}}, [
+				$CERIF_TYPE{$1},
+				undef,
+				{},
+			];
+	}
+	# relations
+	elsif( $name =~ /^cf(respubl_respubl|pers_respubl)$/ )
+	{
+		my( $from, $to ) = @{$CERIF_RELATION_TYPE{$1}};
+		push @{$self->{stack}}, [
+				"$from:relation",
+				APR::UUID->new->format,
+				{
+					_object => $current ? $current->[1] : undef,
+					_subjectclass => $to,
+				},
+			];
+	}
+	# classes
+	elsif( $name =~ /^cf(respubl_class|pers_class)$/ )
+	{
+		my $class = $CERIF_CLASS_TYPE{$1};
+		push @{$self->{stack}}, [
+				"$class:class",
+				APR::UUID->new->format,
+				{
+					_object => $current ? $current->[1] : undef,
+				},
+			];
+	}
+	# PersName (which lives in a world all by itself)
+	elsif( $name =~ /^cf(persname)$/ )
+	{
+		if( defined $current )
+		{
+			push @{$self->{stack}}, $self->{stack}[-1];
+		}
+		else
+		{
+			push @{$self->{stack}}, [
+					"user",
+					undef,
+					{},
+				];
+		}
+	}
+	# properties for entities, classes and relations
+	elsif( defined $current )
+	{
+		# identifiers
+		if( $name =~ /^cf(respublid|respublid1|respublid2|persid)$/ )
+		{
+			# /CERIF/cfPers/cfPersId
+			if( !defined $current->[1] )
+			{
+				push @{$self->{stack}},
+					\($self->{stack}[-1][1] = "");
+			}
+			# /CERIF/cfPers_Class/cfPersId or /CERIF/cfPers_ResPubl/cfPersId
+			elsif( !defined $self->{stack}[-1][2]{_object} )
+			{
+				push @{$self->{stack}},
+					\($self->{stack}[-1][2]{_object} = "");
+			}
+			# /CERIF/cfPers_ResPubl/cfResPublId or /CERIF/cfPers/cfPers_ResPubl/cfResPublId
+			else
+			{
+				push @{$self->{stack}},
+					\($self->{stack}[-1][2]{_subject} = "");
+			}
+		}
+		# class properties
+		elsif( $name =~ /^cf(classid|classschemeid|startdate|enddate|fraction)$/ )
+		{
+			push @{$self->{stack}},
+				\($self->{stack}[-1][2]{$1} = "");
+		}
+		# entity properties
+		elsif( $name =~ /^cf(respubldate|num|vol|edition|series|issue|startpage|endpage|totalpages|isbn|issn|birthdate|gender|familynames|middlenames|firstnames|othernames)$/ )
+		{
+			my $fieldid = $CERIF_RESPUBL_FIELD{$1} || $1;
+			push @{$self->{stack}}, 
+				\($self->{stack}[-1][2]{$fieldid} = "");
+		}
+		# multilingual entity properties
+		elsif( $name =~ /^cf(title|abstr|keyw)$/ )
+		{
+			my $fieldid = $CERIF_RESPUBL_FIELD{$1} || $1;
+			my $langid = $data->{Attributes}{'{}cfLangCode'}{Value};
+			push @{$self->{stack}[-1][2]{$fieldid}}, {
+					lang => $langid,
+					text => "",
+				};
+			push @{$self->{stack}}, \$self->{stack}[-1][2]{$fieldid}[-1]{text};
+		}
+		else
+		{
+			# Unsupported child element
+			push @{$self->{stack}}, undef;
+		}
+	}
+	else
+	{
+		# Unsupported element
+		push @{$self->{stack}}, undef;
+	}
+}
+
+sub end_element
+{
+	my( $self, $data ) = @_;
+
+	--$self->{depth};
+
+	return if $self->{depth} == 0;
+
+	my $item = pop @{$self->{stack}};
+	return if !defined $item || ref($item) ne "ARRAY";
+
+	$self->merge_data( @$item );
+}
+
+sub merge_data
+{
+	my( $self, $datasetid, $objectid, $epdata ) = @_;
+
+	return if !defined $datasetid;
+	return if !defined $objectid;
+
+	$self->{data}{$datasetid}{$objectid} = {
+			%{$self->{data}{$datasetid}{$objectid} || {}},
+			%$epdata,
+		};
+}
+
+sub characters
+{
+	my( $self, $data ) = @_;
+
+	return if ref($self->{stack}->[-1]) ne "SCALAR";
+
+	${$self->{stack}->[-1]} .= $data->{Data};
+}
+
+1;
+
+=head1 COPYRIGHT
+
+=for COPYRIGHT BEGIN
+
+Copyright 2000-2011 University of Southampton.
+
+=for COPYRIGHT END
+
+=for LICENSE BEGIN
+
+This file is part of EPrints L<http://www.eprints.org/>.
+
+EPrints is free software: you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+EPrints is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with EPrints.  If not, see L<http://www.gnu.org/licenses/>.
+
+=for LICENSE END
+
