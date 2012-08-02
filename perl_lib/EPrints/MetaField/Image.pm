69e9270b2e1cda89e42c5541d9313f9a39e90bf1
Tim Brody <tdb2@ecs.soton.ac.uk>
2012-08-02 10:45:18 +0000

    * Documentation plus initial scheme for the database for Image metafields
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@7832 9491667e-5006-0410-a446-efbe8990b998

--- perl_lib/EPrints/MetaField/Image.pm
+++ perl_lib/EPrints/MetaField/Image.pm
@@ -1,9 +1,40 @@
+=head1 NAME
+
+EPrints::MetaField::Image - upload and display inline images
+
+=head1 DESCRIPTION
+
+Upload and store a (small) image.
+
+Rendering the value will output an image tag with the image data inlined using the data: URI scheme:
+
+	<img src="data:image/jpeg;base64,..." />
+
+On upload images will be automatically reduced in size such that it fits within C<maxwidth> and C<maxheight>.
+
+=over 4
+
+=cut
+
+
 use strict;
 
 package EPrints::MetaField::Image;
 
-use base EPrints::MetaField::Base64;
+use base EPrints::MetaField::Multipart;
 
+sub get_property_defaults
+{
+	my( $self ) = @_;
+	my %defaults = $self->SUPER::get_property_defaults;
+	$defaults{fields} = [
+		{ sub_name => "original", type => "base64", },
+		{ sub_name => "small", type => "base64", },
+	];
+	$defaults{maxwidth} = 640;
+	$defaults{maxheight} = 480;
+	return %defaults;
+}
 
 
 1;
