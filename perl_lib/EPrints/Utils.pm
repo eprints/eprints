96a83d6227908202c02732d55c82a07fce1b2533
tmb <tmb@9491667e-5006-0410-a446-efbe8990b998>
2012-10-04 10:30:43 +0000

    Allow options to be controlled eg. on search form #4082
    git-svn-id: https://svn.eprints.org/eprints/trunk/system@7896 9491667e-5006-0410-a446-efbe8990b998

--- perl_lib/EPrints/Utils.pm
+++ perl_lib/EPrints/Utils.pm
@@ -653,6 +653,11 @@ sub field_from_config_string
 			{
 				my( $k, $v ) = split( /=/, $render_pair );
 				$v = 1 unless defined $v;
+				if( $k eq "options" )
+				{
+					$q{$k} = [ split( ",", $v ) ];
+					next;
+				}
 				$q{($k eq "top"?"top":"render_$k")} = $v;
 			}
 		}
