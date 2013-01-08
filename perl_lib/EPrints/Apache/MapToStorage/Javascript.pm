eprints/eprints

commit b758633103bdf17c5f13d914479117d4279bffd5

Tidy-up EPrints::Page and file delivery
The code for deliverying "pages" (HTML that is templated) was going through
EPrints::Repository, Apache::Template, EPrints::XHTML, EPrints::Page, amongst
others.

This patch rationalises this situation into EPrints::Page and a new
EPrints::Template tree. These two modules are now responsible for
reading/writing pins from disk (static HTML content), generating dynamic pins
and outputting the content through an EPrints XML template.

As part of these changes the general delivery of static content has been
rationalised by the creation of a set of Apache::MapToStorage classes that map
URLs onto files (generating those files if necessary).

Details:

404 errors are now delivered using a Screen::Error::404 plugin

HTTP Expires is now only set on .css, .js and plain files (not templated
content).

Removed Repository::render_error, replacing it with EPrints->abort or
Screen::Error as appropriate.

Removed all page and template processing code from Repository. Use
Repository::template() to access a template object.

Removed support for "page_id" and page-hooks.

perl_lib/EPrints/Apache/MapToStorage/Javascript.pm
@@ -1,0 +1,68 @@
+=head1 NAME
+
+EPrints::Apache::MapToStorage::Javascript - generate auto.js
+
+=head1 DESCRIPTION
+
+=over 4
+
+=cut
+
+package EPrints::Apache::MapToStorage::Javascript;
+
+use EPrints::Const qw( :http );
+use base qw( EPrints::Apache::MapToStorage );
+
+use strict;
+
+sub handler
+{
+	my( $r ) = @_;
+
+	my $repo = EPrints->new->current_repository;
+
+	my $langid = $r->pnotes( "langid" );
+
+	my @static_dirs = $repo->get_static_dirs( $langid );
+
+	my $target = $repo->config( "htdocs_path" )."/".$langid."/javascript/auto.js";
+	$r->filename( $target );
+
+	EPrints::Update::Static::update_auto( $target, [
+			map { "$_/javascript/auto" } @static_dirs
+		]);
+
+	EPrints::Apache::MapToStorage::set_expires( $repo, $r );
+
+	return DECLINED;
+}
+
+1;
+
+=head1 COPYRIGHT
+
+=for COPYRIGHT BEGIN
+
+Copyright 2000-2012 University of Southampton.
+
+=for COPYRIGHT END
+
+=for LICENSE BEGIN
+
+This file is part of EPrints L<http://www.eprints.org/>.
+
+EPrints is free software: you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+EPrints is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with EPrints.  If not, see L<http://www.gnu.org/licenses/>.
+
+=for LICENSE END
+

perl_lib/EPrints/Apache/MapToStorage/Stylesheet.pm
@@ -1,0 +1,68 @@
+=head1 NAME
+
+EPrints::Apache::MapToStorage::Stylesheet - generate auto.css
+
+=head1 DESCRIPTION
+
+=over 4
+
+=cut
+
+package EPrints::Apache::MapToStorage::Stylesheet;
+
+use EPrints::Const qw( :http );
+use base qw( EPrints::Apache::MapToStorage );
+
+use strict;
+
+sub handler
+{
+	my( $r ) = @_;
+
+	my $repo = EPrints->new->current_repository;
+
+	my $langid = $r->pnotes( "langid" );
+
+	my @static_dirs = $repo->get_static_dirs( $langid );
+
+	my $target = $repo->config( "htdocs_path" )."/".$langid."/style/auto.css";
+	$r->filename( $target );
+
+	EPrints::Update::Static::update_auto( $target, [
+			map { "$_/style/auto" } @static_dirs
+		]);
+
+	EPrints::Apache::MapToStorage::set_expires( $repo, $r );
+
+	return DECLINED;
+}
+
+1;
+
+=head1 COPYRIGHT
+
+=for COPYRIGHT BEGIN
+
+Copyright 2000-2012 University of Southampton.
+
+=for COPYRIGHT END
+
+=for LICENSE BEGIN
+
+This file is part of EPrints L<http://www.eprints.org/>.
+
+EPrints is free software: you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+EPrints is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with EPrints.  If not, see L<http://www.gnu.org/licenses/>.
+
+=for LICENSE END
+

cgi/handle_404
@@ -133,12 +133,9 @@ sub plain_404
 {
 	my( $session ) = @_;
 
-	$session->build_page( 
-		$session->html_phrase( "cgi/handle_http_error:404_title" ),
-		$session->html_phrase( "cgi/handle_http_error:404_blurb", 
-			url => $session->make_text( $ENV{REDIRECT_URL} ) ),
-		"plain_404" );
-
-        $session->send_page();
+	EPrints::ScreenProcessor->process(
+		session => $session,
+		screenid => "Error::404",
+	);
 }
 

lib/defaultcfg/cfg.d/misc.pl
@@ -8,16 +8,6 @@
 #
 ######################################################################
 
-# Example page hooks to mess around with the metadata
-# submission page.
-
-# my $doc = EPrints::XML::make_document();
-# my $link = $doc->createElement( "link" );
-# $link->setAttribute( "rel", "copyright" );
-# $link->setAttribute( "href", "http://totl.net/" );
-# $c->{pagehooks}->{submission_meta}->{head} = $link;
-# $c->{pagehooks}->{submission_meta}->{bodyattr}->{bgcolor} = '#ff0000';
-
 
 # If you use the Latex render function and want to use the mimetex
 # package rather than the latex->dvi->ps->png route then enable this

perl_lib/EPrints.pm
@@ -200,6 +200,13 @@ use EPrints::Apache::REST;
 use EPrints::Apache::RobotsTxt;
 use EPrints::Apache::SiteMap;
 use EPrints::Apache::CRUD;
+use EPrints::Apache::MapToStorage;
+use EPrints::Apache::MapToStorage::Javascript;
+use EPrints::Apache::MapToStorage::SecureJavascript;
+use EPrints::Apache::MapToStorage::Stylesheet;
+use EPrints::Apache::MapToStorage::XHTML;
+use EPrints::Apache::MapToStorage::XPage;
+use EPrints::Apache::MapToStorage::View;
 
 use EPrints::BackCompatibility;
 
@@ -252,7 +259,6 @@ use EPrints::MetaField;
 use EPrints::NamedSet;
 use EPrints::OpenArchives;
 use EPrints::Page;
-use EPrints::Page::DOM;
 use EPrints::Paginate;
 use EPrints::Paginate::Columns;
 use EPrints::Plugin;
@@ -269,6 +275,8 @@ use EPrints::ScreenProcessor;
 use EPrints::Script;
 use EPrints::Script::Compiler;
 use EPrints::Script::Compiled;
+use EPrints::Template;
+use EPrints::Template::EPC;
 use EPrints::URL;
 use EPrints::Paracite;
 use EPrints::Update::Static;

perl_lib/EPrints/Apache/MapToStorage.pm
@@ -1,0 +1,100 @@
+=head1 NAME
+
+EPrints::Apache::MapToStorage - delivery static files
+
+=head1 DESCRIPTION
+
+Map requests for plain static files to the file system.
+
+=over 4
+
+=cut
+
+package EPrints::Apache::MapToStorage;
+
+use EPrints::Const qw( :http );
+
+use File::Find;
+
+use strict;
+
+sub handler
+{
+	my( $r ) = @_;
+
+	my $repo = EPrints->new->current_repository;
+
+	my $localpath = $r->pnotes( "localpath" );
+
+	my @static_dirs = $repo->get_static_dirs( $r->pnotes( "langid" ) );
+
+	$r->filename( find_source_file( $localpath, @static_dirs ) );
+
+	set_expires( $repo, $r );
+
+	return DECLINED;
+}
+
+sub set_expires
+{
+	my( $repo, $r ) = @_;
+
+	# set all static files to +1 month expiry
+	$r->headers_out->{Expires} = Apache2::Util::ht_time(
+			$r->pool,
+			time + 30 * 86400
+		);
+	# let Firefox cache secure, static files
+	if( $repo->get_secure )
+	{
+		$r->headers_out->{'Cache-Control'} = 'public';
+	}
+}
+
+sub find_source_file
+{
+	my( $localpath, @dirs ) = @_;
+
+	my $filename;
+
+	DIR: foreach my $dir ( @dirs )
+	{
+		if( -e $dir.$localpath )
+		{
+			$filename = $dir.$localpath;
+			last DIR;
+		}
+	}
+
+	return $filename;
+}
+
+1;
+
+=head1 COPYRIGHT
+
+=for COPYRIGHT BEGIN
+
+Copyright 2000-2011 University of Southampton.
+
+=for COPYRIGHT END
+
+=for LICENSE BEGIN
+
+This file is part of EPrints L<http://www.eprints.org/>.
+
+EPrints is free software: you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+EPrints is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with EPrints.  If not, see L<http://www.gnu.org/licenses/>.
+
+=for LICENSE END
+

perl_lib/EPrints/Apache/MapToStorage/SecureJavascript.pm
@@ -1,0 +1,70 @@
+=head1 NAME
+
+EPrints::Apache::MapToStorage::SecureJavascript - generate secure_auto.js
+
+=head1 DESCRIPTION
+
+=over 4
+
+=cut
+
+package EPrints::Apache::MapToStorage::SecureJavascript;
+
+use EPrints::Const qw( :http );
+use base qw( EPrints::Apache::MapToStorage );
+
+use strict;
+
+sub handler
+{
+	my( $r ) = @_;
+
+	my $repo = EPrints->new->current_repository;
+
+	my $langid = $r->pnotes( "langid" );
+
+	my @static_dirs = $repo->get_static_dirs( $langid );
+
+	my $target = $repo->config( "htdocs_path" )."/".$langid."/javascript/secure_auto.js";
+	$r->filename( $target );
+
+	EPrints::Update::Static::update_secure_auto_js(
+			$repo,
+			$repo->config( "htdocs_path" ) . "/$langid",
+			\@static_dirs
+		);
+
+	EPrints::Apache::MapToStorage::set_expires( $repo, $r );
+
+	return DECLINED;
+}
+
+1;
+
+=head1 COPYRIGHT
+
+=for COPYRIGHT BEGIN
+
+Copyright 2000-2012 University of Southampton.
+
+=for COPYRIGHT END
+
+=for LICENSE BEGIN
+
+This file is part of EPrints L<http://www.eprints.org/>.
+
+EPrints is free software: you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+EPrints is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with EPrints.  If not, see L<http://www.gnu.org/licenses/>.
+
+=for LICENSE END
+

perl_lib/EPrints/Apache/MapToStorage/View.pm
@@ -1,0 +1,77 @@
+=head1 NAME
+
+EPrints::Apache::MapToStorage::View - update and show view pages
+
+=head1 DESCRIPTION
+
+=over 4
+
+=cut
+
+package EPrints::Apache::MapToStorage::View;
+
+use EPrints::Const qw( :http );
+use base qw( EPrints::Apache::MapToStorage );
+
+use strict;
+
+sub handler
+{
+	my( $r ) = @_;
+
+	my $repo = EPrints->new->current_repository;
+
+	my $localpath = $r->pnotes( "localpath" );
+	my $langid = $r->pnotes( "langid" );
+	my $localuri = $r->pnotes( "localuri" );
+
+	my $filename = EPrints::Update::Views::update_view_file( $repo, $langid, $localpath, $localuri );
+	$filename =~ s/\.html$// if defined $filename;
+
+	if( defined $filename && -r "$filename.page" )
+	{
+		my $ua = $r->headers_in->{'User-Agent'};
+		if( $ua && $ua =~ /MSIE ([0-9]{1,}[\.0-9]{0,})/ && $1 >= 8.0 )
+		{
+			$r->headers_out->{'X-UA-Compatible'} = "IE=9";
+		}
+
+		$r->filename( "$filename.html" );
+		$r->handler('perl-script');
+		$r->set_handlers(PerlResponseHandler => [ 'EPrints::Apache::Template' ] );
+
+		return OK;
+	}
+
+	return DECLINED;
+}
+
+1;
+
+=head1 COPYRIGHT
+
+=for COPYRIGHT BEGIN
+
+Copyright 2000-2012 University of Southampton.
+
+=for COPYRIGHT END
+
+=for LICENSE BEGIN
+
+This file is part of EPrints L<http://www.eprints.org/>.
+
+EPrints is free software: you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+EPrints is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with EPrints.  If not, see L<http://www.gnu.org/licenses/>.
+
+=for LICENSE END
+

commit 68bbdcd19459920da5fbd8f83e6e947a0777050b

Fixed MapToStorage not creating .css and .js
Fixed bug introduced by b758633103bdf17c5f13d914479117d4279bffd5.

perl_lib/EPrints/Apache/MapToStorage/Javascript.pm
@@ -28,7 +28,7 @@ sub handler
 	my $target = $repo->config( "htdocs_path" )."/".$langid."/javascript/auto.js";
 	$r->filename( $target );
 
-	EPrints::Update::Static::update_auto( $target, [
+	EPrints::Update::Static::update_auto( $target, "js", [
 			map { "$_/javascript/auto" } @static_dirs
 		]);
 

perl_lib/EPrints/Apache/MapToStorage/Stylesheet.pm
@@ -28,7 +28,7 @@ sub handler
 	my $target = $repo->config( "htdocs_path" )."/".$langid."/style/auto.css";
 	$r->filename( $target );
 
-	EPrints::Update::Static::update_auto( $target, [
+	EPrints::Update::Static::update_auto( $target, "css", [
 			map { "$_/style/auto" } @static_dirs
 		]);
 