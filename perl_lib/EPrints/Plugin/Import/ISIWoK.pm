07f4cc5a7ccc262618aa44d1e97d1aed4c1474c9
Tim Brody <tdb2@ecs.soton.ac.uk>
2013-02-20 14:13:00 +0000

    Update ISI WoK import to API v.3

--- perl_lib/EPrints/Plugin/Import/ISIWoK.pm
+++ perl_lib/EPrints/Plugin/Import/ISIWoK.pm
@@ -6,8 +6,8 @@ EPrints::Plugin::Import::ISIWoK
 
 package EPrints::Plugin::Import::ISIWoK;
 
+use base qw( EPrints::Plugin::Import::ISIWoKXML );
 use EPrints::Plugin::Import::TextFile;
-@ISA = qw( EPrints::Plugin::Import::TextFile );
 
 use strict;
 
@@ -18,17 +18,25 @@ sub new
 	my $self = $class->SUPER::new( %params );
 
 	$self->{name} = "ISI Web of Knowledge";
+	$self->{advertise} = 1;
 	$self->{visible} = "all";
 	$self->{produce} = [ 'list/eprint' ];
 	$self->{screen} = "Import::ISIWoK";
 
-	if( !EPrints::Utils::require_if_exists( "SOAP::ISIWoK::Lite", "1.05" ) )
+	if( !EPrints::Utils::require_if_exists( "SOAP::ISIWoK", "3.00" ) )
 	{
 		$self->{visible} = 0;
-		$self->{error} = "Requires SOAP::ISIWoK::Lite 1.05";
+		$self->{error} = "Requires SOAP::ISIWoK 3.00";
 	}
 
 	return $self;
+}
+
+sub input_fh
+{
+	my ($self, %opts) = @_;
+
+	return $self->EPrints::Plugin::Import::TextFile::input_fh( %opts );
 }
 
 sub input_text_fh
@@ -36,123 +44,41 @@ sub input_text_fh
 	my( $self, %opts ) = @_;
 
 	my $session = $self->{session};
-	my $dataset = $opts{dataset};
 
 	my @ids;
 
 	my $fh = $opts{fh};
 	my $query = join '', <$fh>;
 
-	my $wok = SOAP::ISIWoK::Lite->new;
+	my $wok = SOAP::ISIWoK->new;
 
-	my $xml = $wok->search( $query,
+	my $som = $wok->authenticate;
+	if ($som->fault)
+	{
+		$self->warning($som->faultstring);
+		return undef;
+	}
+
+	$som = $wok->search( $query,
 		offset => $opts{offset},
 	);
-	$self->{total} = $xml->documentElement->getAttribute( "recordsFound" );
-
-	foreach my $rec ($xml->getElementsByTagName( "REC" ))
+	if ($som->fault)
 	{
-		my $epdata = $self->xml_to_epdata( $dataset, $rec );
-		next if !scalar keys %$epdata;
-		my $dataobj = $self->epdata_to_dataobj( $dataset, $epdata );
-		push @ids, $dataobj->id if defined $dataobj;
+		$self->error($som->faultstring);
+		return EPrints::List->new(
+				session => $session,
+				dataset => $opts{dataset},
+				ids => []
+			);
 	}
+	$self->{total} = $som->result->{recordsFound};
 
-	return EPrints::List->new(
-		session => $session,
-		dataset => $dataset,
-		ids => \@ids );
-}
+	open(my $xml_fh, "<", \$som->result->{records}) or die "Error getting fh to scalar: $!";
 
-sub xml_to_epdata
-{
-	my( $self, $dataset, $rec ) = @_;
-
-	my $epdata = {};
-
-	my $node;
-
-	( $node ) = $rec->findnodes( "item/ut" );
-	$epdata->{source} = $node->textContent if $node;
-
-	( $node ) = $rec->findnodes( "item/item_title" );
-	$epdata->{title} = $node->textContent if $node;
-
-	if( !$node )
-	{
-		die "Expected to find item_title in: ".$rec->toString( 1 );
-	}
-
-	( $node ) = $rec->findnodes( "item/source_title" );
-	if( $node )
-	{
-		$epdata->{publication} = $node->textContent;
-		$epdata->{ispublished} = "pub";
-	}
-
-	foreach my $node ($rec->findnodes( "item/article_nos/article_no" ))
-	{
-		my $id = $node->textContent;
-		if( $id =~ s/^DOI\s+// )
-		{
-			$epdata->{id_number} = $id;
-		}
-	}
-
-	( $node ) = $rec->findnodes( "item/bib_pages" );
-	$epdata->{pagerange} = $node->textContent if $node;
-
-	( $node ) = $rec->findnodes( "item/bib_issue" );
-	if( $node )
-	{
-		$epdata->{date} = $node->getAttribute( "year" ) if $node->hasAttribute( "year" );
-		$epdata->{volume} = $node->getAttribute( "vol" ) if $node->hasAttribute( "vol" );
-	}
-
-	# 
-	$epdata->{type} = "article";
-	( $node ) = $rec->findnodes( "item/doctype" );
-	if( $node )
-	{
-	}
-
-	foreach my $node ($rec->findnodes( "item/authors/*" ))
-	{
-		if( $node->nodeName eq "fullauthorname" )
-		{
-			next if !$epdata->{creators};
-			my( $family ) = $node->getElementsByTagName( "AuLastName" );
-			my( $given ) = $node->getElementsByTagName( "AuFirstName" );
-			$family = $family->textContent if $family;
-			$given = $given->textContent if $given;
-			$epdata->{creators}->[$#{$epdata->{creators}}]->{name} = {
-				family => $family,
-				given => $given,
-			};
-		}
-		else
-		{
-			my $name = $node->textContent;
-			my( $family, $given ) = split /,/, $name;
-			push @{$epdata->{creators}}, {
-				name => { family => $family, given => $given },
-			};
-		}
-	}
-
-	foreach my $node ($rec->findnodes( "item/keywords/*" ))
-	{
-		push @{$epdata->{keywords}}, $node->textContent;
-	}
-	$epdata->{keywords} = join ", ", @{$epdata->{keywords}} if $epdata->{keywords};
-
-	( $node ) = $rec->findnodes( "item/abstract" );
-	$epdata->{abstract} = $node->textContent if $node;
-
-	# include the complete data for debug
-	$epdata->{suggestions} = $rec->toString( 1 );
-
-	return $epdata;
+	return $self->SUPER::input_fh(
+		%opts,
+		fh => $xml_fh,
+	);
 }
 
 1;
