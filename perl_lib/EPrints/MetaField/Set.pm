9535c2361a2403ecb065f7d5c9e03e6431b41300
Matthew Kerwin <matthew.kerwin@qut.edu.au>
2013-03-11 13:23:38 +1000

    Allow blank Set fields
    Changed XSD for Set field to include "" in the enumeration.  (Analogous to Compound having minOccurs=0)

--- perl_lib/EPrints/MetaField/Set.pm
+++ perl_lib/EPrints/MetaField/Set.pm
@@ -538,6 +538,12 @@ sub render_xml_schema_type
 
 	my( $tags, $labels ) = $self->tags_and_labels( $session );
 
+	# Allow Set fields to have no value. This weakens the constraints
+	# applies by the schema, but is an approach that works for
+	# optional Set fields.
+	push @{$tags}, '';
+	$labels->{''} = $session->phrase( 'lib/metafield:unspecified' );
+
 	my $restriction = $session->make_element( "xs:restriction", base => "xs:string" );
 	$type->appendChild( $restriction );
 	foreach my $value (@$tags)
@@ -587,3 +593,4 @@ License along with EPrints.  If not, see L<http://www.gnu.org/licenses/>.
 
 =for LICENSE END
 
+# vim:ts=4:sts=4:sw=4
