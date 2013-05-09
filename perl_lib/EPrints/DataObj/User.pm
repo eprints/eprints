6c965497e1a66a80bd1b89a05a87612bafd675cf
Sebastien Francois <sf2@ecs.soton.ac.uk>
2013-05-09 17:43:20 +0100

    0003-Adds-optional-cc_list-parameter-to-mail

--- perl_lib/EPrints/DataObj/User.pm
+++ perl_lib/EPrints/DataObj/User.pm
@@ -675,7 +675,7 @@ sub has_owner
 ######################################################################
 =pod
 
-=item $ok = $user->mail( $subjectid, $message, [$replyto], [$email] )
+=item $ok = $user->mail( $subjectid, $message, [$replyto], [$email], [$cc_list] )
 
 Send an email to this user. 
 
@@ -689,6 +689,8 @@ repository default.
 $email is the email address to send this email to if different from
 this users configured email address.
 
+$cc_list is an optional cc_list (array ref)
+
 Return true if the email was sent OK.
 
 =cut
@@ -696,7 +698,7 @@ Return true if the email was sent OK.
 
 sub mail
 {
-	my( $self,   $subjectid, $message, $replyto,  $email ) = @_;
+	my( $self,   $subjectid, $message, $replyto,  $email, $cc_list ) = @_;	#EPrints Services/sf2 2010-11-18 added optional cc_list (array ref) for GREENWICH-17
 	#   User   , string,     DOM,      User/undef Other Email
 
 	# Mail the admin in the default language
@@ -725,6 +727,7 @@ sub mail
 		sig      => $lang->phrase( "mail_sig", {}, $self->{session} ),
 		replyto_name  => $rname, 
 		replyto_email => $remail,
+		cc_list => $cc_list,
 	); 
 }
 
