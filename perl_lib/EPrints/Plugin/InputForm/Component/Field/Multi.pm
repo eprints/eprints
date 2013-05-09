399df46e35771d7354de1a5c04f69b598ae9feef
Sebastien Francois <sf2@ecs.soton.ac.uk>
2013-05-09 17:46:10 +0100

    customisation-for-type-specific-help-on-the-eprint-workflow

--- perl_lib/EPrints/Plugin/InputForm/Component/Field/Multi.pm
+++ perl_lib/EPrints/Plugin/InputForm/Component/Field/Multi.pm
@@ -168,9 +168,18 @@ sub render_content
 				"sys:ep_form_required",
 				label=>$parts{label} );
 		}
- 
-		$parts{help} = $field->render_help( $self->{session} );
 
+		# customisation for type specific help on the eprint workflow
+		# phrase IDs should be (e.g. eprint_fieldhelp_title.article)
+	        if( $self->{dataobj}->exists_and_set('type') )
+	        {
+	                my $phrase_id = 'eprint_fieldhelp_' . $field->{name} . '.' . $self->{dataobj}->get_value('type');
+	                if( $self->{session}->get_lang->has_phrase( $phrase_id ) )
+	                {
+	                        $parts{help} = $self->{session}->html_phrase($phrase_id);
+	                }
+	        }
+		$parts{help} = $field->render_help( $self->{session} ) unless $parts{help};
 
 		# Get the field and its value/default
 		my $value;
