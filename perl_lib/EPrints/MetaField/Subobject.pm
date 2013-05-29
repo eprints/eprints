d9e3f88b6cdfbfe4d7538e6ed9c4acdfe8036ad5
Tim Brody <tdb2@ecs.soton.ac.uk>
2013-05-29 13:48:27 +0100

    fixed s/dataset/dataobj/ typo
    Improved the error message emitting from MetaField::Subobject when the required
    properties don't work.

--- perl_lib/EPrints/MetaField/Subobject.pm
+++ perl_lib/EPrints/MetaField/Subobject.pm
@@ -167,7 +167,7 @@ sub get_value
 		{
 			if( !$ds->has_field( $fieldname ) )
 			{
-				EPrints->abort( "dataset_fieldname property on ".$self->{dataset}->id.".".$self->{name}." is not a valid field on ".$ds->id );
+				EPrints->abort( "dataset_fieldname property on ".$self->{dataset}->id.".".$self->{name}." (".$fieldname.") is not a valid field on ".$ds->id );
 			}
 			$searchexp->add_field(
 				$ds->field( $fieldname ),
@@ -177,7 +177,7 @@ sub get_value
 		$fieldname = $self->get_property( "dataobj_fieldname" );
 		if( !$ds->has_field( $fieldname ) )
 		{
-			EPrints->abort( "dataset_fieldname property on ".$self->{dataset}->id.".".$self->{name}." is not a valid field on ".$ds->id );
+			EPrints->abort( "dataobj_fieldname property on ".$self->{dataset}->id.".".$self->{name}." (".$fieldname.") is not a valid field on ".$ds->id );
 		}
 		$searchexp->add_field(
 			$ds->field( $fieldname ),
