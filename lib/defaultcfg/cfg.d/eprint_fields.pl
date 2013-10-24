cf9e3c28e71c93931a838eb1b325fa6735e94784
Timothy Miles-Board <tmb@asdf.ecs.soton.ac.uk>
2013-10-24 17:36:10 +0100

    Remove producers, directors etc. - superceded by contributors If contributors is included in all deposit workflows by default, makes sense to use that instead of individual fields for producer, director etc.

--- lib/defaultcfg/cfg.d/eprint_fields.pl
+++ lib/defaultcfg/cfg.d/eprint_fields.pl
@@ -389,28 +389,6 @@ push @{$c->{fields}->{eprint}},
 },
 
 {
-	name => 'exhibitors',
-	type => 'compound',
-	multiple => 1,
-	fields => [
-		{
-			sub_name => 'name',
-			type => 'name',
-			hide_honourific => 1,
-			hide_lineage => 1,
-			family_first => 1,
-		},
-		{
-			sub_name => 'id',
-			type => 'text',
-			input_cols => 20,
-			allow_null => 1,
-		}
-	],
-	input_boxes => 2,
-},
-
-{
 	name => 'num_pieces',
 	type => 'int',
 },
@@ -418,79 +396,6 @@ push @{$c->{fields}->{eprint}},
 {
 	name => 'composition_type',
 	type => 'text',
-},
-
-{
-	name => 'producers',
-	type => 'compound',
-	multiple => 1,
-	fields => [
-		{
-			sub_name => 'name',
-			type => 'name',
-			hide_honourific => 1,
-			hide_lineage => 1,
-			family_first => 1,
-		},
-		{
-			sub_name => 'id',
-			type => 'text',
-			input_cols => 20,
-			allow_null => 1,
-		}
-	],
-	input_boxes => 1,
-},
-
-{
-	name => 'conductors',
-	type => 'compound',
-	multiple => 1,
-	fields => [
-		{
-			sub_name => 'name',
-			type => 'name',
-			hide_honourific => 1,
-			hide_lineage => 1,
-			family_first => 1,
-		},
-		{
-			sub_name => 'id',
-			type => 'text',
-			input_cols => 20,
-			allow_null => 1,
-		}
-	],
-	input_boxes => 1,
-},
-
-{
-	name => 'lyricists',
-	type => 'compound',
-	multiple => 1,
-	fields => [
-		{
-			sub_name => 'name',
-			type => 'name',
-			hide_honourific => 1,
-			hide_lineage => 1,
-			family_first => 1,
-		},
-		{
-			sub_name => 'id',
-			type => 'text',
-			input_cols => 20,
-			allow_null => 1,
-		}
-	],
-	input_boxes => 1,
-},
-
-{
-	name => 'accompaniment',
-	type => 'text',
-	multiple => 1,
-	input_boxes => 1,
 },
 
 {
