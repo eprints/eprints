e3cef27ffd2971b6ccbb3e2c51597c518b54f07a
Sebastien Francois <sf2@ecs.soton.ac.uk>
2014-06-05 11:45:10 +0100

    a failed commit

--- tests/01_use.pl
+++
@@ -1,7 +0,0 @@
-use Test::More tests => 2;
-
-BEGIN { use_ok( "EPrints" ); }
-
-ok(EPrints->human_version =~ /^\d+\.\d+\.\d+$/, "version is X.Y.Z");
-
-diag("EPrints Version: ".EPrints->human_version);
--- tests/TestLib.pm
+++
@@ -1,5 +0,0 @@
-
-package TestLib;
-
-@INC = ( '/opt/eprints3/perl_lib',@INC);
-$TestLib::ARCID = "devel";
