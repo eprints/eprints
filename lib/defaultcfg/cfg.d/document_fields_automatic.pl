6eb577fd2d68ebc7b40ff7c20c3394ddc5e3a0db
Denix <denics@users.noreply.github.com>
2016-07-19 17:09:10 +0200

    media_width and media_height are not used
    I just realized that media_width and media_height are not used in document. it is a pity, because they can be used for test and eprints validation (like using a carousel, is the image of the right size?). Issue #411
    TODO: add information for other media too.

--- lib/defaultcfg/cfg.d/document_fields_automatic.pl
+++ lib/defaultcfg/cfg.d/document_fields_automatic.pl
@@ -1,5 +1,15 @@
-
-$c->{set_document_automatic_fields} = sub
-{
+$c->{set_document_automatic_fields} = sub {
 	my( $doc ) = @_;
+	#######
+	#
+	# Populate the media size values. TODO other media type
+	#
+	#######
+	if(($doc->value("format") eq "image") && defined($doc->value("main"))){
+		use Image::Size;
+		my $file_local_path = $doc->local_path . "/" . $doc->value("main");
+		my ($width, $height, $id) = imgsize( "$file_local_path" );
+		$doc->set_value("media_width", $width);
+		$doc->set_value("media_height", $height);
+	}
 };
